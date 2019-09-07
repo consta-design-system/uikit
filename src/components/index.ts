c4a95e14adea9169d1265bf575a62539f3f1c133
Nikolay.Vasilyev <Nikolay.Vasilyev@nexign.com>
2019-09-07 16:06:04 +0300

    themeProvider draft

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,8 +1,8 @@
 export { default as Button } from './Button/Button';
 export { default as Checkbox } from './Checkbox';
 export { default as ChoiceGroup } from './ChoiceGroup';
-// export const Common = require('./Common/styles.css')
 export { default as Input } from './Input';
 export { default as Loader } from './Loader/Loader';
 export { default as Radio } from './Radio/Radio';
 export { default as Textarea } from './Textarea';
+export { default as ThemeProvider } from './ThemeProvider';
