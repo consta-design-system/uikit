c8567228d02db4dedc2f96af73012c77de0e2531
Nikolay.Vasilyev <Nikolay.Vasilyev@nexign.com>
2019-09-10 18:07:46 +0300

    add themes css to bundle and package, add storybook-deployer

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,9 @@
+import '../themes/theme_color_gpn-dark.css';
+import '../themes/theme_color_gpn-default.css';
+import '../themes/theme_control_gpn-dark.css';
+import '../themes/theme_control_gpn-default.css';
+import '../themes/theme_size_gpn-default.css';
+
 export { default as Button } from './Button';
 export { default as Checkbox } from './Checkbox';
 export { default as ChoiceGroup } from './ChoiceGroup';
@@ -5,4 +11,3 @@ export { default as Input } from './Input';
 export { default as Loader } from './Loader';
 export { default as Radio } from './Radio';
 export { default as Textarea } from './Textarea';
-export { default as ThemeProvider } from './ThemeProvider';
