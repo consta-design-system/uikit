675a06bd575c567f794e53ca6220a253a3b993d3
Andrew Seredkin <ask@midler.xyz>
2019-10-24 11:54:41 +0300

    Update testconfig and snapshots

--- jest.config.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  setupFilesAfterEnv: ['<rootDir>/src/config/setupTests.js'],
-};
--- src/components/Button/Button.test.jsx
+++ src/components/Button/Button.test.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Button from './Button';
+import Button from './index';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
