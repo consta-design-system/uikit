f25a65c987e285c9a4381143af98bd479d86d1fb
Julia Miocene <julia.miocene@gmail.com>
2020-02-12 12:21:45 +0300

    place files in grid in storybook

--- src/components/File/File.stories.tsx
+++ src/components/File/File.stories.tsx
@@ -31,28 +31,94 @@ const defaultKnobs = () => ({
 storiesOf('File', module)
   .addDecorator(withKnobs)
   .add('File', () => (
-    <div>
-      <FileAvi className={'decorator decorator_indent-r_s'} {...defaultKnobs()} />
-      <FileBmp className={'decorator decorator_indent-r_s'} {...defaultKnobs()} />
-      <FileCsv className={'decorator decorator_indent-r_s'} {...defaultKnobs()} />
-      <FileDoc className={'decorator decorator_indent-r_s'} {...defaultKnobs()} />
-      <FileExe className={'decorator decorator_indent-r_s'} {...defaultKnobs()} />
-      <FileGif className={'decorator decorator_indent-r_s'} {...defaultKnobs()} />
-      <FileJpg className={'decorator decorator_indent-r_s'} {...defaultKnobs()} />
-      <FileLoading className={'decorator decorator_indent-r_s'} {...defaultKnobs()} />
-      <FileMov className={'decorator decorator_indent-r_s'} {...defaultKnobs()} />
-      <FileMp3 className={'decorator decorator_indent-r_s'} {...defaultKnobs()} />
-      <FileMp4 className={'decorator decorator_indent-r_s'} {...defaultKnobs()} />
-      <FilePdf className={'decorator decorator_indent-r_s'} {...defaultKnobs()} />
-      <FilePng className={'decorator decorator_indent-r_s'} {...defaultKnobs()} />
-      <FilePpt className={'decorator decorator_indent-r_s'} {...defaultKnobs()} />
-      <FileRar className={'decorator decorator_indent-r_s'} {...defaultKnobs()} />
-      <FileRtf className={'decorator decorator_indent-r_s'} {...defaultKnobs()} />
-      <FileTiff className={'decorator decorator_indent-r_s'} {...defaultKnobs()} />
-      <FileTxt className={'decorator decorator_indent-r_s'} {...defaultKnobs()} />
-      <FileUndefined className={'decorator decorator_indent-r_s'} {...defaultKnobs()} />
-      <FileWav className={'decorator decorator_indent-r_s'} {...defaultKnobs()} />
-      <FileXls className={'decorator decorator_indent-r_s'} {...defaultKnobs()} />
-      <FileZip className={'decorator decorator_indent-r_s'} {...defaultKnobs()} />
+    <div className={'tpl-grid tpl-grid_s-ratio_1-1-1-1-1 tpl-grid_row-gap_full'}>
+      <div className={'tpl-grid__fraction text text_align_center'}>
+        <FileLoading {...defaultKnobs()} />
+        <div className={'text_size_s text_view_secondary'}>Loading</div>
+      </div>
+      <div className={'tpl-grid__fraction text text_align_center'}>
+        <FileUndefined {...defaultKnobs()} />
+        <div className={'text_size_s text_view_secondary'}>Undefined</div>
+      </div>
+      <div className={'tpl-grid__fraction text text_align_center'}>
+        <FileAvi {...defaultKnobs()} />
+        <div className={'text_size_s text_view_secondary'}>AVI</div>
+      </div>
+      <div className={'tpl-grid__fraction text text_align_center'}>
+        <FileBmp {...defaultKnobs()} />
+        <div className={'text_size_s text_view_secondary'}>BMP</div>
+      </div>
+      <div className={'tpl-grid__fraction text text_align_center'}>
+        <FileCsv {...defaultKnobs()} />
+        <div className={'text_size_s text_view_secondary'}>CSV</div>
+      </div>
+      <div className={'tpl-grid__fraction text text_align_center'}>
+        <FileDoc {...defaultKnobs()} />
+        <div className={'text_size_s text_view_secondary'}>DOC</div>
+      </div>
+      <div className={'tpl-grid__fraction text text_align_center'}>
+        <FileExe {...defaultKnobs()} />
+        <div className={'text_size_s text_view_secondary'}>EXE</div>
+      </div>
+      <div className={'tpl-grid__fraction text text_align_center'}>
+        <FileGif {...defaultKnobs()} />
+        <div className={'text_size_s text_view_secondary'}>GIF</div>
+      </div>
+      <div className={'tpl-grid__fraction text text_align_center'}>
+        <FileJpg {...defaultKnobs()} />
+        <div className={'text_size_s text_view_secondary'}>JPG</div>
+      </div>
+      <div className={'tpl-grid__fraction text text_align_center'}>
+        <FileMov {...defaultKnobs()} />
+        <div className={'text_size_s text_view_secondary'}>MOV</div>
+      </div>
+      <div className={'tpl-grid__fraction text text_align_center'}>
+        <FileMp3 {...defaultKnobs()} />
+        <div className={'text_size_s text_view_secondary'}>MP3</div>
+      </div>
+      <div className={'tpl-grid__fraction text text_align_center'}>
+        <FileMp4 {...defaultKnobs()} />
+        <div className={'text_size_s text_view_secondary'}>MP4</div>
+      </div>
+      <div className={'tpl-grid__fraction text text_align_center'}>
+        <FilePdf {...defaultKnobs()} />
+        <div className={'text_size_s text_view_secondary'}>PDF</div>
+      </div>
+      <div className={'tpl-grid__fraction text text_align_center'}>
+        <FilePng {...defaultKnobs()} />
+        <div className={'text_size_s text_view_secondary'}>PNG</div>
+      </div>
+      <div className={'tpl-grid__fraction text text_align_center'}>
+        <FilePpt {...defaultKnobs()} />
+        <div className={'text_size_s text_view_secondary'}>PPT</div>
+      </div>
+      <div className={'tpl-grid__fraction text text_align_center'}>
+        <FileRar {...defaultKnobs()} />
+        <div className={'text_size_s text_view_secondary'}>RAR</div>
+      </div>
+      <div className={'tpl-grid__fraction text text_align_center'}>
+        <FileRtf {...defaultKnobs()} />
+        <div className={'text_size_s text_view_secondary'}>RTF</div>
+      </div>
+      <div className={'tpl-grid__fraction text text_align_center'}>
+        <FileTiff {...defaultKnobs()} />
+        <div className={'text_size_s text_view_secondary'}>TIFF</div>
+      </div>
+      <div className={'tpl-grid__fraction text text_align_center'}>
+        <FileTxt {...defaultKnobs()} />
+        <div className={'text_size_s text_view_secondary'}>TXT</div>
+      </div>
+      <div className={'tpl-grid__fraction text text_align_center'}>
+        <FileWav {...defaultKnobs()} />
+        <div className={'text_size_s text_view_secondary'}>WAV</div>
+      </div>
+      <div className={'tpl-grid__fraction text text_align_center'}>
+        <FileXls {...defaultKnobs()} />
+        <div className={'text_size_s text_view_secondary'}>XLS</div>
+      </div>
+      <div className={'tpl-grid__fraction text text_align_center'}>
+        <FileZip {...defaultKnobs()} />
+        <div className={'text_size_s text_view_secondary'}>ZIP</div>
+      </div>
     </div>
   ));
