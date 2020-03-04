3a4749e2145be8ad026dacc238e134a09399c743
Reutov <dikovina8@mail.ru>
2020-03-04 16:15:55 +0300

    tabs without drag

--- src/components/Tabs/index.tsx
+++ src/components/Tabs/index.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect, useState } from 'react';
+import React, { useRef, useEffect } from 'react';
 import bem from '../../utils/bem';
 
 import './styles.css';
@@ -20,7 +20,6 @@ type TabsProps = {
 const Tabs: React.FC<TabsProps> = ({ activeCode, list, view, wpSize, onChange }) => {
   const refInner = useRef<HTMLDivElement>(null);
   const refLine = useRef<HTMLDivElement>(null);
-  const refRoot = useRef<HTMLDivElement>(null);
 
   const updateLine = () => {
     const lineElement = refLine.current;
@@ -40,61 +39,18 @@ const Tabs: React.FC<TabsProps> = ({ activeCode, list, view, wpSize, onChange })
 
   useEffect(updateLine, [activeCode]);
 
-  const [drag, setIsDrag] = useState({
-    start: 0,
-    current: 0,
-    isActive: false,
-  });
-
-  const onMoveStart = e => {
-    if (drag.isActive) return;
-    const rootElement = refRoot.current;
-
-    if (rootElement === null) return;
-    setIsDrag({
-      ...drag,
-      start: e.pageX,
-      current: rootElement.scrollLeft,
-      isActive: true,
-    });
-  };
-
-  const onMoveEnd = () => {
-    setIsDrag({
-      ...drag,
-      isActive: false,
-    });
-  };
-
-  const onMove = e => {
-    if (!drag.isActive) return;
-    const rootElement = refRoot.current;
-    if (rootElement === null) return;
-    rootElement.scrollLeft = drag.current - (e.pageX - drag.start);
-  };
-
   const onClick = (e, code) => {
     e.preventDefault();
     onChange(code);
   };
 
   useEffect(() => {
-    document.addEventListener('mousemove', onMove);
-    document.addEventListener('mouseup', onMoveEnd);
-
-    return () => {
-      document.removeEventListener('mousemove', onMove);
-      document.removeEventListener('mouseup', onMoveEnd);
-    };
-  }, [onMove, onMoveEnd]);
-
-  useEffect(() => {
     setTimeout(updateLine, 150);
   }, []);
 
   return (
-    <div className={b({ view, wpSize })} ref={refRoot}>
-      <div className={b('inner')} ref={refInner} onMouseDown={onMoveStart}>
+    <div className={b({ view, wpSize })}>
+      <div className={b('inner')} ref={refInner}>
         {list.map(({ label, code }) => (
           <a
             key={code}
