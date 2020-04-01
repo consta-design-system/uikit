46fc388d1b5e50fa65997eddaa4b5b7e394275a5
gizeasy <gizeasy@gmail.com>
2020-04-01 19:00:48 +0300

    fix

--- src/components/User/User.stories.tsx
+++ src/components/User/User.stories.tsx
@@ -7,7 +7,7 @@ const defaultKnobs = () => ({
   view: select('view', ['ghost', 'clear'], 'clear'),
   width: select('width', ['full', 'default'], 'default'),
   size: select('size', ['s', 'm'], 'm'),
-  status: select('status', ['available', 'remote', 'out', ''], ''),
+  status: select('status', ['available', 'remote', 'out'], undefined),
   avatarUrl: text(
     'avatarUrl',
     `https://pbs.twimg.com/profile_images/1150453787603156992/DoiKLDMY_400x400.png`
