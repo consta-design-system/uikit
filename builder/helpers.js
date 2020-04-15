0b40c4f575c0df2a6aba9565aa920f307902a423
gizeasy <gizeasy@gmail.com>
2020-04-15 18:18:45 +0300

    svg-rename

--- builder/helpers.js
+++ builder/helpers.js
@@ -139,6 +139,52 @@ const iconsTransformed = async (ignore, src) => {
   await createIconStories(svgComponents, src);
 };
 
+const iconsRenamerToBem = async (ignore, src) => {
+  const iconParse = async ({ componentName, path, pathOutdir }) => {
+    const svg = await readFile(path, 'utf8');
+    const jsPatch = `${pathOutdir}/${componentName}.svg`;
+    await ensureDir(dirname(jsPatch));
+    await writeFile(jsPatch, svg);
+    await remove(path);
+  };
+
+  const svgFiles = await fg([`${src}/icons/**/*.{svg}`], { ignore });
+
+  const test = /.\/src\/icons\/(.+)\/(.+).svg/;
+  const svgComponents = {};
+
+  svgFiles.forEach((fileName) => {
+    if (test.test(fileName)) {
+      const [file, componentName, size] = test.exec(fileName);
+      if (!svgComponents[componentName]) {
+        svgComponents[componentName] = {};
+      }
+      svgComponents[componentName][size.toLowerCase()] = file;
+    }
+  });
+
+  Object.keys(svgComponents).forEach(async (componentName) => {
+    if (iconComponentIsValid(svgComponents[componentName])) {
+      await iconParse({
+        componentName: `${componentName}_size_xs`,
+        path: svgComponents[componentName].xs,
+        pathOutdir: `./src/icons/${componentName}/`,
+      });
+      await iconParse({
+        componentName: `${componentName}_size_s`,
+        path: svgComponents[componentName].s,
+        pathOutdir: `./src/icons/${componentName}/`,
+      });
+      await iconParse({
+        componentName: `${componentName}_size_xs`,
+        path: svgComponents[componentName].m,
+        pathOutdir: `./src/icons/${componentName}/`,
+      });
+    }
+  });
+  await createIconStories(svgComponents, src);
+};
+
 const copyAssets = async (ignore, src, distPaths) => {
   const assetFiles = await fg([`${src}/**/*.{svg,jpg,png,gif,md}`], { ignore });
 
