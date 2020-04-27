0d308a0332243529ce99a56e090bf8af128a6797
Roman Gurinovich <gurinovich.rv@gmail.com>
2020-04-27 17:36:22 +0300

    chore(tests): добавление инфраструктуры для тестов

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  verbose: true,
+  preset: 'ts-jest',
+  moduleNameMapper: {
+    '^.+\\.css$': 'identity-obj-proxy',
+  },
+  coveragePathIgnorePatterns: ['/node_modules/', '/coverage/', '/types/'],
+  testMatch: ['**/*.test.{ts,tsx}'],
+  collectCoverageFrom: ['**/*.{ts,tsx}', '!**/*.stories.tsx'],
+};
