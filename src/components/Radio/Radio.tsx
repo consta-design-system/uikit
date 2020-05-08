a4ccda129ffb7f02adb6db90db6df54017244d34
gizeasy <gizeasy@gmail.com>
2020-05-08 14:02:26 +0300

    Поменял в компонентах onClick на нативный: Checkbox.tsx Radio.tsx Switch.tsx

--- src/components/Radio/Radio.tsx
+++ src/components/Radio/Radio.tsx
@@ -1,32 +1,21 @@
 import './Radio.css';
 
-import React from 'react';
+import React, { ChangeEventHandler } from 'react';
 import { cn } from '../../utils/bem';
 
 export type RadioPropName = string;
-export type RadioPropId = string | number;
-export type RadioPropValue<T = any> = T | null;
 export type RadioPropChecked = boolean;
 export type RadioPropIntermediate = boolean;
 export type RadioPropSize = 'm' | 'l';
-export type RadioOnChangeArguments<T = any> = {
-  e: React.ChangeEvent<HTMLInputElement>;
-  id?: RadioPropId;
-  name?: RadioPropName;
-  value: RadioPropValue<T>;
-  checked: RadioPropChecked;
-};
 
-export type RadioProps<T = any> = {
-  value?: RadioPropValue<T>;
+export type RadioProps = {
   checked?: RadioPropChecked;
   size?: RadioPropSize;
   disabled?: boolean;
   intermediate?: RadioPropIntermediate;
   className?: string;
   label?: string;
-  onChange?: (object: RadioOnChangeArguments<T>) => void;
-  id?: RadioPropId;
+  onChange?: ChangeEventHandler<HTMLInputElement>;
   name?: RadioPropName;
   onFocus?: React.FocusEventHandler<HTMLInputElement>;
   onBlur?: React.FocusEventHandler<HTMLInputElement>;
@@ -39,16 +28,14 @@ export type RadioProps<T = any> = {
   innerRef?: React.Ref<HTMLLabelElement>;
 };
 
-declare type IRadio<T = any> = RadioProps<T> &
-  Omit<React.InputHTMLAttributes<HTMLLabelElement>, keyof RadioProps<T>>;
+declare type IRadio = RadioProps &
+  Omit<React.InputHTMLAttributes<HTMLLabelElement>, keyof RadioProps>;
 
 export const cnRadio = cn('Radio');
 
-export function Radio<T = any>(props: IRadio<T>): React.ReactElement | null {
+export function Radio(props: IRadio): React.ReactElement | null {
   const {
-    value = null,
     checked = false,
-    id,
     name,
     size = 'm',
     disabled,
@@ -65,22 +52,16 @@ export function Radio<T = any>(props: IRadio<T>): React.ReactElement | null {
     innerRef,
     ...otherProps
   } = props;
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (!disabled && onChange) {
-      onChange({ e, id, name, value, checked: !checked });
-    }
-  };
 
   return (
     <label {...otherProps} className={cnRadio({ size, disabled }, [className])} ref={innerRef}>
       <input
         type="radio"
-        id={id ? id.toString() : undefined}
         name={name}
         className={cnRadio('Input')}
         checked={checked}
         disabled={disabled}
-        onChange={handleChange}
+        onChange={onChange}
         onFocus={onFocus}
         onBlur={onBlur}
         readOnly={readOnly}
