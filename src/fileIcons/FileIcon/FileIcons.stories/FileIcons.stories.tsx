21e34e258b8e3095f6b15f3fa4cbe75125675606
gizeasy <gizeasy@gmail.com>
2020-05-15 11:48:13 +0300

    fix

--- src/fileIcons/FileIcon/FileIcons.stories/FileIcons.stories.tsx
+++ src/fileIcons/FileIcon/FileIcons.stories/FileIcons.stories.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { withKnobs, select } from '@storybook/addon-knobs';
 import { storiesOf } from '@storybook/react';
-import { FileIconAvi } from '../../FileIconAvi/FileIconAvi';
 import { FileIconBmp } from '../../FileIconBmp/FileIconBmp';
+import { FileIconAvi } from '../../FileIconAvi/FileIconAvi';
 import { FileIconCsv } from '../../FileIconCsv/FileIconCsv';
 import { FileIconDoc } from '../../FileIconDoc/FileIconDoc';
+import { FileIconJpg } from '../../FileIconJpg/FileIconJpg';
 import { FileIconExe } from '../../FileIconExe/FileIconExe';
 import { FileIconGif } from '../../FileIconGif/FileIconGif';
-import { FileIconJpg } from '../../FileIconJpg/FileIconJpg';
 import { FileIconLoading } from '../../FileIconLoading/FileIconLoading';
 import { FileIconMov } from '../../FileIconMov/FileIconMov';
 import { FileIconMp3 } from '../../FileIconMp3/FileIconMp3';
@@ -34,29 +34,27 @@ storiesOf('FileIcons', module)
   .addDecorator(withKnobs)
   .add('FileIcons', () => (
     <div className={'tpl-grid tpl-grid_s-ratio_1-1-1-1-1 tpl-grid_row-gap_full'}>
-    <IconsItem name="FileIconAvi" icon={FileIconAvi} {...defaultKnobs()} />
-<IconsItem name="FileIconBmp" icon={FileIconBmp} {...defaultKnobs()} />
-<IconsItem name="FileIconCsv" icon={FileIconCsv} {...defaultKnobs()} />
-<IconsItem name="FileIconDoc" icon={FileIconDoc} {...defaultKnobs()} />
-<IconsItem name="FileIconExe" icon={FileIconExe} {...defaultKnobs()} />
-<IconsItem name="FileIconGif" icon={FileIconGif} {...defaultKnobs()} />
-<IconsItem name="FileIconJpg" icon={FileIconJpg} {...defaultKnobs()} />
-<IconsItem name="FileIconLoading" icon={FileIconLoading} {...defaultKnobs()} />
-<IconsItem name="FileIconMov" icon={FileIconMov} {...defaultKnobs()} />
-<IconsItem name="FileIconMp3" icon={FileIconMp3} {...defaultKnobs()} />
-<IconsItem name="FileIconMp4" icon={FileIconMp4} {...defaultKnobs()} />
-<IconsItem name="FileIconPdf" icon={FileIconPdf} {...defaultKnobs()} />
-<IconsItem name="FileIconPng" icon={FileIconPng} {...defaultKnobs()} />
-<IconsItem name="FileIconPtt" icon={FileIconPtt} {...defaultKnobs()} />
-<IconsItem name="FileIconRar" icon={FileIconRar} {...defaultKnobs()} />
-<IconsItem name="FileIconRtf" icon={FileIconRtf} {...defaultKnobs()} />
-<IconsItem name="FileIconTiff" icon={FileIconTiff} {...defaultKnobs()} />
-<IconsItem name="FileIconTxt" icon={FileIconTxt} {...defaultKnobs()} />
-<IconsItem name="FileIconUndefined" icon={FileIconUndefined} {...defaultKnobs()} />
-<IconsItem name="FileIconWav" icon={FileIconWav} {...defaultKnobs()} />
-<IconsItem name="FileIconXls" icon={FileIconXls} {...defaultKnobs()} />
-<IconsItem name="FileIconZip" icon={FileIconZip} {...defaultKnobs()} />
-
+      <IconsItem name="FileIconBmp" icon={FileIconBmp} {...defaultKnobs()} />
+      <IconsItem name="FileIconAvi" icon={FileIconAvi} {...defaultKnobs()} />
+      <IconsItem name="FileIconCsv" icon={FileIconCsv} {...defaultKnobs()} />
+      <IconsItem name="FileIconDoc" icon={FileIconDoc} {...defaultKnobs()} />
+      <IconsItem name="FileIconJpg" icon={FileIconJpg} {...defaultKnobs()} />
+      <IconsItem name="FileIconExe" icon={FileIconExe} {...defaultKnobs()} />
+      <IconsItem name="FileIconGif" icon={FileIconGif} {...defaultKnobs()} />
+      <IconsItem name="FileIconLoading" icon={FileIconLoading} {...defaultKnobs()} />
+      <IconsItem name="FileIconMov" icon={FileIconMov} {...defaultKnobs()} />
+      <IconsItem name="FileIconMp3" icon={FileIconMp3} {...defaultKnobs()} />
+      <IconsItem name="FileIconMp4" icon={FileIconMp4} {...defaultKnobs()} />
+      <IconsItem name="FileIconPdf" icon={FileIconPdf} {...defaultKnobs()} />
+      <IconsItem name="FileIconPng" icon={FileIconPng} {...defaultKnobs()} />
+      <IconsItem name="FileIconPtt" icon={FileIconPtt} {...defaultKnobs()} />
+      <IconsItem name="FileIconRar" icon={FileIconRar} {...defaultKnobs()} />
+      <IconsItem name="FileIconRtf" icon={FileIconRtf} {...defaultKnobs()} />
+      <IconsItem name="FileIconTiff" icon={FileIconTiff} {...defaultKnobs()} />
+      <IconsItem name="FileIconTxt" icon={FileIconTxt} {...defaultKnobs()} />
+      <IconsItem name="FileIconUndefined" icon={FileIconUndefined} {...defaultKnobs()} />
+      <IconsItem name="FileIconWav" icon={FileIconWav} {...defaultKnobs()} />
+      <IconsItem name="FileIconXls" icon={FileIconXls} {...defaultKnobs()} />
+      <IconsItem name="FileIconZip" icon={FileIconZip} {...defaultKnobs()} />
     </div>
   ));
-
