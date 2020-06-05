0488b9026604149fd93b7ea8e7a4bc105338dd96
gizeasy <gizeasy@gmail.com>
2020-06-05 14:03:10 +0300

    chore(commitizen): add commitizen and configure it

---
+++ commitizen.js
@@ -0,0 +1,29 @@
+module.exports = {
+  types: [
+    { value: 'feat', name: 'feat:      Adding new feature' },
+    { value: 'fix', name: 'fix:       Bug fix' },
+    { value: 'test', name: 'test:      Adding Tests' },
+    {
+      value: 'refactor',
+      name: 'refactor:  Editing code without fixing bugs or adding new features',
+    },
+    { value: 'docs', name: 'docs:      Documentation Update' },
+    { value: 'chore', name: 'chore      Add / update / customize tools and libraries' },
+  ],
+
+  scopes: [{ name: 'custom' }, { name: 'components' }, { name: 'global' }],
+
+  messages: {
+    type: 'Type of change',
+    scope: 'Scope of changes (optional):',
+    customScope: 'Enter your scope:',
+    subject: 'Summary of changes:\n',
+    body: 'Detailed description of changes:\n',
+    breaking: 'Description BREAKING CHANGES:\n',
+    confirmCommit: 'Save the resulting commit?',
+  },
+  skipQuestions: ['footer'],
+  allowBreakingChanges: ['feat', 'fix', 'test', 'refactor', 'docs', 'chore'],
+  allowCustomScopes: true,
+  subjectLimit: 100,
+};
