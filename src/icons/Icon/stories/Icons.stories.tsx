d5be2773977be10fcc4d6e457fc54cc188308367
gizeasy <gizeasy@gmail.com>
2020-06-29 19:33:33 +0300

    refactor(icons): re-generate icons
    issue -  #110

--- src/icons/Icon/stories/Icons.stories.tsx
+++ src/icons/Icon/stories/Icons.stories.tsx
@@ -114,9 +114,9 @@ storiesOf('Icons', module)
   .addDecorator(withKnobs)
   .add('Icons', () => (
     <div className="tpl-grid tpl-grid_s-ratio_1-1-1-1-1 tpl-grid_row-gap_full">
+      <IconsItem name="IconAlignCenter" icon={IconAlignCenter} {...defaultKnobs()} />
       <IconsItem name="IconAdd" icon={IconAdd} {...defaultKnobs()} />
       <IconsItem name="IconAlignJustify" icon={IconAlignJustify} {...defaultKnobs()} />
-      <IconsItem name="IconAlignCenter" icon={IconAlignCenter} {...defaultKnobs()} />
       <IconsItem name="IconAlert" icon={IconAlert} {...defaultKnobs()} />
       <IconsItem name="IconAlignLeft" icon={IconAlignLeft} {...defaultKnobs()} />
       <IconsItem name="IconAlignRight" icon={IconAlignRight} {...defaultKnobs()} />
@@ -126,31 +126,31 @@ storiesOf('Icons', module)
       <IconsItem name="IconArrowUp" icon={IconArrowUp} {...defaultKnobs()} />
       <IconsItem name="IconAttach" icon={IconAttach} {...defaultKnobs()} />
       <IconsItem name="IconBackward" icon={IconBackward} {...defaultKnobs()} />
-      <IconsItem name="IconBag" icon={IconBag} {...defaultKnobs()} />
       <IconsItem name="IconBarrier" icon={IconBarrier} {...defaultKnobs()} />
       <IconsItem name="IconBento" icon={IconBento} {...defaultKnobs()} />
+      <IconsItem name="IconBag" icon={IconBag} {...defaultKnobs()} />
       <IconsItem name="IconBold" icon={IconBold} {...defaultKnobs()} />
       <IconsItem name="IconBookmarkFilled" icon={IconBookmarkFilled} {...defaultKnobs()} />
       <IconsItem name="IconBookmarkStroked" icon={IconBookmarkStroked} {...defaultKnobs()} />
       <IconsItem name="IconCalendar" icon={IconCalendar} {...defaultKnobs()} />
-      <IconsItem name="IconCamera" icon={IconCamera} {...defaultKnobs()} />
       <IconsItem name="IconCancel" icon={IconCancel} {...defaultKnobs()} />
+      <IconsItem name="IconCamera" icon={IconCamera} {...defaultKnobs()} />
       <IconsItem name="IconChat" icon={IconChat} {...defaultKnobs()} />
       <IconsItem name="IconCheck" icon={IconCheck} {...defaultKnobs()} />
-      <IconsItem name="IconClose" icon={IconClose} {...defaultKnobs()} />
       <IconsItem name="IconCollapse" icon={IconCollapse} {...defaultKnobs()} />
+      <IconsItem name="IconClose" icon={IconClose} {...defaultKnobs()} />
       <IconsItem name="IconColorFill" icon={IconColorFill} {...defaultKnobs()} />
       <IconsItem name="IconColorText" icon={IconColorText} {...defaultKnobs()} />
+      <IconsItem name="IconConnection" icon={IconConnection} {...defaultKnobs()} />
       <IconsItem name="IconColumns" icon={IconColumns} {...defaultKnobs()} />
       <IconsItem name="IconComment" icon={IconComment} {...defaultKnobs()} />
-      <IconsItem name="IconConnection" icon={IconConnection} {...defaultKnobs()} />
+      <IconsItem name="IconDiamond" icon={IconDiamond} {...defaultKnobs()} />
       <IconsItem name="IconCopy" icon={IconCopy} {...defaultKnobs()} />
       <IconsItem name="IconCrown" icon={IconCrown} {...defaultKnobs()} />
-      <IconsItem name="IconDiamond" icon={IconDiamond} {...defaultKnobs()} />
       <IconsItem name="IconDocAdd" icon={IconDocAdd} {...defaultKnobs()} />
       <IconsItem name="IconDocBlank" icon={IconDocBlank} {...defaultKnobs()} />
-      <IconsItem name="IconDocDelete" icon={IconDocDelete} {...defaultKnobs()} />
       <IconsItem name="IconDocFilled" icon={IconDocFilled} {...defaultKnobs()} />
+      <IconsItem name="IconDocDelete" icon={IconDocDelete} {...defaultKnobs()} />
       <IconsItem name="IconDown" icon={IconDown} {...defaultKnobs()} />
       <IconsItem name="IconDrag" icon={IconDrag} {...defaultKnobs()} />
       <IconsItem name="IconDrop" icon={IconDrop} {...defaultKnobs()} />
@@ -162,17 +162,17 @@ storiesOf('Icons', module)
       <IconsItem name="IconFolders" icon={IconFolders} {...defaultKnobs()} />
       <IconsItem name="IconForward" icon={IconForward} {...defaultKnobs()} />
       <IconsItem name="IconFunnel" icon={IconFunnel} {...defaultKnobs()} />
-      <IconsItem name="IconHamburger" icon={IconHamburger} {...defaultKnobs()} />
       <IconsItem name="IconGas" icon={IconGas} {...defaultKnobs()} />
+      <IconsItem name="IconHamburger" icon={IconHamburger} {...defaultKnobs()} />
       <IconsItem name="IconItalic" icon={IconItalic} {...defaultKnobs()} />
-      <IconsItem name="IconLayers" icon={IconLayers} {...defaultKnobs()} />
       <IconsItem name="IconKebab" icon={IconKebab} {...defaultKnobs()} />
-      <IconsItem name="IconLeaf" icon={IconLeaf} {...defaultKnobs()} />
+      <IconsItem name="IconLayers" icon={IconLayers} {...defaultKnobs()} />
       <IconsItem name="IconLink" icon={IconLink} {...defaultKnobs()} />
+      <IconsItem name="IconLeaf" icon={IconLeaf} {...defaultKnobs()} />
       <IconsItem name="IconList" icon={IconList} {...defaultKnobs()} />
       <IconsItem name="IconListNumbered" icon={IconListNumbered} {...defaultKnobs()} />
-      <IconsItem name="IconLock" icon={IconLock} {...defaultKnobs()} />
       <IconsItem name="IconMail" icon={IconMail} {...defaultKnobs()} />
+      <IconsItem name="IconLock" icon={IconLock} {...defaultKnobs()} />
       <IconsItem name="IconMeatball" icon={IconMeatball} {...defaultKnobs()} />
       <IconsItem name="IconOpenInNew" icon={IconOpenInNew} {...defaultKnobs()} />
       <IconsItem name="IconPause" icon={IconPause} {...defaultKnobs()} />
@@ -182,8 +182,8 @@ storiesOf('Icons', module)
       <IconsItem name="IconQuestion" icon={IconQuestion} {...defaultKnobs()} />
       <IconsItem name="IconQuote" icon={IconQuote} {...defaultKnobs()} />
       <IconsItem name="IconRecord" icon={IconRecord} {...defaultKnobs()} />
-      <IconsItem name="IconRemove" icon={IconRemove} {...defaultKnobs()} />
       <IconsItem name="IconReply" icon={IconReply} {...defaultKnobs()} />
+      <IconsItem name="IconRemove" icon={IconRemove} {...defaultKnobs()} />
       <IconsItem name="IconRevert" icon={IconRevert} {...defaultKnobs()} />
       <IconsItem name="IconRing" icon={IconRing} {...defaultKnobs()} />
       <IconsItem name="IconRouble" icon={IconRouble} {...defaultKnobs()} />
@@ -197,13 +197,13 @@ storiesOf('Icons', module)
       <IconsItem name="IconSortUp" icon={IconSortUp} {...defaultKnobs()} />
       <IconsItem name="IconSortUpCenter" icon={IconSortUpCenter} {...defaultKnobs()} />
       <IconsItem name="IconStop" icon={IconStop} {...defaultKnobs()} />
-      <IconsItem name="IconStorage" icon={IconStorage} {...defaultKnobs()} />
       <IconsItem name="IconStrikethrough" icon={IconStrikethrough} {...defaultKnobs()} />
       <IconsItem name="IconTable" icon={IconTable} {...defaultKnobs()} />
-      <IconsItem name="IconTest" icon={IconTest} {...defaultKnobs()} />
       <IconsItem name="IconThumbUp" icon={IconThumbUp} {...defaultKnobs()} />
-      <IconsItem name="IconTie" icon={IconTie} {...defaultKnobs()} />
+      <IconsItem name="IconTest" icon={IconTest} {...defaultKnobs()} />
+      <IconsItem name="IconStorage" icon={IconStorage} {...defaultKnobs()} />
       <IconsItem name="IconTop" icon={IconTop} {...defaultKnobs()} />
+      <IconsItem name="IconTie" icon={IconTie} {...defaultKnobs()} />
       <IconsItem name="IconTrash" icon={IconTrash} {...defaultKnobs()} />
       <IconsItem name="IconType" icon={IconType} {...defaultKnobs()} />
       <IconsItem name="IconUnderline" icon={IconUnderline} {...defaultKnobs()} />
