fd056d03b6e0dd3d747f3b9694de6004cccd368e
gizeasy <gizeasy@gmail.com>
2020-07-06 13:37:44 +0300

    feat(Icons): sort icons in storyBook
    issue -  #206

--- src/icons/Icon/stories/Icons.stories.tsx
+++ src/icons/Icon/stories/Icons.stories.tsx
@@ -111,106 +111,112 @@ const defaultKnobs = () => ({
   ),
 });
 
+const icons = {
+  IconAdd,
+  IconAlert,
+  IconAlignCenter,
+  IconAlignLeft,
+  IconAlignJustify,
+  IconAlignRight,
+  IconArrowDown,
+  IconArrowLeft,
+  IconArrowRight,
+  IconArrowUp,
+  IconAttach,
+  IconBackward,
+  IconBag,
+  IconBarrier,
+  IconBento,
+  IconBold,
+  IconBookmarkFilled,
+  IconBookmarkStroked,
+  IconCalendar,
+  IconCamera,
+  IconCancel,
+  IconChat,
+  IconCheck,
+  IconClose,
+  IconCollapse,
+  IconColorFill,
+  IconColorText,
+  IconColumns,
+  IconComment,
+  IconConnection,
+  IconCopy,
+  IconCrown,
+  IconDiamond,
+  IconDocAdd,
+  IconDocBlank,
+  IconDocDelete,
+  IconDocFilled,
+  IconDown,
+  IconDrag,
+  IconDrop,
+  IconEdit,
+  IconExpand,
+  IconEye,
+  IconFavorite,
+  IconFilter,
+  IconFolders,
+  IconForward,
+  IconFunnel,
+  IconGas,
+  IconHamburger,
+  IconItalic,
+  IconKebab,
+  IconLayers,
+  IconLeaf,
+  IconLink,
+  IconList,
+  IconListNumbered,
+  IconLock,
+  IconMail,
+  IconMeatball,
+  IconOpenInNew,
+  IconPause,
+  IconPhoto,
+  IconPlay,
+  IconProcessing,
+  IconQuestion,
+  IconQuote,
+  IconRecord,
+  IconRemove,
+  IconReply,
+  IconRevert,
+  IconRing,
+  IconRouble,
+  IconSearch,
+  IconSelect,
+  IconSelectOpen,
+  IconSettings,
+  IconShuffle,
+  IconSmile,
+  IconSortDown,
+  IconSortDownCenter,
+  IconSortUp,
+  IconSortUpCenter,
+  IconStop,
+  IconStorage,
+  IconStrikethrough,
+  IconTable,
+  IconTest,
+  IconThumbUp,
+  IconTie,
+  IconTop,
+  IconTrash,
+  IconType,
+  IconUnderline,
+  IconUser,
+  IconWarning,
+  IconWorld,
+};
+
 storiesOf('Icons', module)
   .addDecorator(withKnobs)
   .add('Icons', () => (
     <div className="tpl-grid tpl-grid_s-ratio_1-1-1-1-1 tpl-grid_row-gap_full">
-      <IconsItem name="IconAlignJustify" icon={IconAlignJustify} {...defaultKnobs()} />
-      <IconsItem name="IconAlert" icon={IconAlert} {...defaultKnobs()} />
-      <IconsItem name="IconAlignCenter" icon={IconAlignCenter} {...defaultKnobs()} />
-      <IconsItem name="IconAlignLeft" icon={IconAlignLeft} {...defaultKnobs()} />
-      <IconsItem name="IconAdd" icon={IconAdd} {...defaultKnobs()} />
-      <IconsItem name="IconAlignRight" icon={IconAlignRight} {...defaultKnobs()} />
-      <IconsItem name="IconArrowDown" icon={IconArrowDown} {...defaultKnobs()} />
-      <IconsItem name="IconArrowLeft" icon={IconArrowLeft} {...defaultKnobs()} />
-      <IconsItem name="IconArrowRight" icon={IconArrowRight} {...defaultKnobs()} />
-      <IconsItem name="IconArrowUp" icon={IconArrowUp} {...defaultKnobs()} />
-      <IconsItem name="IconAttach" icon={IconAttach} {...defaultKnobs()} />
-      <IconsItem name="IconBackward" icon={IconBackward} {...defaultKnobs()} />
-      <IconsItem name="IconBag" icon={IconBag} {...defaultKnobs()} />
-      <IconsItem name="IconBarrier" icon={IconBarrier} {...defaultKnobs()} />
-      <IconsItem name="IconBento" icon={IconBento} {...defaultKnobs()} />
-      <IconsItem name="IconBold" icon={IconBold} {...defaultKnobs()} />
-      <IconsItem name="IconBookmarkFilled" icon={IconBookmarkFilled} {...defaultKnobs()} />
-      <IconsItem name="IconBookmarkStroked" icon={IconBookmarkStroked} {...defaultKnobs()} />
-      <IconsItem name="IconCalendar" icon={IconCalendar} {...defaultKnobs()} />
-      <IconsItem name="IconCamera" icon={IconCamera} {...defaultKnobs()} />
-      <IconsItem name="IconCancel" icon={IconCancel} {...defaultKnobs()} />
-      <IconsItem name="IconChat" icon={IconChat} {...defaultKnobs()} />
-      <IconsItem name="IconCheck" icon={IconCheck} {...defaultKnobs()} />
-      <IconsItem name="IconClose" icon={IconClose} {...defaultKnobs()} />
-      <IconsItem name="IconCollapse" icon={IconCollapse} {...defaultKnobs()} />
-      <IconsItem name="IconColorFill" icon={IconColorFill} {...defaultKnobs()} />
-      <IconsItem name="IconColorText" icon={IconColorText} {...defaultKnobs()} />
-      <IconsItem name="IconColumns" icon={IconColumns} {...defaultKnobs()} />
-      <IconsItem name="IconComment" icon={IconComment} {...defaultKnobs()} />
-      <IconsItem name="IconConnection" icon={IconConnection} {...defaultKnobs()} />
-      <IconsItem name="IconCopy" icon={IconCopy} {...defaultKnobs()} />
-      <IconsItem name="IconCrown" icon={IconCrown} {...defaultKnobs()} />
-      <IconsItem name="IconDiamond" icon={IconDiamond} {...defaultKnobs()} />
-      <IconsItem name="IconDocAdd" icon={IconDocAdd} {...defaultKnobs()} />
-      <IconsItem name="IconDocBlank" icon={IconDocBlank} {...defaultKnobs()} />
-      <IconsItem name="IconDocDelete" icon={IconDocDelete} {...defaultKnobs()} />
-      <IconsItem name="IconDocFilled" icon={IconDocFilled} {...defaultKnobs()} />
-      <IconsItem name="IconDown" icon={IconDown} {...defaultKnobs()} />
-      <IconsItem name="IconDrag" icon={IconDrag} {...defaultKnobs()} />
-      <IconsItem name="IconDrop" icon={IconDrop} {...defaultKnobs()} />
-      <IconsItem name="IconEdit" icon={IconEdit} {...defaultKnobs()} />
-      <IconsItem name="IconExpand" icon={IconExpand} {...defaultKnobs()} />
-      <IconsItem name="IconEye" icon={IconEye} {...defaultKnobs()} />
-      <IconsItem name="IconFavorite" icon={IconFavorite} {...defaultKnobs()} />
-      <IconsItem name="IconFilter" icon={IconFilter} {...defaultKnobs()} />
-      <IconsItem name="IconFolders" icon={IconFolders} {...defaultKnobs()} />
-      <IconsItem name="IconForward" icon={IconForward} {...defaultKnobs()} />
-      <IconsItem name="IconFunnel" icon={IconFunnel} {...defaultKnobs()} />
-      <IconsItem name="IconGas" icon={IconGas} {...defaultKnobs()} />
-      <IconsItem name="IconHamburger" icon={IconHamburger} {...defaultKnobs()} />
-      <IconsItem name="IconItalic" icon={IconItalic} {...defaultKnobs()} />
-      <IconsItem name="IconKebab" icon={IconKebab} {...defaultKnobs()} />
-      <IconsItem name="IconLayers" icon={IconLayers} {...defaultKnobs()} />
-      <IconsItem name="IconLeaf" icon={IconLeaf} {...defaultKnobs()} />
-      <IconsItem name="IconLink" icon={IconLink} {...defaultKnobs()} />
-      <IconsItem name="IconListNumbered" icon={IconListNumbered} {...defaultKnobs()} />
-      <IconsItem name="IconList" icon={IconList} {...defaultKnobs()} />
-      <IconsItem name="IconLock" icon={IconLock} {...defaultKnobs()} />
-      <IconsItem name="IconMail" icon={IconMail} {...defaultKnobs()} />
-      <IconsItem name="IconMeatball" icon={IconMeatball} {...defaultKnobs()} />
-      <IconsItem name="IconOpenInNew" icon={IconOpenInNew} {...defaultKnobs()} />
-      <IconsItem name="IconPause" icon={IconPause} {...defaultKnobs()} />
-      <IconsItem name="IconPhoto" icon={IconPhoto} {...defaultKnobs()} />
-      <IconsItem name="IconPlay" icon={IconPlay} {...defaultKnobs()} />
-      <IconsItem name="IconProcessing" icon={IconProcessing} {...defaultKnobs()} />
-      <IconsItem name="IconQuestion" icon={IconQuestion} {...defaultKnobs()} />
-      <IconsItem name="IconQuote" icon={IconQuote} {...defaultKnobs()} />
-      <IconsItem name="IconRecord" icon={IconRecord} {...defaultKnobs()} />
-      <IconsItem name="IconRemove" icon={IconRemove} {...defaultKnobs()} />
-      <IconsItem name="IconReply" icon={IconReply} {...defaultKnobs()} />
-      <IconsItem name="IconRevert" icon={IconRevert} {...defaultKnobs()} />
-      <IconsItem name="IconRing" icon={IconRing} {...defaultKnobs()} />
-      <IconsItem name="IconRouble" icon={IconRouble} {...defaultKnobs()} />
-      <IconsItem name="IconSearch" icon={IconSearch} {...defaultKnobs()} />
-      <IconsItem name="IconSelect" icon={IconSelect} {...defaultKnobs()} />
-      <IconsItem name="IconSelectOpen" icon={IconSelectOpen} {...defaultKnobs()} />
-      <IconsItem name="IconSettings" icon={IconSettings} {...defaultKnobs()} />
-      <IconsItem name="IconShuffle" icon={IconShuffle} {...defaultKnobs()} />
-      <IconsItem name="IconSmile" icon={IconSmile} {...defaultKnobs()} />
-      <IconsItem name="IconSortDown" icon={IconSortDown} {...defaultKnobs()} />
-      <IconsItem name="IconSortDownCenter" icon={IconSortDownCenter} {...defaultKnobs()} />
-      <IconsItem name="IconSortUp" icon={IconSortUp} {...defaultKnobs()} />
-      <IconsItem name="IconSortUpCenter" icon={IconSortUpCenter} {...defaultKnobs()} />
-      <IconsItem name="IconStop" icon={IconStop} {...defaultKnobs()} />
-      <IconsItem name="IconStorage" icon={IconStorage} {...defaultKnobs()} />
-      <IconsItem name="IconStrikethrough" icon={IconStrikethrough} {...defaultKnobs()} />
-      <IconsItem name="IconTest" icon={IconTest} {...defaultKnobs()} />
-      <IconsItem name="IconTable" icon={IconTable} {...defaultKnobs()} />
-      <IconsItem name="IconThumbUp" icon={IconThumbUp} {...defaultKnobs()} />
-      <IconsItem name="IconTie" icon={IconTie} {...defaultKnobs()} />
-      <IconsItem name="IconTop" icon={IconTop} {...defaultKnobs()} />
-      <IconsItem name="IconTrash" icon={IconTrash} {...defaultKnobs()} />
-      <IconsItem name="IconType" icon={IconType} {...defaultKnobs()} />
-      <IconsItem name="IconUnderline" icon={IconUnderline} {...defaultKnobs()} />
-      <IconsItem name="IconUser" icon={IconUser} {...defaultKnobs()} />
-      <IconsItem name="IconWarning" icon={IconWarning} {...defaultKnobs()} />
-      <IconsItem name="IconWorld" icon={IconWorld} {...defaultKnobs()} />
+      {Object.keys(icons).map((name) => (
+        <IconsItem key={name} name={name} icon={icons[name]} {...defaultKnobs()} />
+      ))}
     </div>
   ));
