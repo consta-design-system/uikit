469014543c9551d68d28a357ef1e5f5f1bb43dec
fooontic <fooontic@gmail.com>
2020-07-17 16:55:26 +0300

    feat(Icons): add IconDisconnection and IconRestart
    closes #259

---
+++ src/icons/IconRestart/IconRestart.tsx
@@ -0,0 +1,12 @@
+import { createIcon } from '../createIcon/createIcon';
+
+import IconRestartSizeM from './IconRestart_size_m';
+import IconRestartSizeS from './IconRestart_size_s';
+import IconRestartSizeXs from './IconRestart_size_xs';
+
+export const IconRestart = createIcon({
+  m: IconRestartSizeM,
+  s: IconRestartSizeS,
+  xs: IconRestartSizeXs,
+  name: 'IconRestart',
+});
---
+++ src/icons/IconRestart/IconRestart_size_s.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+function IconRestartSizeS(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg viewBox="0 0 16 16" {...props}>
+      <path d="M3.853 2.36L2.5.5 1 6h5.5L5.03 3.978A5.002 5.002 0 0112.9 7h2.028A7 7 0 003.853 2.36zm-1.569 9.68A7 7 0 011.072 9H3.1a5.002 5.002 0 007.87 3.022L9.5 10H15l-1.5 5.5-1.353-1.86a6.998 6.998 0 01-9.864-1.6z" />
+    </svg>
+  );
+}
+
+export default IconRestartSizeS;
