feef2680cc7a560731ad2cb300b920144c9555cf
Oleg Denisov <c1n1kk@gmail.com>
2020-08-03 15:17:04 +0300

    feat(BasicSelect): add BasicSelect

---
+++ src/components/Select/components/Dropdown/Dropdown.tsx
@@ -0,0 +1,22 @@
+import './Dropdown.css';
+
+import React from 'react';
+
+import { cnSelect } from '../../cnSelect';
+
+type Props = {
+  className?: string;
+  role?: string;
+  children: React.ReactNode;
+};
+
+// eslint-disable-next-line react/display-name
+export const Dropdown = React.forwardRef<HTMLDivElement, Props>((props, ref) => {
+  const { className, children, ...restProps } = props;
+
+  return (
+    <div className={cnSelect('Dropdown', [className])} ref={ref} {...restProps}>
+      {children}
+    </div>
+  );
+});
