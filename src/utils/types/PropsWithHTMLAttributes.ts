32c5b0258b88ab21fde916e4a8f53a75f14796df
gizeasy <gizeasy@gmail.com>
2020-08-06 14:02:56 +0300

    fix(Types): emotions affect types
    issue -  #305

--- src/utils/types/PropsWithHTMLAttributes.ts
+++ src/utils/types/PropsWithHTMLAttributes.ts
@@ -1,2 +1,8 @@
 export type PropsWithHTMLAttributes<Props, HTMLElement> = Props &
   Omit<React.HTMLAttributes<HTMLElement>, keyof Props>;
+
+declare module 'react' {
+  interface DOMAttributes<T> {
+    css?: never;
+  }
+}
