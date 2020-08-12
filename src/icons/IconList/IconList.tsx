f9420ffe1aa22972f8fa69af80d034602e79fce0
gizeasy <gizeasy@gmail.com>
2020-08-12 12:41:45 +0300

    feat(Icons): automatic fill removal and loyalty to size
    * now the fill attribute is removed automatically
    * now there is no strict need to add all icon sizes
    * fix sort icons
    
    issue - #301

--- src/icons/IconList/IconList.tsx
+++ src/icons/IconList/IconList.tsx
@@ -2,11 +2,10 @@ import { createIcon } from '../createIcon/createIcon';
 
 import IconListSizeM from './IconList_size_m';
 import IconListSizeS from './IconList_size_s';
-import IconListSizeXs from './IconList_size_xs';
 
 export const IconList = createIcon({
   m: IconListSizeM,
   s: IconListSizeS,
-  xs: IconListSizeXs,
+  xs: IconListSizeS,
   name: 'IconList',
 });
--- src/icons/IconTable/IconTable.tsx
+++ src/icons/IconTable/IconTable.tsx
@@ -2,11 +2,10 @@ import { createIcon } from '../createIcon/createIcon';
 
 import IconTableSizeM from './IconTable_size_m';
 import IconTableSizeS from './IconTable_size_s';
-import IconTableSizeXs from './IconTable_size_xs';
 
 export const IconTable = createIcon({
   m: IconTableSizeM,
   s: IconTableSizeS,
-  xs: IconTableSizeXs,
+  xs: IconTableSizeS,
   name: 'IconTable',
 });
