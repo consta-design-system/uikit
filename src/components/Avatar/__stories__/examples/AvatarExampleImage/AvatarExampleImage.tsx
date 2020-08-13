e2337089f8539391d19c98749cbe18cbaccaf673
fooontic <fooontic@gmail.com>
2020-08-13 15:59:58 +0300

    docs(Avatar): fix examples

--- src/components/Avatar/__stories__/examples/AvatarExampleImage/AvatarExampleImage.tsx
+++ src/components/Avatar/__stories__/examples/AvatarExampleImage/AvatarExampleImage.tsx
@@ -2,16 +2,18 @@ import React from 'react';
 
 import { cnDocsDecorator } from '../../../../../uiKit/components/DocsDecorator/DocsDecorator';
 import { StoryBookExample } from '../../../../../uiKit/components/StoryBookExample/StoryBookExample';
+import * as wp from '../../../../../utils/whitepaper/whitepaper';
 import { Avatar } from '../../../Avatar';
 
 export const AvatarExampleImage = () => (
-  <StoryBookExample className={cnDocsDecorator('Section')}>
+  <StoryBookExample className={cnDocsDecorator('Section', [wp.decorator({ distribute: 'left' })])}>
     <Avatar
-      url="https://pbs.twimg.com/profile_images/1150453787603156992/DoiKLDMY_400x400.png"
-      name="Вадим Матвеев"
+      url="https://i.ibb.co/K2R8Lqb/Rectangle-1496.png"
+      name="Геннадий Куст"
+      className={wp.decorator({ 'indent-r': 'm' })}
     />
-    <Avatar name="Вадим Матвеев" />
-    <Avatar name="Матвеев Вадим Александрович" />
+    <Avatar name="Геннадий Куст" className={wp.decorator({ 'indent-r': 'm' })} />
+    <Avatar name="Куст Геннадий Альбертович" className={wp.decorator({ 'indent-r': 'm' })} />
     <Avatar />
   </StoryBookExample>
 );
