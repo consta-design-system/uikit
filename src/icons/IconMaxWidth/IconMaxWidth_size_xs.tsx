7c26666b63afbe566854d1355d45a8444b886e81
Vadik Matveev <fooontic@gmail.com>
2020-08-13 16:25:04 +0300

    feat(Icons): add new icons MaxWidth and Phone (#332)
    issue - #315 #331

---
+++ src/icons/IconMaxWidth/IconMaxWidth_size_xs.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+function IconMaxWidthSizeXs(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg viewBox="0 0 12 12" {...props}>
+      <path d="M0 2h1v3.5l2-2v2h6v-2l2 2V2h1v8h-1V6.5l-2 2v-2H3v2l-2-2V10H0V2z" />
+    </svg>
+  );
+}
+
+export default IconMaxWidthSizeXs;
---
+++ src/icons/IconPhone/IconPhone_size_m.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+function IconPhoneSizeM(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg viewBox="0 0 24 24" {...props}>
+      <path d="M13.344 17.978a16.832 16.832 0 01-7.322-7.322l2.752-2.052a1 1 0 00.375-1.032L8.015 2.77A1 1 0 007.042 2h-3.93C2.5 2 2 2.5 2 3.111 2 13.544 10.456 22 20.889 22 21.5 22 22 21.5 22 20.889v-3.932a1 1 0 00-.768-.973L16.43 14.84a1 1 0 00-1.034.377l-2.052 2.76z" />
+    </svg>
+  );
+}
+
+export default IconPhoneSizeM;
