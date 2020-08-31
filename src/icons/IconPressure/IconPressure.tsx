10f8d443de6ac527eb5cb4bbdcd7274311a0be61
gizeasy <gizeasy@gmail.com>
2020-08-31 12:11:02 +0300

    feat(Icons): add new icons (#370)
    * IconDrillingRig
    * IconMBU
    * IconMGRP
    * IconMLSP
    * IconMMP
    * IconMolecules
    * IconPressure
    * IconRUO
    * IconRUS
    * IconVZD
    
    issue -  #354

---
+++ src/icons/IconPressure/IconPressure.tsx
@@ -0,0 +1,10 @@
+import { createIcon } from '../createIcon/createIcon';
+
+import IconPressureSizeM from './IconPressure_size_m';
+
+export const IconPressure = createIcon({
+  m: IconPressureSizeM,
+  s: IconPressureSizeM,
+  xs: IconPressureSizeM,
+  name: 'IconPressure',
+});
---
+++ src/icons/IconRUS/IconRUS.tsx
@@ -0,0 +1,10 @@
+import { createIcon } from '../createIcon/createIcon';
+
+import IconRUSSizeM from './IconRUS_size_m';
+
+export const IconRUS = createIcon({
+  m: IconRUSSizeM,
+  s: IconRUSSizeM,
+  xs: IconRUSSizeM,
+  name: 'IconRUS',
+});
