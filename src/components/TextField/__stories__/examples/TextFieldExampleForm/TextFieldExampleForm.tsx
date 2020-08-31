1b71c59703433cbb8d0373a88ec15a37b418924e
Anya Ovsyannikova <arhayka@gmail.com>
2020-08-31 12:06:32 +0300

    docs(TextField): edit examples

--- src/components/TextField/__stories__/examples/TextFieldExampleForm/TextFieldExampleForm.tsx
+++ src/components/TextField/__stories__/examples/TextFieldExampleForm/TextFieldExampleForm.tsx
@@ -1,34 +1,26 @@
 import React from 'react';
 
 import { cnDocsDecorator } from '../../../../../uiKit/components/DocsDecorator/DocsDecorator';
-import { cnDocsExample } from '../../../../../uiKit/components/DocsExample/DocsExample';
+import { StoryBookExample } from '../../../../../uiKit/components/StoryBookExample/StoryBookExample';
 import * as wp from '../../../../../utils/whitepaper/whitepaper';
 import { Button } from '../../../../Button/Button';
 import { TextField } from '../../../TextField';
 
-export function TextFieldExampleFormBasic() {
-  return (
-    <div className={cnDocsDecorator('Section', [wp.decorator({ distribute: 'left' })])}>
-      <div className={cnDocsExample(null, [wp.decorator({ 'indent-r': 's' })])}>
-        <TextField placeholder="Default" />
-      </div>
-      <div className={cnDocsExample(null, [wp.decorator({ 'indent-r': 's' })])}>
-        <TextField form="brick" placeholder="Brick" />
-      </div>
-      <div className={cnDocsExample()}>
-        <TextField form="round" placeholder="Round" />
-      </div>
-    </div>
-  );
-}
+export const TextFieldExampleFormBasic = () => (
+  <StoryBookExample className={cnDocsDecorator('Section')}>
+    <TextField placeholder="Default" />
+    <TextField form="brick" placeholder="Brick" />
+    <TextField form="round" placeholder="Round" />
+  </StoryBookExample>
+);
 
 export function TextFieldExampleFormHybrid() {
   return (
     <div className={cnDocsDecorator('Section', [wp.decorator({ distribute: 'left' })])}>
-      <div className={cnDocsExample()}>
+      <div>
         <TextField placeholder="Электронная почта" form="roundClear" style={{ width: '260px' }} />
       </div>
-      <div className={cnDocsExample()}>
+      <div>
         <Button form="brickRound" label="Подписаться" />
       </div>
     </div>
