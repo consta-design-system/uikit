37c9ca9f48ef7e4eaba4759be89396b50bead57e
arhayka <arhayka@gmail.com>
2020-09-01 19:49:54 +0300

    docs(FileField): edit docs for FileField (#364)
    Co-authored-by: gizeasy <gizeasy@gmail.com>

--- src/components/FileField/__stories__/examples/FileFieldExampleWithoutRenderProp/FileFieldExampleWithoutRenderProp.tsx
+++ src/components/FileField/__stories__/examples/FileFieldExampleWithoutRenderProp/FileFieldExampleWithoutRenderProp.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 
 import { cnDocsDecorator } from '../../../../../uiKit/components/DocsDecorator/DocsDecorator';
+import { StoryBookExample } from '../../../../../uiKit/components/StoryBookExample/StoryBookExample';
 import { Text } from '../../../../Text/Text';
 import { FileField } from '../../../FileField';
 
 export function FileFieldExampleWithoutRenderProp() {
   return (
-    <div className={cnDocsDecorator('Section')}>
+    <StoryBookExample className={cnDocsDecorator('Section')}>
       <FileField id="FileFieldExampleWithoutRenderProp">
         <Text>Нажми на меня</Text>
       </FileField>
-    </div>
+    </StoryBookExample>
   );
 }
