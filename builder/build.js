196fedb20ab08aee20299b099aeb539ad613ef92
gizeasy <gizeasy@gmail.com>
2020-09-11 16:22:57 +0300

    chore(builder): add README.md, CHANGELOG.md to build
    add README.md, CHANGELOG.md to build. Add keywords to package.json
    
    issue -  #276

--- builder/build.js
+++ builder/build.js
@@ -5,7 +5,14 @@ const { Command, flags } = require('@oclif/command');
 const logSymbols = require('log-symbols');
 const { remove, ensureDir } = require('fs-extra');
 
-const { transformCSS, generateReExports, copyAssets, copyPackageJson } = require('./helpers');
+const {
+  transformCSS,
+  generateReExports,
+  copyAssets,
+  copyPackageJson,
+  copyReadme,
+  copyChangelog,
+} = require('./helpers');
 
 const execAsync = promisify(exec);
 const INTERNAL_PREFIX = '__internal__';
@@ -85,6 +92,8 @@ class GenerateCommand extends Command {
 
     try {
       await copyPackageJson(distPath);
+      await copyReadme(distPath);
+      await copyChangelog(distPath);
       await Promise.all([
         transformCSS(ignore, srcPath, [distSrc, distEsSrc], {
           namespace,
