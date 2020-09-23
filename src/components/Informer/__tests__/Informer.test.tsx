1066a1963ae1458b46f588ab4f4c5784a9587e16
gizeasy <gizeasy@gmail.com>
2020-09-23 13:56:57 +0300

    test(Informer): add test
    issue -  #410

---
+++ src/components/Informer/__tests__/Informer.test.tsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+
+import { IconLeaf } from '../../../icons/IconLeaf/IconLeaf';
+import { cnInformer, Informer, informerPropView } from '../Informer';
+
+type InformerProps = React.ComponentProps<typeof Informer>;
+
+const testId = cnInformer();
+
+const renderComponent = (props: InformerProps = {}) => {
+  return render(<Informer data-testid={testId} {...props} />);
+};
+
+function getRender() {
+  return screen.getByTestId(testId);
+}
+
+function getIcon() {
+  return getRender().querySelector(`.${cnInformer('Icon')}`);
+}
+
+describe('Компонент Informer', () => {
+  it('должен рендериться без ошибок', () => {
+    expect(renderComponent).not.toThrow();
+  });
+  describe('проверка props', () => {
+    describe('проверка label', () => {
+      it(`label отображается`, () => {
+        const label = 'Label';
+
+        renderComponent({ label });
+
+        expect(getRender().textContent).toEqual(label);
+      });
+    });
+    describe('проверка Title', () => {
+      it(`Title отображается`, () => {
+        const title = 'Title';
+
+        renderComponent({ title });
+
+        expect(getRender().textContent).toEqual(title);
+      });
+    });
+    describe('проверка icon', () => {
+      it(`иконка отображается`, () => {
+        renderComponent({ icon: IconLeaf });
+
+        expect(getIcon()).toHaveClass('IconLeaf');
+      });
+      it(`присвоился модификатор withIcon `, () => {
+        renderComponent({ icon: IconLeaf });
+
+        expect(getRender()).toHaveClass(cnInformer({ withIcon: true }));
+      });
+    });
+    describe('проверка view', () => {
+      informerPropView.forEach((view) => {
+        it(`присваивает класс для view=${view}`, () => {
+          renderComponent({ view });
+
+          expect(getRender()).toHaveClass(cnInformer({ view }));
+        });
+      });
+    });
+  });
+});
