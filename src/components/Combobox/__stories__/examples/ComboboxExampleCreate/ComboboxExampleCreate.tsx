730ba2dc517f911e9141c8104100e9b060a19d73
Anya Ovsyannikova <arhayka@gmail.com>
2020-09-29 11:42:05 +0300

    docs(Combobox): add docs and examples for Combobox

---
+++ src/components/Combobox/__stories__/examples/ComboboxExampleCreate/ComboboxExampleCreate.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+import { cnDocsDecorator } from '../../../../../uiKit/components/DocsDecorator/DocsDecorator';
+import { StoryBookExample } from '../../../../../uiKit/components/StoryBookExample/StoryBookExample';
+import { Combobox } from '../../../Combobox';
+
+type SelectOption = {
+  label: string;
+  value: string;
+};
+
+const items = [
+  { label: 'Синий', value: 'blue' },
+  { label: 'Белый', value: 'white' },
+  { label: 'Красный', value: 'red' },
+  { label: 'Черный', value: 'black' },
+  { label: 'Зеленый', value: 'green' },
+  { label: 'В полосочку', value: 'stripe' },
+];
+
+let options = items;
+
+const handleCreate = (v: string): void => {
+  options = [{ label: v, value: v }, ...options];
+};
+
+export const ComboboxExampleCreate = () => {
+  const getItemLabel = (option: SimpleOption): string => option.label;
+  return (
+    <StoryBookExample className={cnDocsDecorator('Section')}>
+      <Combobox
+        id="color"
+        options={items}
+        getOptionLabel={getItemLabel}
+        onCreate={handleCreate}
+        placeholder="Выберите цвет"
+      />
+    </StoryBookExample>
+  );
+};
---
+++ src/components/Combobox/__stories__/examples/ComboboxExampleForm/ComboboxExampleForm.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+
+import { cnDocsDecorator } from '../../../../../uiKit/components/DocsDecorator/DocsDecorator';
+import { StoryBookExample } from '../../../../../uiKit/components/StoryBookExample/StoryBookExample';
+import { Combobox } from '../../../Combobox';
+
+type SelectOption = {
+  label: string;
+  value: string;
+};
+
+const items = [
+  { label: 'Синий', value: 'blue' },
+  { label: 'Белый', value: 'white' },
+  { label: 'Красный', value: 'red' },
+  { label: 'Черный', value: 'black' },
+  { label: 'Зеленый', value: 'green' },
+  { label: 'В полосочку', value: 'stripe' },
+];
+
+export const ComboboxExampleForm = () => {
+  const getItemLabel = (option: SelectOption): string => option.label;
+  return (
+    <StoryBookExample className={cnDocsDecorator('Section')}>
+      <Combobox
+        id="color"
+        options={items}
+        getOptionLabel={getItemLabel}
+        placeholder="Default"
+        form="default"
+      />
+      <Combobox
+        id="color"
+        options={items}
+        getOptionLabel={getItemLabel}
+        placeholder="Brick"
+        form="brick"
+      />
+      <Combobox
+        id="color"
+        options={items}
+        getOptionLabel={getItemLabel}
+        placeholder="Round"
+        form="round"
+      />
+    </StoryBookExample>
+  );
+};
