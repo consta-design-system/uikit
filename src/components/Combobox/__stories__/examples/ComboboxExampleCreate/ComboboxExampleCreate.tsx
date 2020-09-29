ff2e9ec6177776c95143ca7becad998e0df92615
gizeasy <gizeasy@gmail.com>
2020-09-29 14:03:02 +0300

    docs(Combobox): add examples

---
+++ src/components/Combobox/__stories__/examples/ComboboxExampleCreate/ComboboxExampleCreate.tsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+
+import { cnDocsDecorator } from '../../../../../uiKit/components/DocsDecorator/DocsDecorator';
+import { StoryBookExample } from '../../../../../uiKit/components/StoryBookExample/StoryBookExample';
+import { Combobox } from '../../../Combobox';
+
+type SelectOption = {
+  label: string;
+  value: string;
+};
+
+const items: SelectOption[] = [
+  { label: 'Синий', value: 'blue' },
+  { label: 'Белый', value: 'white' },
+  { label: 'Красный', value: 'red' },
+  { label: 'Черный', value: 'black' },
+  { label: 'Зеленый', value: 'green' },
+  { label: 'В полосочку', value: 'stripe' },
+];
+
+export const ComboboxExampleCreate = () => {
+  const [options, setOptions] = useState<SelectOption[]>(items);
+
+  const handleCreate = (label: string): void => {
+    setOptions([{ label, value: label }, ...items]);
+  };
+
+  const getItemLabel = (option: SelectOption): string => option.label;
+  return (
+    <StoryBookExample className={cnDocsDecorator('Section')}>
+      <Combobox
+        id="color"
+        options={options}
+        getOptionLabel={getItemLabel}
+        onCreate={handleCreate}
+        placeholder="Выберите цвет"
+      />
+    </StoryBookExample>
+  );
+};
