6c1a19ed8e8e6da52c5073174392e6d20fd47587
arhayka <arhayka@gmail.com>
2020-09-30 14:43:49 +0300

    docs(Combobox): add docs (#467)
    Co-authored-by: gizeasy <gizeasy@gmail.com>

---
+++ src/components/Combobox/__stories__/examples/ComboboxExampleText/ComboboxExampleText.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import { cnDocsDecorator } from '../../../../../uiKit/components/DocsDecorator/DocsDecorator';
+import { StoryBookExample } from '../../../../../uiKit/components/StoryBookExample/StoryBookExample';
+import { Combobox } from '../../../Combobox';
+
+type SelectOption = {
+  label: string;
+  value: string;
+};
+
+const items = [
+  { label: 'Синий', value: 'blue' },
+  { label: 'Белый', value: 'white' },
+  { label: 'Красный', value: 'red' },
+  { label: 'Черный', value: 'black' },
+  { label: 'Зеленый', value: 'green' },
+  { label: 'В полосочку', value: 'stripe' },
+];
+
+export const ComboboxExampleText = () => {
+  const getItemLabel = (option: SelectOption): string => option.label;
+  return (
+    <StoryBookExample className={cnDocsDecorator('Section')}>
+      <p>Цвет фона</p>
+      <Combobox
+        id="colors"
+        options={items}
+        getOptionLabel={getItemLabel}
+        placeholder="Выберите"
+        size="s"
+      />
+    </StoryBookExample>
+  );
+};
