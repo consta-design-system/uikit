29fd8da482dc7ecb526c575b17272248f53f8b07
Shupirlik <shupirlik@gmail.com>
2020-10-07 18:29:40 +0300

    feat(Icons): add IconTrash-xs
    closes #497

---
+++ src/icons/IconTrash/IconTrash_size_xs.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+
+function IconTrashSizeXs(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg viewBox="0 0 12 12" {...props}>
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M4.5 2a1 1 0 011-1h1a1 1 0 011 1h3v1h-1v7a1 1 0 01-1 1h-5a1 1 0 01-1-1V3h-1V2h3zm1 2h-1v5h1V4zm2 0h-1v5h1V4z"
+      />
+    </svg>
+  );
+}
+
+export default IconTrashSizeXs;
