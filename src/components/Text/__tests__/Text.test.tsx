ba6e8eca4b16e97fecef84a7c2de52bc1660641e
gizeasy <gizeasy@gmail.com>
2020-10-09 14:51:21 +0300

    test(Text): added tests
    issue -  #416

---
+++ src/components/Text/__tests__/Text.test.tsx
@@ -0,0 +1,32 @@
+import * as React from 'react';
+import { render, screen } from '@testing-library/react';
+
+import { cnText, Text } from '../Text';
+
+type TagProps = React.ComponentProps<typeof Text>;
+
+const testId = cnText();
+
+const renderComponent = (props: TagProps) => {
+  return render(<Text data-testid={testId} {...props} />);
+};
+
+describe('Компонент Текст', () => {
+  it('должен рендериться без ошибок', () => {
+    expect(renderComponent).not.toThrow();
+  });
+
+  describe('проверка props', () => {
+    describe('проверка className', () => {
+      const className = 'className';
+
+      it(`присваивает className`, () => {
+        renderComponent({ className });
+
+        const tagBase = screen.getByTestId(testId);
+
+        expect(tagBase).toHaveClass(className);
+      });
+    });
+  });
+});
