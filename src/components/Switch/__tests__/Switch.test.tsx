938ad3e6854f84d9adea5b02327415b89b86bf84
gizeasy <gizeasy@gmail.com>
2020-10-12 12:55:11 +0300

    test(Switch): added tests (#508)
    issue -  #415

---
+++ src/components/Switch/__tests__/Switch.test.tsx
@@ -0,0 +1,70 @@
+import * as React from 'react';
+import { fireEvent, render, screen } from '@testing-library/react';
+
+import { cnSwitch, Switch } from '../Switch';
+
+type SwitchProps = React.ComponentProps<typeof Switch>;
+
+const testId = cnSwitch();
+
+const defaultHandleChange = jest.fn();
+
+const renderComponent = ({
+  onChange = defaultHandleChange,
+  checked = false,
+  ...props
+}: Omit<SwitchProps, 'onChange' | 'checked'> & {
+  onChange?: SwitchProps['onChange'];
+  checked?: SwitchProps['checked'];
+}) => {
+  return render(<Switch data-testid={testId} onChange={onChange} checked={checked} {...props} />);
+};
+
+function getRender() {
+  return screen.getByTestId(testId);
+}
+
+function getLabel() {
+  return getRender().querySelector(`.${cnSwitch('Label')}`);
+}
+
+describe('Компонент Switch', () => {
+  it('должен рендериться без ошибок', () => {
+    expect(() => renderComponent({})).not.toThrow();
+  });
+  describe('проверка props', () => {
+    describe('проверка className', () => {
+      it(`Дополнительный className присваевается`, () => {
+        const className = 'className';
+
+        renderComponent({ className });
+        expect(getRender()).toHaveClass(className);
+      });
+    });
+    describe('проверка label', () => {
+      it(`label отображается`, () => {
+        const label = 'label';
+
+        renderComponent({ label });
+
+        const labelElement = getLabel() as HTMLSpanElement;
+
+        expect(labelElement.textContent).toEqual(label);
+      });
+    });
+    describe('проверка onChandge', () => {
+      it(`клик должен вызвать callback c ожидаемыми параметрами`, () => {
+        const handleChange = jest.fn();
+
+        renderComponent({ onChange: handleChange });
+
+        const element = getRender() as HTMLButtonElement;
+
+        fireEvent.click(element);
+        expect(handleChange).toHaveBeenCalled();
+        expect(handleChange).toHaveBeenCalledTimes(1);
+        expect(handleChange).toHaveBeenCalledWith(expect.objectContaining({ checked: true }));
+      });
+    });
+  });
+});
