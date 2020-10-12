5c59dda8ecb7f4c010fb56c6e6dbac8b5d62e507
Shupirlik <shupirlik@gmail.com>
2020-10-12 12:58:00 +0300

    feat(Icons): add IconTrash-xs (#504)
    issue #497

--- src/icons/IconTrash/IconTrash.tsx
+++ src/icons/IconTrash/IconTrash.tsx
@@ -2,10 +2,11 @@ import { createIcon } from '../createIcon/createIcon';
 
 import IconTrashSizeM from './IconTrash_size_m';
 import IconTrashSizeS from './IconTrash_size_s';
+import IconTrashSizeXs from './IconTrash_size_xs';
 
 export const IconTrash = createIcon({
   m: IconTrashSizeM,
   s: IconTrashSizeS,
-  xs: IconTrashSizeS,
+  xs: IconTrashSizeXs,
   name: 'IconTrash',
 });
