4cd30cd77bfa04bdd5538a21d20bc1450c276179
gizeasy <gizeasy@gmail.com>
2020-10-12 17:59:39 +0300

    test(Theme): added tests (#487)
    issue - #418

---
+++ src/components/Theme/__tests__/Theme.test.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+
+import {
+  cnTheme,
+  generateThemeClassNames,
+  presetGpnDark,
+  presetGpnDefault,
+  Theme,
+  useTheme,
+} from '../Theme';
+
+type ThemeProps = React.ComponentProps<typeof Theme>;
+
+const testId = cnTheme();
+
+function getRender() {
+  return screen.getByTestId(testId);
+}
+
+const renderComponent = (props: Omit<ThemeProps, 'preset'> & { preset?: ThemeProps['preset'] }) => {
+  return render(
+    <Theme {...props} preset={props.preset || presetGpnDefault} data-testid={testId} />,
+  );
+};
+
+describe('Компонент Theme', () => {
+  it('должен рендериться без ошибок', () => {
+    expect(() => renderComponent({})).not.toThrow();
+  });
+
+  describe('проверка useTheme', () => {
+    it('возвращает ожидаемый theme', () => {
+      const Children = () => {
+        const { theme } = useTheme();
+        return <>{JSON.stringify(theme)}</>;
+      };
+      renderComponent({ children: <Children />, preset: presetGpnDark });
+      expect(getRender()).toHaveTextContent(JSON.stringify(presetGpnDark));
+    });
+    it('возвращает ожидаемый themeClassNames', () => {
+      const Children = () => {
+        const { themeClassNames } = useTheme();
+        return <>{JSON.stringify(themeClassNames)}</>;
+      };
+      renderComponent({ children: <Children />, preset: presetGpnDark });
+      expect(getRender()).toHaveTextContent(JSON.stringify(generateThemeClassNames(presetGpnDark)));
+    });
+  });
+});
