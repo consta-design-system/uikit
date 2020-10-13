533c55cee23175d570794c1ae318b1f7ca144318
Shupirlik <shupirlik@gmail.com>
2020-10-13 15:52:48 +0300

    feat(Icons): add IconWatch (#503)
    issue - #478

---
+++ src/icons/IconWatch/IconWatch_size_s.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+
+function IconWatchSizeS(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg viewBox="0 0 16 16" {...props}>
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M8 16A8 8 0 108 0a8 8 0 000 16zm1-8.512V2.002H7V8.53l4.437 3.106 1.147-1.639L9 7.488z"
+      />
+    </svg>
+  );
+}
+
+export default IconWatchSizeS;
