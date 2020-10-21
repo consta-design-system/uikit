d8dc744f9cda18922071c8e56de632fa83cf7e9c
Anastasia Dekhtyar <44090737+andekhtyar@users.noreply.github.com>
2020-10-21 11:18:21 +0300

    feat(iconwidescreen): added icons (#542)

---
+++ src/icons/IconWideScreen/IconWideScreen_size_xs.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+
+function IconWideScreenSizeXs(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg viewBox="0 0 12 12" {...props}>
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M2 2a1 1 0 00-1 1v4a1 1 0 001 1h3v1H4v1h4V9H7V8h3a1 1 0 001-1V3a1 1 0 00-1-1H2zm8 1H2v4h8V3z"
+      />
+    </svg>
+  );
+}
+
+export default IconWideScreenSizeXs;
