fclmman/consta-uikit

feat(DatePicker): add DatePicker

src/components/DatePicker/Calendar/__tests__/index.test.tsx
@@ -1,0 +1,201 @@
+import { DateRange } from '../../DatePicker';
+import { isDateHighlighted, isValueSelected, isValueSelectedBackwards } from '../Calendar';
+
+describe('isValueSelected', () => {
+  describe('переданное значение - одна дата', () => {
+    it('возвращает true, если значение календарно совпадает с переданной датой', () => {
+      expect(
+        isValueSelected({
+          date: new Date(2019, 0, 2),
+          value: new Date(2019, 0, 2),
+        }),
+      ).toBeTruthy();
+    });
+
+    it('возвращет false, если значение календарно не совпадает с переданной датой', () => {
+      expect(
+        isValueSelected({
+          date: new Date(2019, 0, 2),
+          value: new Date(2019, 0, 3),
+        }),
+      ).toBeFalsy();
+    });
+
+    it('возвращет false, если значение не задано', () => {
+      expect(
+        isValueSelected({
+          date: new Date(2019, 0, 2),
+          value: undefined,
+        }),
+      ).toBeFalsy();
+    });
+  });
+
+  describe('переданное значение - диапазон дат', () => {
+    const range: DateRange = [new Date(2019, 0, 10), new Date(2019, 0, 12)];
+
+    it('возвращает true, если значение представлено полностью и дата календарно в пределах этого значения', () => {
+      expect(isValueSelected({ date: new Date(2019, 0, 11), value: range })).toBeTruthy();
+    });
+
+    it('возвращает true, если первая дата значения позже второй', () => {
+      expect(
+        isValueSelected({
+          date: new Date(2019, 0, 11),
+          value: [range[1], range[0]],
+        }),
+      ).toBeTruthy();
+    });
+
+    it('возвращает true, если значение представлено полностью и дата календарно на границах этого значения', () => {
+      expect(isValueSelected({ date: range[0]!, value: range })).toBeTruthy();
+      expect(isValueSelected({ date: range[1]!, value: range })).toBeTruthy();
+    });
+
+    it('возвращает false, если значение представлено полностью и дата календарно за пределами этого значения', () => {
+      expect(isValueSelected({ date: new Date(2019, 0, 13), value: range })).toBeFalsy();
+    });
+
+    it('возвращает true, если значение представлено одной датой и дата календарно совпадает с ней', () => {
+      expect(isValueSelected({ date: range[0]!, value: [range[0], undefined] })).toBeTruthy();
+      expect(isValueSelected({ date: range[1]!, value: [undefined, range[1]] })).toBeTruthy();
+    });
+
+    it('возвращает false, если значение представлено одной датой и дата календарно не совпадает с ней', () => {
+      expect(isValueSelected({ date: range[1]!, value: [range[0], undefined] })).toBeFalsy();
+      expect(isValueSelected({ date: range[0]!, value: [undefined, range[1]] })).toBeFalsy();
+    });
+
+    it('возвращет false, если значение не задано', () => {
+      expect(isValueSelected({ date: new Date(2019, 0, 10), value: undefined })).toBeFalsy();
+      expect(
+        isValueSelected({ date: new Date(2019, 0, 10), value: [undefined, undefined] }),
+      ).toBeFalsy();
+    });
+  });
+});
+
+describe('isDateHighlighted', () => {
+  it('возвращает false, если не передана наведенная дата', () => {
+    expect(isDateHighlighted({ date: new Date(), value: [new Date(), new Date()] })).toBeFalsy();
+  });
+
+  it('возвращает false, если value не является интервалом', () => {
+    expect(
+      isDateHighlighted({
+        date: new Date(2015, 1, 1),
+        hoveredDate: new Date(2014, 1, 1),
+        value: new Date(2018, 1, 1),
+      }),
+    ).toBeFalsy();
+  });
+
+  it('возвращает false, если в переданном интервале не одна дата', () => {
+    expect(
+      isDateHighlighted({
+        date: new Date(2015, 1, 1),
+        hoveredDate: new Date(2014, 1, 1),
+        value: [new Date(2018, 1, 1), new Date(2020, 1, 1)],
+      }),
+    ).toBeFalsy();
+  });
+
+  it('возвращает false, если переданная дата не входит в интервал [наведенная дата, дата из переданного интервала]', () => {
+    expect(
+      isDateHighlighted({
+        date: new Date(2012, 1, 1),
+        hoveredDate: new Date(2014, 1, 1),
+        value: [new Date(2018, 1, 1), undefined],
+      }),
+    ).toBeFalsy();
+  });
+
+  it('возвращает false, если переданная дата не входит в интервал [дата из переданного интервала, наведенная дата]', () => {
+    expect(
+      isDateHighlighted({
+        date: new Date(2012, 1, 1),
+        hoveredDate: new Date(2018, 1, 1),
+        value: [undefined, new Date(2014, 1, 1)],
+      }),
+    ).toBeFalsy();
+  });
+
+  it('возвращает true, если переданная дата входит в интервал [наведенная дата, дата из переданного интервала]', () => {
+    expect(
+      isDateHighlighted({
+        date: new Date(2015, 1, 1),
+        hoveredDate: new Date(2014, 1, 1),
+        value: [new Date(2018, 1, 1), undefined],
+      }),
+    ).toBeTruthy();
+  });
+
+  it('возвращает true, если переданная дата входит в интервал [дата из переданного интервала, наведенная дата]', () => {
+    expect(
+      isDateHighlighted({
+        date: new Date(2015, 1, 1),
+        hoveredDate: new Date(2018, 1, 1),
+        value: [undefined, new Date(2014, 1, 1)],
+      }),
+    ).toBeTruthy();
+  });
+});
+
+describe('isValueSelectedBackwards', () => {
+  const date = new Date(2019, 0, 10);
+  const range: DateRange = [new Date(2019, 0, 10), new Date(2019, 0, 12)];
+
+  it('возвращает false, если не задана выбранная дата', () => {
+    expect(isValueSelectedBackwards({ value: range })).toBeFalsy();
+  });
+
+  it('возвращает false, если не задан интервал', () => {
+    expect(isValueSelectedBackwards({ hoveredDate: date })).toBeFalsy();
+  });
+
+  it('возвращает false, если переданное значение не является интервалом', () => {
+    expect(
+      isValueSelectedBackwards({ hoveredDate: date, value: new Date(2017, 0, 2) }),
+    ).toBeFalsy();
+  });
+
+  it('возвращает false, если в переданном интервале не одна дата', () => {
+    expect(isValueSelectedBackwards({ hoveredDate: date })).toBeFalsy();
+    expect(
+      isValueSelectedBackwards({ hoveredDate: date, value: [undefined, undefined] }),
+    ).toBeFalsy();
+    expect(isValueSelectedBackwards({ hoveredDate: date, value: range })).toBeFalsy();
+  });
+
+  it('возвращает false если переданная дата позже даты из интервала', () => {
+    expect(
+      isValueSelectedBackwards({
+        hoveredDate: new Date(2017, 4, 1),
+        value: [new Date(2017, 2, 1), undefined],
+      }),
+    ).toBeFalsy();
+
+    expect(
+      isValueSelectedBackwards({
+        hoveredDate: new Date(2017, 4, 1),
+        value: [undefined, new Date(2017, 2, 1)],
+      }),
+    ).toBeFalsy();
+  });
+
+  it('возвращает true если переданная дата раньше даты из интервала', () => {
+    expect(
+      isValueSelectedBackwards({
+        hoveredDate: new Date(2016, 4, 1),
+        value: [new Date(2017, 2, 1), undefined],
+      }),
+    ).toBeTruthy();
+
+    expect(
+      isValueSelectedBackwards({
+        hoveredDate: new Date(2016, 4, 1),
+        value: [undefined, new Date(2017, 2, 1)],
+      }),
+    ).toBeTruthy();
+  });
+});

src/components/DatePicker/__stories__/DatePicker.stories.tsx
@@ -1,0 +1,120 @@
+import React from 'react';
+import { date as knobsDate, select } from '@storybook/addon-knobs';
+import { addMonths, isValid, subYears } from 'date-fns';
+
+import { createMetadata, createStory } from '../../../utils/storybook';
+import { DatePicker, DateRange, sizes } from '../DatePicker';
+import { getDateMidnightFromString, getInputValue } from '../InputDate/helpers';
+
+const setInputValue = (value?: string) => {
+  if (!value) {
+    return undefined;
+  }
+
+  const date = getDateMidnightFromString(value);
+
+  return isValid(date) ? date : undefined;
+};
+
+const defaultProps = () =>
+  ({
+    minDate: new Date(knobsDate('minDate', subYears(new Date(), 1))),
+    maxDate: new Date(knobsDate('maxDate', addMonths(new Date(), 1))),
+    size: select('size', sizes, sizes[1]),
+  } as const);
+
+const DakePickerSingleStoryContent = () => {
+  const [date, setDate] = React.useState<Date | undefined>();
+
+  return <DatePicker type="date" value={date} onChange={setDate} {...defaultProps()} />;
+};
+
+export const DakePickerSingleStory = createStory(() => <DakePickerSingleStoryContent />, {
+  name: 'Выбор даты',
+});
+
+const DatePickerThirdPartyInputStoryContent = () => {
+  const [date, setDate] = React.useState<Date | undefined>(new Date(2019, 3, 1));
+
+  return (
+    <DatePicker
+      type="date"
+      value={date}
+      onChange={setDate}
+      {...defaultProps()}
+      renderControls={({ size, value, onChange }) => {
+        return (
+          <input
+            type="date"
+            value={getInputValue(value)}
+            onChange={(event) => {
+              onChange(setInputValue(event.target.value));
+            }}
+            className={size}
+          />
+        );
+      }}
+    />
+  );
+};
+
+export const DatePickerThirdPartyInputStory = createStory(
+  () => <DatePickerThirdPartyInputStoryContent />,
+  { name: 'Выбор даты с сторонним инпутом' },
+);
+
+const DatePickerRangeStoryContent = () => {
+  const [range, setRange] = React.useState<DateRange | undefined>([undefined, undefined]);
+
+  return <DatePicker type="date-range" value={range} onChange={setRange} {...defaultProps()} />;
+};
+
+export const DatePickerRangeStory = createStory(() => <DatePickerRangeStoryContent />, {
+  name: 'Выбор диапазона дат',
+});
+
+const DatePickerRangeThirdPartyInputStoryContent = () => {
+  const [range, setRange] = React.useState<DateRange | undefined>([undefined, undefined]);
+
+  return (
+    <DatePicker
+      type="date-range"
+      value={range}
+      onChange={setRange}
+      {...defaultProps()}
+      renderControls={({ size, value, onChange }) => {
+        return (
+          <>
+            <input
+              type="date"
+              value={value && getInputValue(value[0])}
+              onChange={(event) => {
+                onChange([setInputValue(event.target.value), (value && value[1]) || undefined]);
+              }}
+              className={size}
+            />
+            <input
+              type="date"
+              value={value && getInputValue(value[1])}
+              onChange={(event) => {
+                onChange([(value && value[0]) || undefined, setInputValue(event.target.value)]);
+              }}
+              className={size}
+            />
+          </>
+        );
+      }}
+    />
+  );
+};
+
+export const DatePickerRangeThirdPartyInputStory = createStory(
+  () => <DatePickerRangeThirdPartyInputStoryContent />,
+  {
+    name: 'Выбор диапазона дат с сторонними инпутами',
+  },
+);
+
+export default createMetadata({
+  title: 'Components|/DatePicker',
+});

consta-design-system/uikit

src/components/DatePicker/DatePickerCalendar/__tests__/index.test.tsx
@@ -1,0 +1,205 @@
+import { DateRange } from '../../types';
+import {
+  isDateHighlighted,
+  isValueSelected,
+  isValueSelectedBackwards,
+} from '../DatePickerCalendar';
+
+describe('isValueSelected', () => {
+  describe('переданное значение - одна дата', () => {
+    it('возвращает true, если значение календарно совпадает с переданной датой', () => {
+      expect(
+        isValueSelected({
+          date: new Date(2019, 0, 2),
+          value: new Date(2019, 0, 2),
+        }),
+      ).toBeTruthy();
+    });
+
+    it('возвращет false, если значение календарно не совпадает с переданной датой', () => {
+      expect(
+        isValueSelected({
+          date: new Date(2019, 0, 2),
+          value: new Date(2019, 0, 3),
+        }),
+      ).toBeFalsy();
+    });
+
+    it('возвращет false, если значение не задано', () => {
+      expect(
+        isValueSelected({
+          date: new Date(2019, 0, 2),
+          value: undefined,
+        }),
+      ).toBeFalsy();
+    });
+  });
+
+  describe('переданное значение - диапазон дат', () => {
+    const range: DateRange = [new Date(2019, 0, 10), new Date(2019, 0, 12)];
+
+    it('возвращает true, если значение представлено полностью и дата календарно в пределах этого значения', () => {
+      expect(isValueSelected({ date: new Date(2019, 0, 11), value: range })).toBeTruthy();
+    });
+
+    it('возвращает true, если первая дата значения позже второй', () => {
+      expect(
+        isValueSelected({
+          date: new Date(2019, 0, 11),
+          value: [range[1], range[0]],
+        }),
+      ).toBeTruthy();
+    });
+
+    it('возвращает true, если значение представлено полностью и дата календарно на границах этого значения', () => {
+      expect(isValueSelected({ date: range[0]!, value: range })).toBeTruthy();
+      expect(isValueSelected({ date: range[1]!, value: range })).toBeTruthy();
+    });
+
+    it('возвращает false, если значение представлено полностью и дата календарно за пределами этого значения', () => {
+      expect(isValueSelected({ date: new Date(2019, 0, 13), value: range })).toBeFalsy();
+    });
+
+    it('возвращает true, если значение представлено одной датой и дата календарно совпадает с ней', () => {
+      expect(isValueSelected({ date: range[0]!, value: [range[0], undefined] })).toBeTruthy();
+      expect(isValueSelected({ date: range[1]!, value: [undefined, range[1]] })).toBeTruthy();
+    });
+
+    it('возвращает false, если значение представлено одной датой и дата календарно не совпадает с ней', () => {
+      expect(isValueSelected({ date: range[1]!, value: [range[0], undefined] })).toBeFalsy();
+      expect(isValueSelected({ date: range[0]!, value: [undefined, range[1]] })).toBeFalsy();
+    });
+
+    it('возвращет false, если значение не задано', () => {
+      expect(isValueSelected({ date: new Date(2019, 0, 10), value: undefined })).toBeFalsy();
+      expect(
+        isValueSelected({ date: new Date(2019, 0, 10), value: [undefined, undefined] }),
+      ).toBeFalsy();
+    });
+  });
+});
+
+describe('isDateHighlighted', () => {
+  it('возвращает false, если не передана наведенная дата', () => {
+    expect(isDateHighlighted({ date: new Date(), value: [new Date(), new Date()] })).toBeFalsy();
+  });
+
+  it('возвращает false, если value не является интервалом', () => {
+    expect(
+      isDateHighlighted({
+        date: new Date(2015, 1, 1),
+        hoveredDate: new Date(2014, 1, 1),
+        value: new Date(2018, 1, 1),
+      }),
+    ).toBeFalsy();
+  });
+
+  it('возвращает false, если в переданном интервале не одна дата', () => {
+    expect(
+      isDateHighlighted({
+        date: new Date(2015, 1, 1),
+        hoveredDate: new Date(2014, 1, 1),
+        value: [new Date(2018, 1, 1), new Date(2020, 1, 1)],
+      }),
+    ).toBeFalsy();
+  });
+
+  it('возвращает false, если переданная дата не входит в интервал [наведенная дата, дата из переданного интервала]', () => {
+    expect(
+      isDateHighlighted({
+        date: new Date(2012, 1, 1),
+        hoveredDate: new Date(2014, 1, 1),
+        value: [new Date(2018, 1, 1), undefined],
+      }),
+    ).toBeFalsy();
+  });
+
+  it('возвращает false, если переданная дата не входит в интервал [дата из переданного интервала, наведенная дата]', () => {
+    expect(
+      isDateHighlighted({
+        date: new Date(2012, 1, 1),
+        hoveredDate: new Date(2018, 1, 1),
+        value: [undefined, new Date(2014, 1, 1)],
+      }),
+    ).toBeFalsy();
+  });
+
+  it('возвращает true, если переданная дата входит в интервал [наведенная дата, дата из переданного интервала]', () => {
+    expect(
+      isDateHighlighted({
+        date: new Date(2015, 1, 1),
+        hoveredDate: new Date(2014, 1, 1),
+        value: [new Date(2018, 1, 1), undefined],
+      }),
+    ).toBeTruthy();
+  });
+
+  it('возвращает true, если переданная дата входит в интервал [дата из переданного интервала, наведенная дата]', () => {
+    expect(
+      isDateHighlighted({
+        date: new Date(2015, 1, 1),
+        hoveredDate: new Date(2018, 1, 1),
+        value: [undefined, new Date(2014, 1, 1)],
+      }),
+    ).toBeTruthy();
+  });
+});
+
+describe('isValueSelectedBackwards', () => {
+  const date = new Date(2019, 0, 10);
+  const range: DateRange = [new Date(2019, 0, 10), new Date(2019, 0, 12)];
+
+  it('возвращает false, если не задана выбранная дата', () => {
+    expect(isValueSelectedBackwards({ value: range })).toBeFalsy();
+  });
+
+  it('возвращает false, если не задан интервал', () => {
+    expect(isValueSelectedBackwards({ hoveredDate: date })).toBeFalsy();
+  });
+
+  it('возвращает false, если переданное значение не является интервалом', () => {
+    expect(
+      isValueSelectedBackwards({ hoveredDate: date, value: new Date(2017, 0, 2) }),
+    ).toBeFalsy();
+  });
+
+  it('возвращает false, если в переданном интервале не одна дата', () => {
+    expect(isValueSelectedBackwards({ hoveredDate: date })).toBeFalsy();
+    expect(
+      isValueSelectedBackwards({ hoveredDate: date, value: [undefined, undefined] }),
+    ).toBeFalsy();
+    expect(isValueSelectedBackwards({ hoveredDate: date, value: range })).toBeFalsy();
+  });
+
+  it('возвращает false если переданная дата позже даты из интервала', () => {
+    expect(
+      isValueSelectedBackwards({
+        hoveredDate: new Date(2017, 4, 1),
+        value: [new Date(2017, 2, 1), undefined],
+      }),
+    ).toBeFalsy();
+
+    expect(
+      isValueSelectedBackwards({
+        hoveredDate: new Date(2017, 4, 1),
+        value: [undefined, new Date(2017, 2, 1)],
+      }),
+    ).toBeFalsy();
+  });
+
+  it('возвращает true если переданная дата раньше даты из интервала', () => {
+    expect(
+      isValueSelectedBackwards({
+        hoveredDate: new Date(2016, 4, 1),
+        value: [new Date(2017, 2, 1), undefined],
+      }),
+    ).toBeTruthy();
+
+    expect(
+      isValueSelectedBackwards({
+        hoveredDate: new Date(2016, 4, 1),
+        value: [undefined, new Date(2017, 2, 1)],
+      }),
+    ).toBeTruthy();
+  });
+});

src/components/DatePicker/__stories__/DatePicker.stories.tsx
@@ -1,0 +1,121 @@
+import React from 'react';
+import { date as knobsDate, select } from '@storybook/addon-knobs';
+import { addMonths, isValid, subYears } from 'date-fns';
+
+import { createMetadata, createStory } from '../../../utils/storybook';
+import { DatePicker } from '../DatePicker';
+import { getDateMidnightFromString, getInputValue } from '../DatePickerInputDate/helpers';
+import { DateRange, sizes } from '../types';
+
+const setInputValue = (value?: string): Date | undefined => {
+  if (!value) {
+    return undefined;
+  }
+
+  const date = getDateMidnightFromString(value);
+
+  return isValid(date) ? date : undefined;
+};
+
+const defaultProps = () =>
+  ({
+    minDate: new Date(knobsDate('minDate', subYears(new Date(), 1))),
+    maxDate: new Date(knobsDate('maxDate', addMonths(new Date(), 1))),
+    size: select('size', sizes, sizes[1]),
+  } as const);
+
+const DakePickerSingleStoryContent = () => {
+  const [date, setDate] = React.useState<Date | undefined>();
+
+  return <DatePicker type="date" value={date} onChange={setDate} {...defaultProps()} />;
+};
+
+export const DakePickerSingleStory = createStory(() => <DakePickerSingleStoryContent />, {
+  name: 'Выбор даты',
+});
+
+const DatePickerThirdPartyInputStoryContent = () => {
+  const [date, setDate] = React.useState<Date | undefined>(new Date(2019, 3, 1));
+
+  return (
+    <DatePicker
+      type="date"
+      value={date}
+      onChange={setDate}
+      {...defaultProps()}
+      renderControls={({ size, value, onChange }) => {
+        return (
+          <input
+            type="date"
+            value={getInputValue(value)}
+            onChange={(event) => {
+              onChange(setInputValue(event.target.value));
+            }}
+            className={size}
+          />
+        );
+      }}
+    />
+  );
+};
+
+export const DatePickerThirdPartyInputStory = createStory(
+  () => <DatePickerThirdPartyInputStoryContent />,
+  { name: 'Выбор даты с сторонним инпутом' },
+);
+
+const DatePickerRangeStoryContent = () => {
+  const [range, setRange] = React.useState<DateRange | undefined>([undefined, undefined]);
+
+  return <DatePicker type="date-range" value={range} onChange={setRange} {...defaultProps()} />;
+};
+
+export const DatePickerRangeStory = createStory(() => <DatePickerRangeStoryContent />, {
+  name: 'Выбор диапазона дат',
+});
+
+const DatePickerRangeThirdPartyInputStoryContent = () => {
+  const [range, setRange] = React.useState<DateRange | undefined>([undefined, undefined]);
+
+  return (
+    <DatePicker
+      type="date-range"
+      value={range}
+      onChange={setRange}
+      {...defaultProps()}
+      renderControls={({ size, value, onChange }) => {
+        return (
+          <>
+            <input
+              type="date"
+              value={value && getInputValue(value[0])}
+              onChange={(event) => {
+                onChange([setInputValue(event.target.value), (value && value[1]) || undefined]);
+              }}
+              className={size}
+            />
+            <input
+              type="date"
+              value={value && getInputValue(value[1])}
+              onChange={(event) => {
+                onChange([(value && value[0]) || undefined, setInputValue(event.target.value)]);
+              }}
+              className={size}
+            />
+          </>
+        );
+      }}
+    />
+  );
+};
+
+export const DatePickerRangeThirdPartyInputStory = createStory(
+  () => <DatePickerRangeThirdPartyInputStoryContent />,
+  {
+    name: 'Выбор диапазона дат с сторонними инпутами',
+  },
+);
+
+export default createMetadata({
+  title: 'Components|/DatePicker',
+});