e0b0b86b8e53e76766fcc50407c591f006a446b0
Anastasia Dekhtyar <44090737+andekhtyar@users.noreply.github.com>
2020-11-23 12:12:56 +0300

    feat(Icons): added IconSmile (#609)
    issue -  #608 
    
    Co-authored-by: gizeasy <gizeasy@gmail.com>

--- src/icons/IconExit/IconExit_size_m.tsx
+++ src/icons/IconExit/IconExit_size_m.tsx
@@ -3,8 +3,8 @@ import * as React from 'react';
 function IconExitSizeM(props: React.SVGProps<SVGSVGElement>) {
   return (
     <svg viewBox="0 0 24 24" {...props}>
-      <path d="M7 4h13v16H7v-5h2v3h9V6H9v3H7V4z" />
-      <path d="M5 8l-4 4 4 4v-3h9v-2H5V8z" />
+      <path d="M16 20H3V4h13v5h-2V6H5v12h9v-3h2v5z" />
+      <path d="M18 16l4-4-4-4v3H9v2h9v3z" />
     </svg>
   );
 }
--- src/icons/IconSmile/IconSmile_size_m.tsx
+++ src/icons/IconSmile/IconSmile_size_m.tsx
@@ -3,7 +3,7 @@ import * as React from 'react';
 function IconSmileSizeM(props: React.SVGProps<SVGSVGElement>) {
   return (
     <svg viewBox="0 0 24 24" {...props}>
-      <path d="M9 9H7v2h2V9zm6 0h2v2h-2V9zm1.026 3.772c-1.009 4.304-7.044 4.301-8.052.006l-1.948.457c1.492 6.355 10.457 6.354 11.948-.007l-1.948-.456z" />
+      <path d="M9 9H7v2h2V9zM15 9h2v2h-2V9zM16.026 12.772c-1.009 4.304-7.044 4.301-8.052.006l-1.948.457c1.492 6.355 10.457 6.354 11.948-.007l-1.948-.456z" />
       <path
         fillRule="evenodd"
         clipRule="evenodd"
