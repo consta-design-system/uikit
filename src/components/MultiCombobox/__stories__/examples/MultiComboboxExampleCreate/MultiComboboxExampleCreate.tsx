bd436027502065de2884b553fc05026cbfef4001
Anya Ovsyannikova <arhayka@gmail.com>
2020-11-29 00:08:45 +0300

    docs(MultiCombobox): add docs for MultiCombobox

---
+++ src/components/MultiCombobox/__stories__/examples/MultiComboboxExampleCreate/MultiComboboxExampleCreate.tsx
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+
+import { cnDocsDecorator } from '../../../../../uiKit/components/DocsDecorator/DocsDecorator';
+import { StoryBookExample } from '../../../../../uiKit/components/StoryBookExample/StoryBookExample';
+import { MultiCombobox } from '../../../MultiCombobox';
+
+type Option = {
+  label: string;
+  value: string;
+};
+
+const items: Option[] = [
+  { label: 'Синий', value: 'blue' },
+  { label: 'Белый', value: 'white' },
+  { label: 'Красный', value: 'red' },
+  { label: 'Черный', value: 'black' },
+  { label: 'Зеленый', value: 'green' },
+  { label: 'В полосочку', value: 'stripe' },
+];
+
+export const MultiComboboxExampleCreate = () => {
+  const [options, setOptions] = useState<Option[]>(items);
+  const [value, setValue] = useState<Option[] | null>(null);
+
+  const handleCreate = (label: string): void => {
+    const newItem = { label, value: label };
+    setOptions([newItem, ...options]);
+    const newValue = value ? [...value, newItem] : [newItem];
+    setValue(newValue);
+  };
+
+  const getItemLabel = (option: Option): string => option.label;
+  return (
+    <StoryBookExample className={cnDocsDecorator('Section')}>
+      <MultiCombobox
+        id="color"
+        options={options}
+        getOptionLabel={getItemLabel}
+        onCreate={handleCreate}
+        placeholder="Выберите цвет"
+        onChange={setValue}
+        value={value}
+      />
+    </StoryBookExample>
+  );
+};
