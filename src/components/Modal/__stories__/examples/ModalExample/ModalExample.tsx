8ed4e78fccd40ee2c48d6ca906a18426809ce92e
Anya Ovsyannikova <arhayka@gmail.com>
2020-11-29 01:15:15 +0300

    docs(Modal): add docs for Modal

---
+++ src/components/Modal/__stories__/examples/ModalExample/ModalExample.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+// import { cnDocsDecorator } from '../../../../../uiKit/components/DocsDecorator/DocsDecorator';
+// import { cnDocsExample } from '../../../../../uiKit/components/DocsExample/DocsExample';
+// import * as wp from '../../../../../uiKit/whitepaper/whitepaper';
+import { Button } from '../../../../Button/Button';
+// import { Modal } from '../../../Modal';
+
+export function ModalExampleText() {
+  return <Button size="m" view="primary" label="Открыть модальное окно" width="default" />;
+}
