71dcf86076dc0d6b675a2b0d17f4cd238c66d698
Anya Ovsyannikova <arhayka@gmail.com>
2020-12-04 01:03:54 +0300

    docs(Modal): add examples

--- src/components/Modal/__stories__/examples/ModalExample/ModalExample.tsx
+++ src/components/Modal/__stories__/examples/ModalExample/ModalExample.tsx
@@ -48,3 +48,118 @@ export function ModalExample() {
     </div>
   );
 }
+
+export function ModalExampleTop() {
+  const [isModalOpen, setIsModalOpen] = React.useState(false);
+
+  return (
+    <div className={cnDocsDecorator('Section')}>
+      <Button
+        size="m"
+        view="primary"
+        label="Окно сверху"
+        width="default"
+        onClick={(): void => setIsModalOpen(true)}
+      />
+      <Modal
+        className={cnModalExample('Modal')}
+        isOpen={isModalOpen}
+        hasOverlay
+        position="top"
+        onOverlayClick={(): void => setIsModalOpen(false)}
+      >
+        <Text as="p" size="s" view="secondary" className={cnModalExample('Title')}>
+          Я окно
+        </Text>
+        <Text as="p" size="m" view="primary" className={cnModalExample('Body')}>
+          Я выше всех!
+        </Text>
+        <div className={cnModalExample('Action')}>
+          <Button
+            size="m"
+            view="primary"
+            label="Ясно-понятно"
+            width="default"
+            onClick={(): void => setIsModalOpen(false)}
+          />
+        </div>
+      </Modal>
+    </div>
+  );
+}
+
+export function ModalExampleCenter() {
+  const [isModalOpen, setIsModalOpen] = React.useState(false);
+
+  return (
+    <div className={cnDocsDecorator('Section')}>
+      <Button
+        size="m"
+        view="primary"
+        label="Окно по центру"
+        width="default"
+        onClick={(): void => setIsModalOpen(true)}
+      />
+      <Modal
+        className={cnModalExample('Modal')}
+        isOpen={isModalOpen}
+        hasOverlay
+        onOverlayClick={(): void => setIsModalOpen(false)}
+      >
+        <Text as="p" size="s" view="secondary" className={cnModalExample('Title')}>
+          Я окно
+        </Text>
+        <Text as="p" size="m" view="primary" className={cnModalExample('Body')}>
+          Я в самой серединке!
+        </Text>
+        <div className={cnModalExample('Action')}>
+          <Button
+            size="m"
+            view="primary"
+            label="И тут всё понятно"
+            width="default"
+            onClick={(): void => setIsModalOpen(false)}
+          />
+        </div>
+      </Modal>
+    </div>
+  );
+}
+
+export function ModalExampleNoOverlay() {
+  const [isModalOpen, setIsModalOpen] = React.useState(false);
+
+  return (
+    <div className={cnDocsDecorator('Section')}>
+      <Button
+        size="m"
+        view="primary"
+        label="Без подложки"
+        width="default"
+        onClick={(): void => setIsModalOpen(true)}
+      />
+      <Modal
+        className={cnModalExample('Modal')}
+        isOpen={isModalOpen}
+        hasOverlay
+        onOverlayClick={(): void => setIsModalOpen(false)}
+      >
+        <Text as="p" size="s" view="secondary" className={cnModalExample('Title')}>
+          Я окно
+        </Text>
+        <Text as="p" size="m" view="primary" className={cnModalExample('Body')}>
+          Я без подложки. Честно-честно.
+        </Text>
+        <div className={cnModalExample('Action')}>
+          <Button
+            size="m"
+            view="primary"
+            label="Закройся!"
+            width="default"
+            onClick={(): void => setIsModalOpen(false)}
+          />
+        </div>
+      </Modal>
+    </div>
+  );
+}
