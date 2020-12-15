36aeac654237110368c4b7da4350ebda47f1a7e6
Dmitriy Korsakov <dmitriy.korsakov@nedra.digital>
2020-12-15 17:49:27 +0300

    feat(Breadcrumbs): Добавил брэдкрамбы

---
+++ src/components/Breadcrumbs/Breadcrumbs.tsx
@@ -0,0 +1,132 @@
+import './Breadcrumbs.css';
+
+import React, { useMemo } from 'react';
+
+import { IconProps, IconPropSize } from '../../icons/Icon/Icon';
+import { IconArrowRight } from '../../icons/IconArrowRight/IconArrowRight';
+import { cn } from '../../utils/bem';
+import { getSizeByMap } from '../../utils/getSizeByMap';
+import { PropsWithHTMLAttributesAndRef } from '../../utils/types/PropsWithHTMLAttributes';
+
+export const breadcrumbPropSize = ['m', 'xs', 's', 'l'] as const;
+export type BreadcrumbPropSize = typeof breadcrumbPropSize[number];
+export const breadcrumbPropSizeDefault: BreadcrumbPropSize = breadcrumbPropSize[0];
+
+export const cnBreadcrumbs = cn('Breadcrumbs');
+
+const sizeMap: Record<BreadcrumbPropSize, IconPropSize> = {
+  xs: 'xs',
+  s: 'xs',
+  m: 's',
+  l: 'm',
+};
+
+export type BreadcrumbsPropGetLabel<ITEM> = (item: ITEM) => string | React.ReactNode;
+export type BreadcrumbsPropGetIsActive<ITEM> = (item: ITEM) => boolean;
+export type BreadcrumbsPropGetLink<ITEM> = (item: ITEM) => string;
+export type BreadcrumbsPropGetIcon<ITEM> = (item: ITEM) => React.FC<IconProps> | undefined;
+
+export type BreadcrumbsProps<ITEM> = {
+  pages: ITEM[];
+  getLabel: BreadcrumbsPropGetLabel<ITEM>;
+  getIsActive?: BreadcrumbsPropGetIsActive<ITEM>;
+  getLink: BreadcrumbsPropGetLink<ITEM>;
+  getIcon?: BreadcrumbsPropGetIcon<ITEM>;
+  size?: BreadcrumbPropSize;
+  maxCount?: number;
+  onClick?: (page: ITEM, e: React.MouseEvent) => void;
+  onlyIconRoot?: boolean;
+  className?: string;
+};
+
+type Breadcrumbs = <ITEM>(
+  props: PropsWithHTMLAttributesAndRef<BreadcrumbsProps<ITEM>, HTMLUListElement>,
+) => React.ReactElement | null;
+
+export const Breadcrumbs: Breadcrumbs = React.forwardRef((props, ref) => {
+  const {
+    pages,
+    getLabel,
+    getIsActive,
+    getLink,
+    getIcon,
+    size = breadcrumbPropSizeDefault,
+    maxCount = 0,
+    onClick,
+    onlyIconRoot = false,
+    className,
+    ...restProps
+  } = props;
+
+  // type Pages = typeof pages;
+  const iconSize = getSizeByMap(sizeMap, size);
+
+  const { head, tail, rest } = useMemo(() => {
+    const rest = pages.slice();
+    const head = rest.splice(0, 1);
+    const tail = rest.splice(2 - maxCount);
+
+    return {
+      head,
+      tail,
+      rest,
+    };
+  }, [pages, maxCount]);
+
+  const delimiter = useMemo(
+    () => (
+      <IconArrowRight
+        size={iconSize}
+        className={cnBreadcrumbs('Delimiter', {
+          size: iconSize,
+        })}
+      />
+    ),
+    [iconSize],
+  );
+
+  const renderPages = (pages: any, isFirst = false) =>
+    pages.map((page: any) => {
+      const Icon = getIcon?.(page);
+      const label = getLabel(page);
+      const link = getLink(page);
+      const isActive = getIsActive?.(page);
+
+      return (
+        <li key={`${label}:${link}`}>
+          {!isFirst && delimiter}
+          <a
+            className={cnBreadcrumbs('Link', { active: isActive })}
+            onClick={(e) => onClick?.(page, e)}
+            href={link}
+          >
+            {Icon && (
+              <Icon
+                className={cnBreadcrumbs('Icon', {
+                  onlyIcon: onlyIconRoot && isFirst,
+                  size: iconSize,
+                })}
+                size={iconSize}
+              />
+            )}
+            {(!isFirst || !onlyIconRoot) && <span className={cnBreadcrumbs('Label')}>{label}</span>}
+          </a>
+        </li>
+      );
+    });
+
+  return (
+    <ul className={cnBreadcrumbs({ size }, [className])} ref={ref} {...restProps}>
+      {renderPages(head, true)}
+      {maxCount && rest.length > 1 ? (
+        <li>
+          {delimiter}
+          <span className={cnBreadcrumbs('More')}>...</span>
+        </li>
+      ) : (
+        renderPages(rest)
+      )}
+      {renderPages(tail)}
+    </ul>
+  );
+});
---
+++ src/components/Breadcrumbs/__tests__/Breadcrumbs.test.tsx
@@ -0,0 +1,162 @@
+import * as React from 'react';
+import { render } from '@testing-library/react';
+
+import { IconProps } from '../../../icons/Icon/Icon';
+import { IconCamera } from '../../../icons/IconCamera/IconCamera';
+import { IconUser } from '../../../icons/IconUser/IconUser';
+import { breadcrumbPropSize, Breadcrumbs, cnBreadcrumbs } from '../Breadcrumbs';
+
+type Page = {
+  icon?: React.FC<IconProps>;
+  link: string;
+  label: string;
+  isActive?: boolean;
+};
+
+const pages: Page[] = [
+  {
+    icon: IconUser,
+    label: 'Home',
+    link: 'http://yandex.ru',
+  },
+  {
+    label: 'Page1',
+    link: 'http://google.ru',
+  },
+  {
+    icon: IconCamera,
+    label: 'Page2',
+    link: 'http://google1.ru',
+  },
+  {
+    label: 'Page3',
+    link: 'http://google2.ru',
+  },
+  {
+    label: 'Page5',
+    link: 'http://google3.ru',
+  },
+];
+
+const initialProps = {
+  pages,
+  getLabel: (page: Page) => page.label,
+  getLink: (page: Page) => page.link,
+  getIcon: (page: Page) => page.icon,
+  getIsActive: (page: Page) => !!page.isActive,
+  onClick: (page: Page, e: React.MouseEvent) => {
+    e.preventDefault();
+    console.log(page.link, e);
+  },
+};
+
+const renderComponent = (props = {}) => {
+  return render(<Breadcrumbs {...initialProps} {...props} />);
+};
+
+describe('Компонент Breadcrumbs', () => {
+  it('должен рендериться без ошибок', () => {
+    expect(renderComponent).not.toThrow();
+  });
+
+  describe('проверка props', () => {
+    describe('проверка size', () => {
+      breadcrumbPropSize.forEach((size) => {
+        it(`присваивает класс для size=${size}`, () => {
+          const { container } = renderComponent({ size });
+
+          const breadcrumbs = container.firstChild;
+
+          expect(breadcrumbs).toHaveClass(cnBreadcrumbs({ size }));
+        });
+      });
+    });
+
+    describe('проверка maxCount', () => {
+      it('когда maxCount не указан, должны отобразиться все элементы', () => {
+        const { container } = renderComponent({ pages, maxCount: undefined });
+        const links = container.querySelectorAll(`.${cnBreadcrumbs('Link')}`);
+        expect(links.length).toEqual(pages.length);
+      });
+
+      it('когда maxCount не указан, не должно отобразиться многоточие', () => {
+        const { container } = renderComponent({ pages, maxCount: undefined });
+        const links = container.querySelectorAll(`.${cnBreadcrumbs('More')}`);
+
+        expect(links.length).toEqual(0);
+      });
+
+      it('когда maxCount больше или равен количеству страниц, должны отобразиться все элементы', () => {
+        const { container } = renderComponent({ pages, maxCount: 5 });
+        const links = container.querySelectorAll(`.${cnBreadcrumbs('Link')}`);
+
+        expect(links.length).toEqual(pages.length);
+      });
+
+      it('когда maxCount больше или равен количеству страниц, не должно отобразиться многоточие', () => {
+        const { container } = renderComponent({ pages, maxCount: 5 });
+        const links = container.querySelectorAll(`.${cnBreadcrumbs('More')}`);
+
+        expect(links.length).toEqual(0);
+      });
+
+      it('когда maxCount меньше количества страниц, должно отобразиться указанное количество элементов вместе с многоточием', () => {
+        const { container } = renderComponent({ pages, maxCount: 3 });
+        const links = container.querySelectorAll(`.${cnBreadcrumbs('Link')}`);
+
+        expect(links.length).toEqual(2);
+      });
+
+      it('когда maxCount меньше количества страниц, должно отобразиться многоточие', () => {
+        const { container } = renderComponent({ pages, maxCount: 3 });
+        const more = container.querySelectorAll(`.${cnBreadcrumbs('More')}`);
+
+        expect(more.length).toEqual(1);
+      });
+    });
+
+    describe('проверка url', () => {
+      const { container } = renderComponent({ pages, maxCount: 5 });
+      const links = container.querySelectorAll(`.${cnBreadcrumbs('Link')}`);
+      pages.forEach((page, i) => {
+        it(`присваивает href=${page.link}`, () => {
+          expect(links[i].getAttribute('href')).toEqual(page.link);
+        });
+      });
+    });
+
+    describe('проверка иконок', () => {
+      const { container } = renderComponent({ pages, maxCount: 5 });
+      const links = container.querySelectorAll(`.${cnBreadcrumbs('Link')}`);
+      pages.forEach((page, i) => {
+        it(`наличие иконки=${!!page.icon}`, () => {
+          expect(!!links[i].querySelector(`.${cnBreadcrumbs('Icon')}`)).toEqual(!!page.icon);
+        });
+      });
+    });
+
+    describe('проверка лэйблов', () => {
+      const { container } = renderComponent({ pages });
+      const links = container.querySelectorAll(`.${cnBreadcrumbs('Link')}`);
+      pages.forEach((page, i) => {
+        it(`лэйбл=${page.label}`, () => {
+          expect(links[i].querySelector(`.${cnBreadcrumbs('Label')}`)).toContainHTML(page.label);
+        });
+      });
+    });
+
+    describe('проверка onlyIconRoot', () => {
+      it(`Лэйбл первой ссылки не должен отображаться если выставлен флаг onlyIconRoot`, () => {
+        const { container } = renderComponent({ pages, onlyIconRoot: true });
+        const links = container.querySelectorAll(`.${cnBreadcrumbs('Link')}`);
+        expect(!!links[0].querySelector(`.${cnBreadcrumbs('Label')}`)).toEqual(false);
+      });
+
+      it(`Лэйбл первой ссылки должен отображаться если флаг onlyIconRoot не выставлен`, () => {
+        const { container } = renderComponent({ pages, onlyIconRoot: false });
+        const links = container.querySelectorAll(`.${cnBreadcrumbs('Link')}`);
+        expect(!!links[0].querySelector(`.${cnBreadcrumbs('Label')}`)).toEqual(true);
+      });
+    });
+  });
+});
