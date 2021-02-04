1ae23f151f7fb0b273736e15bf1d1f3286dc7955
Andrey Zhadanov <a.zhadanov@icloud.com>
2021-02-04 13:15:08 +0300

    feat(Avatar): add xs-size
    issue -  #769

--- src/components/Avatar/__stories__/examples/AvatarExampleSize/AvatarExampleSize.tsx
+++ src/components/Avatar/__stories__/examples/AvatarExampleSize/AvatarExampleSize.tsx
@@ -7,6 +7,7 @@ import { Avatar } from '../../../Avatar';
 
 export const AvatarExampleSize = () => (
   <StoryBookExample className={cnDocsDecorator('Section', [wp.decorator({ distribute: 'left' })])}>
+    <Avatar size="xs" name="Геннадий Куст" className={wp.decorator({ 'indent-r': 'm' })} />
     <Avatar size="s" name="Геннадий Куст" className={wp.decorator({ 'indent-r': 'm' })} />
     <Avatar size="m" name="Геннадий Куст" className={wp.decorator({ 'indent-r': 'm' })} />
   </StoryBookExample>
