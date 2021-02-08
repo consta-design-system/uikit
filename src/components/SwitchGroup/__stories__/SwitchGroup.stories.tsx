22b2aefe05453c694c336a2b2ff3dcf1f67c4c30
azhadanov <38282744+azhadanov@users.noreply.github.com>
2021-02-08 13:02:21 +0300

    feat(SwitchGroup): add SwitchGroup component, story and tests (#806)
    issue -  #665

---
+++ src/components/SwitchGroup/__stories__/SwitchGroup.stories.tsx
@@ -0,0 +1,74 @@
+import React from 'react';
+import { boolean, select } from '@storybook/addon-knobs';
+
+import { cn } from '../../../utils/bem';
+import { createMetadata } from '../../../utils/storybook';
+import {
+  SwitchGroup,
+  switchGroupDefaultDirection,
+  switchGroupDefaultSize,
+  switchGroupDefaultView,
+  switchGroupDirections,
+  switchGroupSizes,
+  switchGroupViews,
+} from '../SwitchGroup';
+
+// eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+// @ts-ignore
+import mdx from './SwitchGroup.mdx';
+
+const cnSwitchGroupStories = cn('SwitchGroupStories');
+
+type Item = {
+  name: string;
+  disabled?: boolean;
+};
+
+const items: Item[] = [
+  { name: 'один' },
+  { name: 'два' },
+  { name: 'три' },
+  { name: 'disabled', disabled: true },
+  { name: 'пять' },
+];
+
+const defaultKnobs = () => ({
+  size: select('size', switchGroupSizes, switchGroupDefaultSize),
+  view: select('view', switchGroupViews, switchGroupDefaultView),
+  direction: select('direction', switchGroupDirections, switchGroupDefaultDirection),
+  disabled: boolean('disabled', false),
+});
+
+export function Playground() {
+  const [value, setValue] = React.useState<Item[] | null>(null);
+  const { size, view, direction, disabled } = defaultKnobs();
+
+  return (
+    <div className={cnSwitchGroupStories()}>
+      <form>
+        <SwitchGroup
+          value={value}
+          items={items}
+          getLabel={(item) => item.name}
+          getDisabled={(item) => item.disabled}
+          onChange={({ value }) => setValue(value)}
+          name={cnSwitchGroupStories()}
+          direction={direction}
+          size={size}
+          view={view}
+          disabled={disabled}
+        />
+      </form>
+    </div>
+  );
+}
+
+export default createMetadata({
+  title: 'Компоненты|/SwitchGroup',
+  id: 'components/SwitchGroup',
+  parameters: {
+    docs: {
+      page: mdx,
+    },
+  },
+});
