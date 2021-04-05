d45d26c6e3130abcd46c73e80f7e2ecae6a1287c
Ilya Kolebayev <kolebayev@gmail.com>
2021-04-05 17:26:00 +0300

    Add components preview template
    На этом этапе не получается затащить в mdx файл с jsx, не хватает загрузчика

---
+++ docs/common/componentsPreview/ComponentsGrid/ComponentsGrid.tsx
@@ -0,0 +1,15 @@
+import './ComponentsGrid.css';
+
+import React from 'react';
+
+import { Text } from '../../../../src/components/Text/Text';
+
+// import { cn } from '../../../../src/utils/bem';
+
+export function ComponentsGrid() {
+  return (
+    <div className="test">
+      <Text>Это не заводится</Text>
+    </div>
+  );
+}
