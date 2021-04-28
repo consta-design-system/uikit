246709475821e13ea51b800e928b320f28e729c3
gizeasy <gizeasy@gmail.com>
2021-04-28 17:42:28 +0300

    fix:

--- docs/common/componentsPreview/ComponentsGrid/ComponentsGrid.tsx
+++ docs/common/componentsPreview/ComponentsGrid/ComponentsGrid.tsx
@@ -2,15 +2,11 @@ import './ComponentsGrid.css';
 
 import React from 'react';
 
-import { Text } from '../../../../src/components/Text/Text';
+// import { Text } from '../../../../src/components/Text/Text';
 import { cn } from '../../../../src/utils/bem';
 
 const cnComponentsGrid = cn('ComponentsGrid');
 
 export const ComponentsGrid = () => {
-  return (
-    <div className={cnComponentsGrid()}>
-      <Text>Это заводится</Text>
-    </div>
-  );
+  return <div className={cnComponentsGrid()}>{/* <Text>Это заводится</Text> */}</div>;
 };
