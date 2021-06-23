09477d001b5b392d568ae61cb6e72a1159c5dfd9
Ilya Kolebayev <kolebayev@gmail.com>
2021-06-23 19:42:27 +0300

    docs(sla): add sla (#1302)
    issue -  #1276
    
    Co-authored-by: gizeasy <gizeasy@gmail.com>

---
+++ docs/common/sla/components/SlaPictures/SlaPictures.tsx
@@ -0,0 +1,5 @@
+import { createImageComponent } from '../../../../../src/uiKit/components/Image/Image';
+
+import slaPicture from './images/sla-scheme.png';
+
+export const SlaScheme = createImageComponent(slaPicture);
---
+++ src/uiKit/components/Image/Image.tsx
@@ -0,0 +1,18 @@
+import './Image.css';
+
+import React from 'react';
+
+import { PropsWithJsxAttributes } from '../../../utils/types/PropsWithJsxAttributes';
+import { cn } from '../../cn';
+
+export type StoryBookModsProps = PropsWithJsxAttributes<{}, 'img'>;
+
+export const cnImage = cn('Image');
+
+export const Image: React.FC<StoryBookModsProps> = (props) => {
+  const { className, alt, ...otherProps } = props;
+
+  return <img {...otherProps} className={cnImage(null, [className])} alt={alt} />;
+};
+
+export const createImageComponent = (src: string) => () => <Image src={src} />;
