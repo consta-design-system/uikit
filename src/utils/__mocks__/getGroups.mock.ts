51c0a454a0981ffaf08a95633e56349716ef3796
gizeasy <gizeasy@gmail.com>
2021-06-23 11:54:12 +0300

    test(getGroups): edit tests
    issue -  #1281

---
+++ src/utils/__mocks__/getGroups.mock.ts
@@ -0,0 +1,47 @@
+export type Item = {
+  name: string;
+  group?: number;
+};
+
+export type Group = {
+  label: string;
+  id: string | number;
+};
+
+export const items: Item[] = [
+  {
+    name: 'Скрепка',
+    group: 2,
+  },
+  {
+    name: 'Глаз',
+    group: 1,
+  },
+  {
+    name: 'Две галочки',
+    group: 1,
+  },
+  {
+    name: 'Чемодан',
+    group: 2,
+  },
+  {
+    name: 'Солнце',
+    group: 2,
+  },
+];
+
+export const groups: Group[] = [
+  {
+    id: 1,
+    label: 'Первая группа',
+  },
+  {
+    id: 2,
+    label: 'Вторая группа',
+  },
+  {
+    id: 3,
+    label: 'Третья группа',
+  },
+];
