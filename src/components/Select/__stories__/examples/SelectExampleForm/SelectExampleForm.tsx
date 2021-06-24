9142e5ef8014110078fabe26bc08f28a5ed31c18
gizeasy <gizeasy@gmail.com>
2021-06-24 13:00:08 +0300

    docs(Select): added docs
    issue -  #1263

---
+++ src/components/Select/__stories__/examples/SelectExampleForm/SelectExampleForm.tsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+
+import { cnDocsDecorator } from '../../../../../uiKit/components/DocsDecorator/DocsDecorator';
+import { Select } from '../../../Select';
+
+type Item = {
+  label: string;
+  id: number;
+};
+
+const items: Item[] = [
+  {
+    label: 'Первый',
+    id: 1,
+  },
+  {
+    label: 'Второй',
+    id: 2,
+  },
+  {
+    label: 'Третий',
+    id: 3,
+  },
+];
+
+export function SelectExampleForm() {
+  const [value, setValue] = useState<Item | null>();
+  return (
+    <div className={cnDocsDecorator('Section')}>
+      <Select
+        placeholder="Выберите значение"
+        form="round"
+        items={items}
+        value={value}
+        onChange={({ value }) => setValue(value)}
+      />
+    </div>
+  );
+}
