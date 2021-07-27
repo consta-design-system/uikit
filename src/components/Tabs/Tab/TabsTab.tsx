874087f0504b751c775a50fc84be59bc03072eca
Nikita Golubov <neekitkah@gmail.com>
2021-07-27 16:45:25 +0300

    feat(Tabs): adapt tabs to available width by hiding unfitting tabs in dropdown
    BREAKING CHANGE:
    Tabs' renderItem render prop doesn't provide key, ref and className anymore as they are no longer
    needed. Just remove them from your render prop implementation.
    
    issue -  #1126

--- src/components/Tabs/Tab/TabsTab.tsx
+++ src/components/Tabs/Tab/TabsTab.tsx
@@ -5,10 +5,13 @@ import React, { forwardRef } from 'react';
 import { IconProps, IconPropSize } from '../../../icons/Icon/Icon';
 import { cnMixFocus } from '../../../mixs/MixFocus/MixFocus';
 import { cn } from '../../../utils/bem';
+import { getSizeByMap } from '../../../utils/getSizeByMap';
+import { TabsPropSize } from '../Tabs';
 
 export const cnTabsTab = cn('TabsTab');
 
 type Props = {
+  size: TabsPropSize;
   onlyIcon?: boolean;
   icon?: React.FC<IconProps>;
   iconSize?: IconPropSize;
@@ -18,12 +21,27 @@ type Props = {
   className?: string;
 };
 
+const sizeMap: Record<TabsPropSize, IconPropSize> = {
+  s: 'xs',
+  m: 's',
+};
+
 export const TabsTab = forwardRef<HTMLButtonElement, Props>((props, ref) => {
-  const { label, onChange, checked, onlyIcon, icon: Icon, iconSize, className } = props;
+  const {
+    label,
+    onChange,
+    checked,
+    size,
+    onlyIcon,
+    icon: Icon,
+    iconSize: iconSizeProp,
+    className,
+  } = props;
+  const iconSize = getSizeByMap(sizeMap, size, iconSizeProp);
 
   return (
     <button
-      className={cnTabsTab({ checked, onlyIcon }, [cnMixFocus({ before: true }), className])}
+      className={cnTabsTab({ size, checked, onlyIcon }, [cnMixFocus({ before: true }), className])}
       onClick={checked ? undefined : onChange}
       ref={ref}
       role="tab"
