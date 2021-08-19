2bb66236f8513add69b3c589b5b868a8674b3d7b
gizeasy <gizeasy@gmail.com>
2021-08-19 11:26:11 +0300

    feat(Calendar): add CalendarGrid (#1426)

--- src/components/Calendar/CalendarMonth/CalendarMonth.tsx
+++ src/components/Calendar/CalendarMonth/CalendarMonth.tsx
@@ -4,17 +4,15 @@ import React from 'react';
 
 import { cn } from '../../../utils/bem';
 import { PropsWithJsxAttributes } from '../../../utils/types/PropsWithJsxAttributes';
-import { Text } from '../../Text/Text';
-import { CalendarCell, CalendarCellProps } from '../CalendarCell/CalendarCell';
-import { CalendarItem, CalendarItemProps } from '../CalendarItem/CalendarItem';
-
-type DayOfMonth = Omit<CalendarItemProps & CalendarCellProps, 'ref'>;
+import { CalendarCellProps } from '../CalendarCell/CalendarCell';
+import { CalendarGrid } from '../CalendarGrid/CalendarGrid';
+import { CalendarItemProps } from '../CalendarItem/CalendarItem';
 
 export type CalendarMonthProps = PropsWithJsxAttributes<
   {
     children?: never;
     daysOfWeek: string[];
-    daysOfMonth: DayOfMonth[];
+    daysOfMonth: Omit<CalendarItemProps & CalendarCellProps, 'ref'>[];
   },
   'div'
 >;
@@ -25,22 +23,10 @@ export const CalendarMonth: React.FC<CalendarMonthProps> = (props) => {
   const { className, daysOfWeek, daysOfMonth, ...otherProps } = props;
 
   return (
-    <div {...otherProps} className={cnCalendarMonth(null, [className])}>
-      {daysOfWeek.map((item, index) => (
-        <CalendarCell key={cnCalendarMonth('DayOfWeek', { index, item })}>
-          <Text as="span" view="ghost" size="2xs" transform="uppercase">
-            {item}
-          </Text>
-        </CalendarCell>
-      ))}
-      {daysOfMonth.map(({ range, ...dayProps }, index) => (
-        <CalendarCell
-          key={cnCalendarMonth('DayOfMonth', { index, number: dayProps.label })}
-          range={range}
-        >
-          <CalendarItem {...dayProps} />
-        </CalendarCell>
-      ))}
-    </div>
+    <CalendarGrid
+      {...otherProps}
+      className={cnCalendarMonth(null, [className])}
+      items={[...daysOfWeek, ...daysOfMonth]}
+    />
   );
 };
