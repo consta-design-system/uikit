4a1c7f8e68237df701eaa48dd88ad15c20edbdcc
N1MBER <socialfox33@gmail.com>
2021-09-06 10:49:29 +0300

    feat(Card): add mixin and new component card
    Add mixin and new component Card
    
    issue -  #1486

---
+++ src/mixs/MixCard/MixCard.tsx
@@ -0,0 +1,28 @@
+import './MixCard.css';
+
+import { cn } from '../../utils/bem';
+
+const sizeMap = ['xs', 's', 'm', 'l', 'xl', '2xl', '3xl', '4xl', '5xl'] as const;
+type SizeMap = typeof sizeMap[number];
+
+const formMap = ['round', 'square'] as const;
+type FormMap = typeof formMap[number];
+
+const cardState = ['alert', 'success', 'warning'] as const;
+type CardState = typeof cardState[number];
+
+type Props = {
+  shadow?: boolean;
+  verticalSpace?: SizeMap;
+  horizontalSpace?: SizeMap;
+  form?: FormMap;
+  state?: CardState;
+};
+
+type CnCard = (
+  elemNameOrBlockMods?: Props | string | null,
+  elemModsOrBlockMix?: Props | Array<string | undefined> | null,
+  elemMix?: Array<string | undefined>,
+) => string;
+
+export const cnMixCard: CnCard = cn('MixCard');
