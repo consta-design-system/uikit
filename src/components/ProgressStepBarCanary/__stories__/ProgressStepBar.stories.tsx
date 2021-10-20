d0e6a9564ebaaeddaffe9924c554b85e8aa673ed
N1MBER <44449561+N1MBER@users.noreply.github.com>
2021-10-20 21:50:06 +0300

    feat(ProgressStepBar): add new component (#1643)
    closes #1616
    
    Co-authored-by: Anya Ovsyannikova <arhayka@gmail.com>
    Co-authored-by: gizeasy <gizeasy@gmail.com>

---
+++ src/components/ProgressStepBarCanary/__stories__/ProgressStepBar.stories.tsx
@@ -0,0 +1,78 @@
+import React from 'react';
+import { object, select } from '@storybook/addon-knobs';
+
+import { IconCheck } from '../../../icons/IconCheck/IconCheck';
+import { cn } from '../../../utils/bem';
+import { createMetadata } from '../../../utils/storybook';
+import { propDirection, propDirectionDefault, propSize, propSizeDefault } from '../helpers';
+import { ProgressStepBar } from '../ProgressStepBarCanary';
+
+import mdx from './ProgressStepBar.docs.mdx';
+
+const cnProgressStepBarStories = cn('ProgressStepBarStories');
+
+const stepsExample = [
+  {
+    label: 'Первый пункт',
+    point: 1,
+    status: 'normal',
+    lineStatus: 'normal',
+    tooltipContent: `Небольшое описание выполнения или состояния текущего этапа`,
+  },
+  {
+    label: 'Второй пункт',
+    point: 2,
+    status: 'warning',
+    progress: true,
+    lineStatus: 'warning',
+    tooltipContent: `Небольшое описание выполнения или состояния текущего этапа`,
+  },
+  {
+    label: 'Третий пункт',
+    point: IconCheck,
+    status: 'alert',
+    lineStatus: 'alert',
+    tooltipContent: `Небольшое описание выполнения или состояния текущего этапа`,
+  },
+  {
+    label: 'Четвертый пункт',
+    point: 4,
+    status: 'success',
+    tooltipContent: `Небольшое описание выполнения или состояния текущего этапа`,
+  },
+];
+
+const defaultKnobs = () => ({
+  steps: object('steps', stepsExample),
+  size: select('size', propSize, propSizeDefault),
+  direction: select('direction', propDirection, propDirectionDefault),
+  activeStep: select('activeStep', ['', 0, 1, 2, 3], ''),
+});
+
+export function Playground() {
+  const { size, direction, activeStep, steps } = defaultKnobs();
+
+  return (
+    <ProgressStepBar
+      className={cnProgressStepBarStories()}
+      direction={direction}
+      size={size}
+      steps={steps}
+      activeStepIndex={typeof activeStep === 'number' ? activeStep : undefined}
+    />
+  );
+}
+
+export default createMetadata({
+  title: 'Компоненты|/Служебные/ProgressStepBar(Canary)',
+  id: 'components/ProgressStepBar',
+  parameters: {
+    docs: {
+      page: mdx,
+    },
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/file/v9Jkm2GrymD277dIGpRBSH/Consta-UI-Kit?node-id=55787%3A112460',
+    },
+  },
+});
