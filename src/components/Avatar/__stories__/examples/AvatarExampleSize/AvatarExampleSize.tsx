780fe0258fc6afa0145e80794b141a12231897e5
arhayka <arhayka@gmail.com>
2021-11-17 17:35:22 +0300

    docs(Avatar): add size L to docs (#1774)

--- src/components/Avatar/__stories__/examples/AvatarExampleSize/AvatarExampleSize.tsx
+++ src/components/Avatar/__stories__/examples/AvatarExampleSize/AvatarExampleSize.tsx
@@ -10,5 +10,6 @@ export const AvatarExampleSize = () => (
     <Avatar size="xs" name="Геннадий Куст" className={wp.decorator({ 'indent-r': 'm' })} />
     <Avatar size="s" name="Геннадий Куст" className={wp.decorator({ 'indent-r': 'm' })} />
     <Avatar size="m" name="Геннадий Куст" className={wp.decorator({ 'indent-r': 'm' })} />
+    <Avatar size="l" name="Геннадий Куст" className={wp.decorator({ 'indent-r': 'm' })} />
   </StoryBookExample>
 );
