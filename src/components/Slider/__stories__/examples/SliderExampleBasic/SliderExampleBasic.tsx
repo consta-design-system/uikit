f0b42f58e979a6e8b151ec60bffe16d51759d055
Anya Ovsyannikova <arhayka@gmail.com>
2021-11-26 14:15:18 +0300

    docs(Slider): edit docs, add examples
    closes #1735

--- src/components/Slider/__stories__/examples/SliderExampleBasic/SliderExampleBasic.tsx
+++ src/components/Slider/__stories__/examples/SliderExampleBasic/SliderExampleBasic.tsx
@@ -1,19 +1,19 @@
 import React, { useState } from 'react';
 
-import { cnDocsDecorator } from '../../../../../uiKit/components/DocsDecorator/DocsDecorator';
+import { StoryBookExample } from '../../../../../uiKit/components/StoryBookExample/StoryBookExample';
 import { Slider } from '../../../Slider';
 
 export const SliderExampleBasic = () => {
   const [value, setValue] = useState<number>(20);
 
   return (
-    <div className={cnDocsDecorator('Section')}>
+    <StoryBookExample>
       <Slider
         range={false}
         label={`Значение ${value}`}
         onChange={({ value }) => setValue(value)}
         value={value}
       />
-    </div>
+    </StoryBookExample>
   );
 };
