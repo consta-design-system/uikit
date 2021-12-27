0414f3a2ad41b4b4fd78f832dbb1883bce1d4a85
Anya Ovsyannikova <arhayka@gmail.com>
2021-12-27 10:21:08 +0300

    docs(common): remove cards component

--- docs/common/addComponent/components/addComponentData/AddComponentData.tsx
+++ docs/common/addComponent/components/addComponentData/AddComponentData.tsx
@@ -11,31 +11,27 @@ const cnAddComponentData = cn('AddComponentData');
 
 export function AddComponentDataStage1() {
   return (
-    <Card
-      style={{ marginTop: 'var(--space-xl)' }}
-      className={cnAddComponentData()}
-      view="bordered"
-      verticalSpace="l"
-      horizontalSpace="l"
-    >
-      <Badge label="Этап 1" />
-      <Text weight="bold">Принимаем решение</Text>
-      <ol style={{ fontSize: 'var(--size-text-s)' }}>
-        <li style={{ marginBottom: 'var(--space-xs)' }}>
-          Оцениваем популярность компонента: смотрим, есть ли еще запросы на его разработку.
-        </li>
-        <li style={{ marginBottom: 'var(--space-xs)' }}>
+    <Card className={cnAddComponentData()} view="bordered" verticalSpace="l" horizontalSpace="l">
+      <div>
+        <Badge label="Этап 1" />
+        <Text weight="bold" className="AddComponentDataText">
+          Принимаем решение
+        </Text>
+      </div>
+      <ol>
+        <li>Оцениваем популярность компонента: смотрим, есть ли еще запросы на его разработку.</li>
+        <li>
           Составляем описание API и описываем, как компонент будет использоваться — на примерах,
           чтобы лучше понимать, как всё работает и зачем это нужно.
         </li>
-        <li style={{ marginBottom: 'var(--space-xs)' }}>
+        <li>
           Смотрим, что уже готово: если вы прислали PR, проверяем его, смотрим, есть ли в нём
           примеры использования и тесты.
         </li>
       </ol>
       <Text size="s">
         После этого этапа становится понятно, что это за компонент и нужно ли его добавлять в
-        дизайн-систему.{' '}
+        дизайн-систему.
       </Text>
       <Text size="s">Договариваемся, кто его разрабатывает — вы или команда Consta.</Text>
     </Card>
@@ -44,19 +40,16 @@ export function AddComponentDataStage1() {
 
 export function AddComponentDataStage2() {
   return (
-    <Card
-      style={{ marginTop: 'var(--space-xl)' }}
-      view="bordered"
-      verticalSpace="l"
-      horizontalSpace="l"
-    >
-      <Badge label="Этап 2" />
-      <Text weight="bold">Разрабатываем и обсуждаем новый компонент</Text>
-      <ol style={{ fontSize: 'var(--size-text-s)' }}>
-        <li style={{ marginBottom: 'var(--space-xs)' }}>
-          Разрабатываем компонент в дизайне и в коде.
-        </li>
-        <li style={{ marginBottom: 'var(--space-xs)' }}>Приглашаем сообщество к обсуждению.</li>
+    <Card className={cnAddComponentData()} view="bordered" verticalSpace="l" horizontalSpace="l">
+      <div>
+        <Badge label="Этап 2" />
+        <Text weight="bold" className="AddComponentDataText">
+          Разрабатываем и обсуждаем новый компонент
+        </Text>
+      </div>
+      <ol>
+        <li>Разрабатываем компонент в дизайне и в коде.</li>
+        <li>Приглашаем сообщество к обсуждению.</li>
       </ol>
     </Card>
   );
@@ -64,19 +57,16 @@ export function AddComponentDataStage2() {
 
 export function AddComponentDataStage3() {
   return (
-    <Card
-      style={{ marginTop: 'var(--space-xl)' }}
-      view="bordered"
-      verticalSpace="l"
-      horizontalSpace="l"
-    >
-      <Badge label="Этап 3" />
-      <Text weight="bold">Проверяем готовый компонент и проводим ревью</Text>
-      <ol style={{ fontSize: 'var(--size-text-s)' }}>
-        <li style={{ marginBottom: 'var(--space-xs)' }}>
-          Проводим ревью кода и устраняем все замечания.
-        </li>
-        <li style={{ marginBottom: 'var(--space-xs)' }}>
+    <Card className={cnAddComponentData()} view="bordered" verticalSpace="l" horizontalSpace="l">
+      <div>
+        <Badge label="Этап 3" />
+        <Text weight="bold" className="AddComponentDataText">
+          Проверяем готовый компонент и проводим ревью
+        </Text>
+      </div>
+      <ol>
+        <li>Проводим ревью кода и устраняем все замечания.</li>
+        <li>
           Команда дизайн-системы подтверждает, что компонент можно включить в состав основной
           библиотеки.
         </li>
@@ -87,17 +77,16 @@ export function AddComponentDataStage3() {
 
 export function AddComponentDataStage4() {
   return (
-    <Card
-      style={{ marginTop: 'var(--space-xl)', marginBottom: 'var(--space-xl)' }}
-      view="bordered"
-      verticalSpace="l"
-      horizontalSpace="l"
-    >
-      <Badge label="Этап 4" />
-      <Text weight="bold">Публикуем релиз</Text>
-      <ol style={{ fontSize: 'var(--size-text-s)' }}>
-        <li style={{ marginBottom: 'var(--space-xs)' }}>Собираем релизный пакет для выкладки.</li>
-        <li style={{ marginBottom: 'var(--space-xs)' }}>Публикуем информацию о релизе.</li>
+    <Card className={cnAddComponentData()} view="bordered" verticalSpace="l" horizontalSpace="l">
+      <div>
+        <Badge label="Этап 4" />
+        <Text weight="bold" className="AddComponentDataText">
+          Публикуем релиз
+        </Text>
+      </div>
+      <ol>
+        <li>Собираем релизный пакет для выкладки.</li>
+        <li>Публикуем информацию о релизе.</li>
       </ol>
     </Card>
   );
