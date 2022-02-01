b24a7c3368e56a73dc0b108f7e10fab703d4dead
N1MBER <socialfox33@gmail.com>
2022-02-01 16:23:42 +0300

    fix(useSortSteps): fix bug with incorrect work of algorithm
    closes #1908

--- src/hooks/useSortSteps/useSortSteps.ts
+++ src/hooks/useSortSteps/useSortSteps.ts
@@ -11,14 +11,17 @@ export const useSortSteps = (props: Props): number | number[] => {
 
   const sortedSteps = useMemo(() => {
     if (Array.isArray(step)) {
-      const stepsArr = [...step].sort((a, b) => a - b);
+      const stepsArr = [...step];
       if (typeof min === 'number' && stepsArr[0] !== min) {
         stepsArr.unshift(min);
       }
       if (typeof max === 'number' && stepsArr[stepsArr.length - 1] !== max) {
         stepsArr.push(max);
       }
-      return stepsArr;
+      const sortedArray = stepsArr.sort((a, b) => a - b);
+      const minValue = typeof min === 'number' ? min : stepsArr[0];
+      const maxValue = typeof max === 'number' ? max : stepsArr[stepsArr.length - 1];
+      return sortedArray.filter((val) => !(val < minValue || val > maxValue));
     }
     return Number(step);
   }, [step, min, max]);
