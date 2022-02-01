727c52318ca4d93d46ddb70eac72dd9a1d9fa6d7
N1MBER <44449561+N1MBER@users.noreply.github.com>
2022-02-01 13:51:04 +0300

    feat(ResponsesExit): add new response image (#1995)
    closes #1925

---
+++ src/responsesImages/ResponsesExit/ResponsesExit.tsx
@@ -0,0 +1,8 @@
+import { createResponsesImage } from '../createResponsesImage/createResponsesImage';
+
+import component from './ResponsesExitSvg';
+
+export const ResponsesExit = createResponsesImage({
+  name: 'ResponsesExit',
+  component,
+});
