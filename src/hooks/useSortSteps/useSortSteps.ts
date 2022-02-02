0f57708ffef79a314cfdb33dd3ace465879542bc
N1MBER <socialfox33@gmail.com>
2022-02-02 12:00:13 +0300

    fix(useSortSteps): rewrite hook logic
    closes #1908

--- src/hooks/useSortSteps/useSortSteps.ts
+++ src/hooks/useSortSteps/useSortSteps.ts
@@ -6,22 +6,26 @@ type Props = {
   max?: number;
 };
 
+const getIsValidValue = (value: number, min?: number, max?: number) => {
+  return !((min && value < min) || (max && value > max));
+};
+
 export const useSortSteps = (props: Props): number | number[] => {
   const { step, min, max } = props;
 
   const sortedSteps = useMemo(() => {
     if (Array.isArray(step)) {
       const stepsArr = [...step];
-      if (typeof min === 'number' && stepsArr[0] !== min) {
-        stepsArr.unshift(min);
+      if (typeof min === 'number') {
+        stepsArr.push(min);
       }
-      if (typeof max === 'number' && stepsArr[stepsArr.length - 1] !== max) {
+      if (typeof max === 'number') {
         stepsArr.push(max);
       }
-      const sortedArray = stepsArr.sort((a, b) => a - b);
-      const minValue = typeof min === 'number' ? min : stepsArr[0];
-      const maxValue = typeof max === 'number' ? max : stepsArr[stepsArr.length - 1];
-      return sortedArray.filter((val) => !(val < minValue || val > maxValue));
+      const sortedArray = stepsArr
+        .sort((a, b) => a - b)
+        .filter((val) => getIsValidValue(val, min, max));
+      return Array.from(new Set(sortedArray));
     }
     return Number(step);
   }, [step, min, max]);
