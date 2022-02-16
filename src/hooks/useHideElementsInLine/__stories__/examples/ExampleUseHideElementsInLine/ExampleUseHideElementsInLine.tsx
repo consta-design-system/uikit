0c6c7575c13350705c2b3ac7343cbb4781f94baa
gizeasy <gizeasy@gmail.com>
2022-02-16 19:00:23 +0300

    docs(useHideElementsInLine): add docs (#2058)
    closes #2027
    
    Co-authored-by: Anya Ovsyannikova <arhayka@gmail.com>

---
+++ src/hooks/useHideElementsInLine/__stories__/examples/ExampleUseHideElementsInLine/ExampleUseHideElementsInLine.tsx
@@ -0,0 +1,55 @@
+import './ExampleUseHideElementsInLine.css';
+
+import React from 'react';
+
+import { Tag } from '../../../../../components/Tag/Tag';
+import { cn } from '../../../../../utils/bem';
+import { useHideElementsInLine } from '../../../useHideElementsInLine';
+
+const cnExampleUseHideElementsInLine = cn('ExampleUseHideElementsInLine');
+
+const items = [
+  'первый',
+  'второй',
+  'третий',
+  'четвёрый',
+  'пятый',
+  'шестой',
+  'седьмой',
+  'восьмой',
+  'девятый',
+  'десятый',
+];
+
+export const ExampleUseHideElementsInLine = () => {
+  const { itemsRefs, wrapperRef, hiddenItems, moreRef, visibleItems } = useHideElementsInLine(
+    items,
+  );
+
+  return (
+    <div ref={wrapperRef} className={cnExampleUseHideElementsInLine()}>
+      {items.map((item, index) => {
+        return (
+          <Tag
+            className={cnExampleUseHideElementsInLine('Tag', {
+              display: !visibleItems[index] && 'hidden',
+            })}
+            key={index}
+            label={item}
+            ref={itemsRefs[index]}
+            mode="info"
+          />
+        );
+      })}
+      <Tag
+        className={cnExampleUseHideElementsInLine('Tag', {
+          display: !hiddenItems.length && 'none',
+        })}
+        key="more"
+        label={`+${hiddenItems.length}`}
+        ref={moreRef}
+        mode="info"
+      />
+    </div>
+  );
+};
