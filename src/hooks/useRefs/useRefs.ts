a55943f0457bac07defe27f9b61c7a8da6ad3bfd
arhayka <arhayka@gmail.com>
2022-02-16 18:59:23 +0300

    docs(useRefs): add docs (#2053)
    Co-authored-by: gizeasy <gizeasy@gmail.com>

--- src/hooks/useRefs/useRefs.ts
+++ src/hooks/useRefs/useRefs.ts
@@ -1,4 +1,4 @@
 import { createRef, useMemo } from 'react';
 
-export const useRefs = <ELEMENT extends HTMLElement>(length: number, deps: unknown[] = []) =>
-  useMemo(() => new Array(length).fill(null).map(() => createRef<ELEMENT>()), [length, ...deps]);
+export const useRefs = <T>(length: number, deps: unknown[] = []) =>
+  useMemo(() => new Array(length).fill(null).map(() => createRef<T>()), [length, ...deps]);
