261f83ac4d301a52bbb18a8afb49d1a5a203512f
Ilya Kolebayev <kolebayev@gmail.com>
2022-02-19 22:10:00 +0300

    feat(icon): add new icons
    closes #2035

---
+++ src/icons/IconTable2/IconTable2_size_m.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+
+function IconTable2SizeM(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg viewBox="0 0 24 24" {...props}>
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M2 3a1 1 0 011-1h18a1 1 0 011 1v18a1 1 0 01-1 1H3a1 1 0 01-1-1V3zm2 7v4h7v-4H4zm7-2H4V4h7v4zm2 2v4h7v-4h-7zm7-2h-7V4h7v4zm0 8h-7v4h7v-4zm-9 4v-4H4v4h7z"
+      />
+    </svg>
+  );
+}
+
+export default IconTable2SizeM;
---
+++ src/icons/IconTable2/IconTable2_size_xs.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+
+function IconTable2SizeXs(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg viewBox="0 0 12 12" {...props}>
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M1 1.5a.5.5 0 01.5-.5h9a.5.5 0 01.5.5v9a.5.5 0 01-.5.5h-9a.5.5 0 01-.5-.5v-9zM2 5v2h3.5V5H2zm4.5 0v2H10V5H6.5zM10 4H6.5V2H10v2zM5.5 2v2H2V2h3.5zM10 8H6.5v2H10V8zm-4.5 2V8H2v2h3.5z"
+      />
+    </svg>
+  );
+}
+
+export default IconTable2SizeXs;
