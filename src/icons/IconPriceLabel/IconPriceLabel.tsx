a4ec03c929ecbc003549371f0fe45d399360156d
Ilya Kolebayev <kolebayev@gmail.com>
2022-02-22 09:39:31 +0300

    feat(icon): add new icons (#2076)
    closes #2035

---
+++ src/icons/IconPriceLabel/IconPriceLabel.tsx
@@ -0,0 +1,12 @@
+import { createIcon } from '../createIcon/createIcon';
+
+import IconPriceLabelSizeM from './IconPriceLabel_size_m';
+import IconPriceLabelSizeS from './IconPriceLabel_size_s';
+import IconPriceLabelSizeXs from './IconPriceLabel_size_xs';
+
+export const IconPriceLabel = createIcon({
+  m: IconPriceLabelSizeM,
+  s: IconPriceLabelSizeS,
+  xs: IconPriceLabelSizeXs,
+  name: 'IconPriceLabel',
+});
---
+++ src/icons/IconWrench/IconWrench_size_xs.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+function IconWrenchSizeXs(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg viewBox="0 0 12 12" {...props}>
+      <path d="M6.625 3.87c-.213-.761-.02-1.611.577-2.21a2.263 2.263 0 012.191-.58l-1.28 1.28.137 1.39 1.455.2 1.281-1.28A2.26 2.26 0 018.098 5.41l-.007.007-5.269 5.269a1.066 1.066 0 11-1.51-1.51L6.626 3.87z" />
+    </svg>
+  );
+}
+
+export default IconWrenchSizeXs;
