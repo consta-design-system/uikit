0ada02ff00a510e5c5fe96fbb0571327f5e83a71
Anya Ovsyannikova <arhayka@gmail.com>
2022-04-05 12:40:45 +0300

    docs(ChoiceGroup): add adaptive section

---
+++ src/components/ChoiceGroup/__stories__/examples/ChoiceGroupExampleAdaptive/ChoiceGroupExampleAdaptive.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import image from './images/choiceGroupAdaptive.png';
+
+export const ChoiceGroupExampleAdaptive = () => {
+  return (
+    <p>
+      <img src={image} alt="Адаптивность для ChoiceGroup" />
+    </p>
+  );
+};
