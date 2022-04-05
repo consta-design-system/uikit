f6c07abc5bd7c90e8779e4b061396f4ca08a526e
Anya Ovsyannikova <arhayka@gmail.com>
2022-04-05 13:18:07 +0300

    docs(ContextMenu): fix examples

--- src/components/ContextMenuCanary/__stories__/examples/ContextMenuExampleOutsideClick/ContextMenuExampleOutsideClick.tsx
+++ src/components/ContextMenuCanary/__stories__/examples/ContextMenuExampleOutsideClick/ContextMenuExampleOutsideClick.tsx
@@ -5,20 +5,34 @@ import { StoryBookExample } from '../../../../../uiKit/components/StoryBookExamp
 import { Button } from '../../../../Button/Button';
 import { ContextMenu } from '../../../ContextMenuCanary';
 
-const items: string[] = ['Пункт 1', 'Пункт 2', 'Пункт 3'];
+type Item = {
+  label: string;
+};
+
+const items: Item[] = [
+  {
+    label: 'Пункт 1',
+  },
+  {
+    label: 'Пункт 2',
+  },
+  {
+    label: 'Пункт 3',
+  },
+];
 
 export const ContextMenuExampleOutsideClick = () => {
   const ref = useRef(null);
   const [isOpen, setIsOpen] = useState<boolean>(false);
-
   return (
     <StoryBookExample className={cnDocsDecorator('Section')}>
       <Button ref={ref} label="Открыть" onClick={() => setIsOpen(!isOpen)} />
       <ContextMenu
+        isOpen={isOpen}
         items={items}
-        getItemLabel={(item) => item}
         anchorRef={ref}
         onClickOutside={() => setIsOpen(false)}
+        direction="downStartLeft"
       />
     </StoryBookExample>
   );
