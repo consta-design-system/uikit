04cee6482933b27f7af6ef75a7c729ca6f0c02a3
Maxim Kolesnikov <44449561+N1MBER@users.noreply.github.com>
2022-05-25 17:36:24 +0300

    feat(ChoiceGroupCanary): add ChoiceGroupCanary (#2312)
    closes #2216
    
    Co-authored-by: Anya Ovsyannikova <arhayka@gmail.com>

---
+++ src/components/ChoiceGroupCanary/__stories__/examples/ChoiceGroupExampleSize/ChoiceGroupExampleSize.tsx
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+
+import { cnDocsDecorator } from '../../../../../uiKit/components/DocsDecorator/DocsDecorator';
+import { StoryBookExample } from '../../../../../uiKit/components/StoryBookExample/StoryBookExample';
+import { ChoiceGroup } from '../../../ChoiceGroupCanary';
+
+type Item = string;
+
+const items: Item[] = ['один', 'два', 'три'];
+
+export const ChoiceGroupExampleSize = () => {
+  const [value, setValue] = useState<Item | null>(items[0]);
+  return (
+    <StoryBookExample className={cnDocsDecorator('Section')}>
+      <div>
+        <ChoiceGroup
+          value={value}
+          onChange={({ value }) => setValue(value)}
+          items={items}
+          getItemLabel={(item) => item}
+          size="xs"
+          name="ChoiceGroupExampleSize"
+        />
+      </div>
+      <div>
+        <ChoiceGroup
+          value={value}
+          onChange={({ value }) => setValue(value)}
+          items={items}
+          getItemLabel={(item) => item}
+          size="s"
+          name="ChoiceGroupExampleSize"
+        />
+      </div>
+      <div>
+        <ChoiceGroup
+          value={value}
+          onChange={({ value }) => setValue(value)}
+          items={items}
+          getItemLabel={(item) => item}
+          size="m"
+          name="ChoiceGroupExampleSize"
+        />
+      </div>
+      <div>
+        <ChoiceGroup
+          value={value}
+          onChange={({ value }) => setValue(value)}
+          items={items}
+          getItemLabel={(item) => item}
+          size="l"
+          name="ChoiceGroupExampleSize"
+        />
+      </div>
+    </StoryBookExample>
+  );
+};
---
+++ src/components/ChoiceGroupCanary/__stories__/examples/ChoiceGroupExampleText/ChoiceGroupExampleText.tsx
@@ -0,0 +1,60 @@
+import React, { useState } from 'react';
+
+import { cnDocsDecorator } from '../../../../../uiKit/components/DocsDecorator/DocsDecorator';
+import { cnDocsExample } from '../../../../../uiKit/components/DocsExample/DocsExample';
+import { StoryBookExample } from '../../../../../uiKit/components/StoryBookExample/StoryBookExample';
+import * as wp from '../../../../../uiKit/whitepaper/whitepaper';
+import { ChoiceGroup } from '../../../ChoiceGroupCanary';
+
+type Item = string;
+
+const itemsWrong: Item[] = ['Сетка', 'Таблицей', 'Карточка'];
+const items: Item[] = ['Списком', 'В таблице', 'По одному'];
+const itemsSimple: Item[] = ['один', 'два', 'три', 'четыре', 'пять', 'шесть'];
+
+export const ChoiceGroupExampleText = () => {
+  const [value, setValue] = useState<Item | null>(items[0]);
+  return (
+    <div
+      className={cnDocsDecorator('Section', [wp.tplGrid({ 'ratio': '1-1', 'col-gap': 'full' })])}
+    >
+      <div className={cnDocsExample()}>
+        <p className={cnDocsExample('Caption')}>Отображение</p>
+        <ChoiceGroup
+          value={value}
+          onChange={({ value }) => setValue(value)}
+          items={itemsWrong}
+          getItemLabel={(item) => item}
+          name="ChoiceGroupExampleText"
+        />
+        <p className={cnDocsExample('Status', { view: 'wrong' })}>Неправильно</p>
+      </div>
+      <div className={cnDocsExample()}>
+        <p className={cnDocsExample('Caption')}>Как показывать товары</p>
+        <ChoiceGroup
+          value={value}
+          onChange={({ value }) => setValue(value)}
+          items={items}
+          getItemLabel={(item) => item}
+          name="ChoiceGroupExampleText"
+        />
+        <p className={cnDocsExample('Status', { view: 'right' })}>Правильно</p>
+      </div>
+    </div>
+  );
+};
+
+export const ChoiceGroupExample = () => {
+  const [value, setValue] = useState<Item | null>(items[0]);
+  return (
+    <StoryBookExample className={cnDocsDecorator('Section')}>
+      <ChoiceGroup
+        value={value}
+        onChange={({ value }) => setValue(value)}
+        items={itemsSimple}
+        getItemLabel={(item) => item}
+        name="ChoiceGroupExampleText"
+      />
+    </StoryBookExample>
+  );
+};
