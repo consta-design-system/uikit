afec05578c46184cdf6a447d2d455fb3eef35d3b
Maxim Kolesnikov <44449561+N1MBER@users.noreply.github.com>
2022-05-25 17:18:45 +0300

    feat(CheckboxGroupCanary): add CheckboxGroupCanary (#2311)
    closes #2215

---
+++ src/components/CheckboxGroupCanary/CheckboxGroupCanary.tsx
@@ -0,0 +1,68 @@
+import './CheckboxGroup.css';
+
+import React, { forwardRef } from 'react';
+
+import { useChoiceGroup } from '../../hooks/useChoiceGroup/useChoiceGroup';
+import { cn } from '../../utils/bem';
+import { Checkbox } from '../Checkbox/Checkbox';
+
+import { withDefaultGetters } from './helper';
+import {
+  CheckboxGroupComponent,
+  checkboxGroupDefaultDirection,
+  checkboxGroupDefaultSize,
+  checkboxGroupDefaultView,
+  CheckboxGroupProps,
+} from './types';
+
+export const cnCheckboxGroup = cn('CheckboxGroup');
+
+function CheckboxGroupRender(props: CheckboxGroupProps, ref: React.Ref<HTMLDivElement>) {
+  const {
+    value = null,
+    items,
+    getItemLabel,
+    getItemDisabled,
+    onChange,
+    name,
+    direction = checkboxGroupDefaultDirection,
+    size = checkboxGroupDefaultSize,
+    view = checkboxGroupDefaultView,
+    disabled = false,
+    className,
+    ...otherProps
+  } = withDefaultGetters(props);
+
+  const { getOnChange, getChecked } = useChoiceGroup({
+    value,
+    getKey: getItemLabel,
+    callBack: onChange,
+    multiple: true,
+  });
+
+  return (
+    <div
+      {...otherProps}
+      ref={ref}
+      className={cnCheckboxGroup({ direction, size, view }, [className])}
+    >
+      {items.map((item) => (
+        <Checkbox
+          key={getItemLabel(item)}
+          label={getItemLabel(item)}
+          size={size}
+          view={view}
+          name={name}
+          disabled={disabled || getItemDisabled?.(item)}
+          checked={getChecked(item)}
+          onChange={({ e }) => getOnChange(item)(e)}
+          className={cnCheckboxGroup('Item')}
+        />
+      ))}
+    </div>
+  );
+}
+
+export const CheckboxGroup = forwardRef(CheckboxGroupRender) as CheckboxGroupComponent;
+
+export * from './types';
---
+++ src/components/CheckboxGroupCanary/__stories__/examples/CheckboxGroupExampleName/CheckboxGroupExampleName.tsx
@@ -0,0 +1,188 @@
+import React from 'react';
+
+import {
+  items,
+  itemsGen,
+  itemsGen2,
+  itemsHead,
+  itemsHead2,
+  itemsNeg,
+  itemsNeg2,
+} from '../../../__mocks__/data.mock';
+import { cnDocsDecorator } from '../../../../../uiKit/components/DocsDecorator/DocsDecorator';
+import { cnDocsExample } from '../../../../../uiKit/components/DocsExample/DocsExample';
+import { StoryBookExample } from '../../../../../uiKit/components/StoryBookExample/StoryBookExample';
+import * as wp from '../../../../../uiKit/whitepaper/whitepaper';
+import { Button } from '../../../../Button/Button';
+import { CheckboxGroup } from '../../../CheckboxGroupCanary';
+
+type Item = {
+  name: string;
+  disabled?: boolean;
+};
+
+export function CheckboxGroupExampleName() {
+  const [value, setValue] = React.useState<Item[] | null>(null);
+
+  return (
+    <StoryBookExample className={cnDocsDecorator('Section')}>
+      <p className={cnDocsExample('Caption')}>
+        <b>Выберите номер</b>
+      </p>
+      <CheckboxGroup
+        value={value}
+        items={items}
+        getItemLabel={(item) => item.name}
+        getItemDisabled={(item) => item.disabled}
+        onChange={({ value }) => setValue(value)}
+        name="CheckboxGroup"
+      />
+    </StoryBookExample>
+  );
+}
+
+export function CheckboxGroupExampleHead() {
+  const [value, setValue] = React.useState<Item[] | null>(null);
+
+  return (
+    <div
+      className={cnDocsDecorator('Section', [wp.tplGrid({ 'ratio': '1-1', 'col-gap': 'full' })])}
+    >
+      <div className={cnDocsExample()}>
+        <p className={cnDocsExample('Caption')}>
+          <b>Где деньги?</b>
+        </p>
+        <div className={wp.decorator({ distribute: 'left' })}>
+          <CheckboxGroup
+            value={value}
+            items={itemsHead}
+            getItemLabel={(item) => item.name}
+            getItemDisabled={(item) => item.disabled}
+            onChange={({ value }) => setValue(value)}
+            name="CheckboxGroup"
+          />
+        </div>
+        <p className={cnDocsExample('Status', { view: 'wrong' })}>Неправильно</p>
+      </div>
+      <div className={cnDocsExample()}>
+        <p className={cnDocsExample('Caption')}>
+          <b>Где деньги?</b>
+        </p>
+        <div className={wp.decorator({ distribute: 'left' })}>
+          <CheckboxGroup
+            value={value}
+            items={itemsHead2}
+            getItemLabel={(item) => item.name}
+            getItemDisabled={(item) => item.disabled}
+            onChange={({ value }) => setValue(value)}
+            name="CheckboxGroup"
+          />
+        </div>
+        <p className={cnDocsExample('Status', { view: 'right' })}>Правильно</p>
+      </div>
+    </div>
+  );
+}
+
+export function CheckboxExampleGroupHead3() {
+  const [value, setValue] = React.useState<Item[] | null>(null);
+
+  return (
+    <StoryBookExample className={cnDocsDecorator('Section')}>
+      <p className={cnDocsExample('Caption')}>
+        <b>Выберите места для хранения денег</b>
+      </p>
+      <CheckboxGroup
+        value={value}
+        items={itemsHead}
+        getItemLabel={(item) => item.name}
+        getItemDisabled={(item) => item.disabled}
+        onChange={({ value }) => setValue(value)}
+        name="CheckboxGroup"
+      />
+    </StoryBookExample>
+  );
+}
+
+export function CheckboxGroupExampleNeg() {
+  const [value, setValue] = React.useState<Item[] | null>(null);
+
+  return (
+    <div
+      className={cnDocsDecorator('Section', [wp.tplGrid({ 'ratio': '1-1', 'col-gap': 'full' })])}
+    >
+      <div className={cnDocsExample()}>
+        <div className={wp.decorator({ distribute: 'left' })}>
+          <CheckboxGroup
+            value={value}
+            items={itemsNeg}
+            getItemLabel={(item) => item.name}
+            getItemDisabled={(item) => item.disabled}
+            onChange={({ value }) => setValue(value)}
+            name="CheckboxGroup"
+          />
+        </div>
+        <p className={cnDocsExample('Status', { view: 'wrong' })}>Неправильно</p>
+      </div>
+      <div className={cnDocsExample()}>
+        <div className={wp.decorator({ distribute: 'left' })}>
+          <CheckboxGroup
+            value={value}
+            items={itemsNeg2}
+            getItemLabel={(item) => item.name}
+            getItemDisabled={(item) => item.disabled}
+            onChange={({ value }) => setValue(value)}
+            name="CheckboxGroup"
+          />
+        </div>
+        <p className={cnDocsExample('Status', { view: 'right' })}>Правильно</p>
+      </div>
+    </div>
+  );
+}
+
+export function CheckboxGroupExampleGeneral() {
+  const [value, setValue] = React.useState<Item[] | null>(null);
+
+  return (
+    <div
+      className={cnDocsDecorator('Section', [wp.tplGrid({ 'ratio': '1-1', 'col-gap': 'full' })])}
+    >
+      <div className={cnDocsExample()}>
+        <p className={cnDocsExample('Caption')}>
+          <b>Каких булок ещё съесть</b>
+        </p>
+        <div className={wp.decorator({ distribute: 'left' })}>
+          <CheckboxGroup
+            value={value}
+            items={itemsGen}
+            getItemLabel={(item) => item.name}
+            getItemDisabled={(item) => item.disabled}
+            onChange={({ value }) => setValue(value)}
+            name="CheckboxGroup"
+          />
+        </div>
+        <p className={cnDocsExample('Status', { view: 'wrong' })}>Неправильно</p>
+      </div>
+      <div className={cnDocsExample()}>
+        <p className={cnDocsExample('Caption')}>
+          <b>Каких булок ещё съесть</b>
+        </p>
+        <div className={wp.decorator({ distribute: 'left' })}>
+          <CheckboxGroup
+            value={value}
+            items={itemsGen2}
+            getItemLabel={(item) => item.name}
+            getItemDisabled={(item) => item.disabled}
+            onChange={({ value }) => setValue(value)}
+            name="CheckboxGroup"
+          />
+        </div>
+        <div className={wp.decorator({ 'distribute': 'left', 'indent-t': 'm' })}>
+          <Button label="Пропустить этот шаг" view="ghost" />
+        </div>
+        <p className={cnDocsExample('Status', { view: 'right' })}>Правильно</p>
+      </div>
+    </div>
+  );
+}
