57e5f93f6d00931513447f722dfdb726d5673f06
Anya Ovsyannikova <arhayka@gmail.com>
2022-06-23 12:42:24 +0300

    docs(DatePicker): add withClearButton description
    closes #2370

---
+++ src/components/DatePickerCanary/__stories__/examples/DatePickerExampleWithClearButton/DatePickerExampleWithClearButton.tsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+
+import { cnDocsDecorator } from '../../../../../uiKit/components/DocsDecorator/DocsDecorator';
+import { StoryBookExample } from '../../../../../uiKit/components/StoryBookExample/StoryBookExample';
+import { DatePicker } from '../../../DatePickerCanary';
+
+export const DatePickerExampleWithClearButton = () => {
+  const [value, setValue] = useState<Date | null>(null);
+
+  return (
+    <StoryBookExample className={cnDocsDecorator('Section')}>
+      <DatePicker value={value} onChange={({ value }) => setValue(value)} withClearButton />
+    </StoryBookExample>
+  );
+};
