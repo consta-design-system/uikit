bb149b2b0b70d600992b7b758f97519011f55efc
Anya Ovsyannikova <arhayka@gmail.com>
2022-06-28 15:07:09 +0300

    docs(ProgressLine): edit docs

--- src/components/ProgressLine/__stories__/examples/ProgressLineExampleMode/ProgressLineExampleMode.tsx
+++ src/components/ProgressLine/__stories__/examples/ProgressLineExampleMode/ProgressLineExampleMode.tsx
@@ -1,18 +1,22 @@
 import React from 'react';
 
+import { cnMixSpace } from '../../../../../mixs/MixSpace/MixSpace';
 import { cnDocsDecorator } from '../../../../../uiKit/components/DocsDecorator/DocsDecorator';
 import { StoryBookExample } from '../../../../../uiKit/components/StoryBookExample/StoryBookExample';
 import { ProgressLine } from '../../../ProgressLine';
 
 export const ProgressLineExampleMode = () => {
   return (
+    <StoryBookExample className={cnDocsDecorator('Section', [cnMixSpace({ mB: '2xl' })])}>
+      <ProgressLine />
+    </StoryBookExample>
+  );
+};
+
+export const ProgressLineExampleModeValue = () => {
+  return (
     <StoryBookExample className={cnDocsDecorator('Section')}>
-      <div>
-        <ProgressLine />
-      </div>
-      <div>
-        <ProgressLine value={30} />
-      </div>
+      <ProgressLine value={30} />
     </StoryBookExample>
   );
 };
