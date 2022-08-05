9936f6814adab85eda3bce516005bc1f6cacb09d
Maxim Kolesnikov <44449561+N1MBER@users.noreply.github.com>
2022-08-05 16:25:51 +0300

    feat(Badge): add  xs size (#2455)
    closes #1952

--- src/components/Badge/__stories__/examples/BadgeExampleSize/BadgeExampleSize.tsx
+++ src/components/Badge/__stories__/examples/BadgeExampleSize/BadgeExampleSize.tsx
@@ -7,6 +7,7 @@ import { Badge } from '../../../Badge';
 
 export const BadgeExampleSize = () => (
   <div className={cnDocsDecorator('Section', [cnDocsExample()])}>
+    <Badge size="xs" label="Badge xs" className={wp.decorator({ 'indent-r': 'm' })} />
     <Badge size="s" label="Badge s" className={wp.decorator({ 'indent-r': 'm' })} />
     <Badge size="m" label="Badge m" className={wp.decorator({ 'indent-r': 'm' })} />
     <Badge size="l" label="Badge l" className={wp.decorator({ 'indent-r': 'm' })} />
