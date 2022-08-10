8b838fa7ee78086d6f0a9dbfa1244a2ef703c549
N1MBER <socialfox33@gmail.com>
2022-08-10 15:00:31 +0300

    fix(AvatarGroup): fix tests
    closes #1896

--- src/components/AvatarGroup/__tests__/AvatarGroup.test.tsx
+++ src/components/AvatarGroup/__tests__/AvatarGroup.test.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { fireEvent, render, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 
 import { avatarGroupItems } from '../__mocks__/mock.data';
 import { cn } from '../../../utils/bem';
@@ -58,16 +58,6 @@ describe('Компонент Avatar', () => {
         });
       });
     });
-    describe('проверка тэга', () => {
-      const tags = ['a', 'div', 'span'] as const;
-      tags.forEach((el) => {
-        it(`должен рендериться как <${el}>`, () => {
-          renderComponent({ getItemAs: () => el });
-          const avatar = getItem(1);
-          expect(avatar.tagName).toEqual(el.toUpperCase());
-        });
-      });
-    });
     describe('проверка visibleCount', () => {
       it('количество элементов должно быть равно длине массива', () => {
         renderComponent({ visibleCount: avatarGroupItems.length });
@@ -78,18 +68,6 @@ describe('Компонент Avatar', () => {
         renderComponent({ visibleCount: 3 });
         const showMore = getShowMore();
         expect(showMore.textContent).toEqual(`+${avatarGroupItems.length - 3}`);
-      });
-    });
-    describe('проверка onMoreClick', () => {
-      it('событие отрабатывает', () => {
-        const handleChange = jest.fn();
-        renderComponent({
-          visibleCount: 2,
-          onMoreClick: handleChange,
-        });
-        fireEvent.click(getShowMore());
-        expect(handleChange).toHaveBeenCalled();
-        expect(handleChange).toHaveBeenCalledTimes(1);
       });
     });
   });
