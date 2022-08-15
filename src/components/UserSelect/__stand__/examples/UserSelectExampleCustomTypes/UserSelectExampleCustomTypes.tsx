0b1d1157350321bd6d74c8635409345263816a50
N1MBER <socialfox33@gmail.com>
2022-08-15 11:38:31 +0300

    docs(custom): userSelect
    Add doc for UserSelect
    
    closes #2448

---
+++ src/components/UserSelect/__stand__/examples/UserSelectExampleCustomTypes/UserSelectExampleCustomTypes.tsx
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+
+import { cnDocsDecorator } from '../../../../../uiKit/components/DocsDecorator/DocsDecorator';
+import { UserSelect } from '../../../UserSelect';
+
+export const UserSelectExampleCustomTypes = () => {
+  const [value, setValue] = useState<string | null>();
+  return (
+    <div className={cnDocsDecorator('Section', { indent: 'bottom' })}>
+      <UserSelect
+        placeholder="Выберите пользователя"
+        items={['Андрей Андреев', 'Егор Егоров', 'Михаил Михайлов']}
+        value={value}
+        onChange={({ value }) => setValue(value)}
+        getItemKey={(item) => item}
+        getItemLabel={(item) => item}
+      />
+    </div>
+  );
+};
+
+// Example 2
+
+const groups = ['Цирк', 'Центр полётов'];
+
+type Item = {
+  firstName: string;
+  lastName: string;
+  group: string;
+};
+
+const items: Item[] = [
+  { firstName: 'Андрей', lastName: 'Андреев', group: 'Цирк' },
+  { firstName: 'Егор', lastName: 'Егоров', group: 'Цирк' },
+  { firstName: 'Михаил', lastName: 'Михайлов', group: 'Центр полётов' },
+];
+
+export const UserSelectExampleCustomTypesWithGroups = () => {
+  const [value, setValue] = useState<Item | null>();
+  return (
+    <div className={cnDocsDecorator('Section')}>
+      <UserSelect
+        placeholder="Выберите пользователя"
+        items={items}
+        groups={groups}
+        value={value}
+        onChange={({ value }) => setValue(value)}
+        getItemKey={(item) => `${item.firstName} ${item.lastName}`}
+        getItemLabel={(item) => `${item.firstName} ${item.lastName}`}
+        getItemGroupKey={(item) => item.group}
+        getItemSubLabel={(item) => item.group}
+        getGroupKey={(group) => group}
+        getGroupLabel={(group) => group}
+      />
+    </div>
+  );
+};
