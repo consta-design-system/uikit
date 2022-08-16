489235e4aa13da9e03930d29468ac95b8b79f80c
N1MBER <socialfox33@gmail.com>
2022-08-16 11:38:36 +0300

    docs(custom): select
    Add doc for Select
    
    closes #2448

---
+++ src/components/Select/__stand__/examples/SelectExampleForm/SelectExampleForm.tsx
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+
+import { cnDocsDecorator } from '../../../../../uiKit/components/DocsDecorator/DocsDecorator';
+import { Select } from '../../../Select';
+
+type Item = {
+  label: string;
+  id: number;
+};
+
+const items: Item[] = [
+  {
+    label: 'Первый',
+    id: 1,
+  },
+  {
+    label: 'Второй',
+    id: 2,
+  },
+  {
+    label: 'Третий',
+    id: 3,
+  },
+];
+
+export const SelectExampleForm = () => {
+  const [value, setValue] = useState<Item | null>();
+  return (
+    <div className={cnDocsDecorator('Section')}>
+      <Select
+        placeholder="Форма round"
+        form="round"
+        items={items}
+        value={value}
+        onChange={({ value }) => setValue(value)}
+      />
+      <Select
+        placeholder="Форма brickDefault"
+        form="brickDefault"
+        items={items}
+        value={value}
+        onChange={({ value }) => setValue(value)}
+      />
+    </div>
+  );
+};
