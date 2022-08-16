3e6f32cd6a6fdcddbb04072121d0c11c37c501f5
N1MBER <socialfox33@gmail.com>
2022-08-16 11:33:32 +0300

    docs(custom): sidebar
    Add doc for Sidebar
    
    closes #2448

---
+++ src/components/Sidebar/__stand__/Sidebar.stand.tsx
@@ -0,0 +1,18 @@
+import { createStand } from '##/stand/standConfig';
+
+import image from './Sidebar.image.svg';
+
+export default createStand({
+  title: 'Sidebar',
+  id: 'Sidebar',
+  group: 'components',
+  image,
+  description: 'Всплывающее окно.',
+  version: '4.0.0',
+  status: 'stable',
+  sandbox:
+    'https://codesandbox.io/embed/react-new?fontsize=14&hidenavigation=1&theme=dark',
+  figma:
+    'https://www.figma.com/embed?embed_host=uikit.consta.design&url=https://www.figma.com/file/v9Jkm2GrymD277dIGpRBSH/Consta-UI-Kit?node-id=20626%3A2498',
+  order: 10,
+});
