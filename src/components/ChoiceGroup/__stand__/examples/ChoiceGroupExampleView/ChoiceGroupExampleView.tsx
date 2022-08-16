eeb3d369b05299ac0a1937be453faa8d880cecc9
Anya Ovsyannikova <arhayka@gmail.com>
2022-08-16 15:37:49 +0300

    docs(custom): fix doc

---
+++ src/components/ChoiceGroup/__stand__/examples/ChoiceGroupExampleView/ChoiceGroupExampleView.tsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+
+import { cnDocsDecorator } from '../../../../../uiKit/components/DocsDecorator/DocsDecorator';
+import { StoryBookExample } from '../../../../../uiKit/components/StoryBookExample/StoryBookExample';
+import { ChoiceGroup } from '../../../ChoiceGroup';
+
+type Item = string;
+
+const items: Item[] = ['один', 'два', 'три'];
+
+export const ChoiceGroupExampleView = () => {
+  const [value, setValue] = useState<Item | null>(items[0]);
+  return (
+    <StoryBookExample className={cnDocsDecorator('Section')}>
+      <div>
+        <ChoiceGroup
+          value={value}
+          onChange={({ value }) => setValue(value)}
+          items={items}
+          getItemLabel={(item) => item}
+          view="primary"
+          name="ChoiceGroupExampleView"
+        />
+      </div>
+      <div>
+        <ChoiceGroup
+          value={value}
+          onChange={({ value }) => setValue(value)}
+          items={items}
+          getItemLabel={(item) => item}
+          view="ghost"
+          name="ChoiceGroupExampleView"
+        />
+      </div>
+      <div>
+        <ChoiceGroup
+          value={value}
+          onChange={({ value }) => setValue(value)}
+          items={items}
+          getItemLabel={(item) => item}
+          view="secondary"
+          name="ChoiceGroupExampleView"
+        />
+      </div>
+    </StoryBookExample>
+  );
+};
