374e21137c6eb341896e06984dfb3719e9474f5f
Maxim Kolesnikov <44449561+N1MBER@users.noreply.github.com>
2022-08-16 16:46:36 +0300

    docs(custom): switch (#2512)
    * docs(custom): switch
    
    Add doc for Switch
    
    closes #2448
    
    * fix(custom): switch
    
    fix table
    
    closes #2448

--- src/components/Switch/__stand__/Switch.stand.tsx
+++ src/components/Switch/__stand__/Switch.stand.tsx
@@ -1,17 +1,17 @@
 import { createStand } from '../../../stand/standConfig';
-import Switch from '../../../uiKit/components/ComponentsGridWithData/data/images/SwitchImage';
+import image from './Switch.image.svg';
 
 export default createStand({
   title: 'Switch',
   id: 'Switch',
   group: 'components',
-  image: Switch,
+  image,
   description: 'Переключатель. Два положения — включено и выключено.',
   version: '4.0.0',
   status: 'stable',
   sandbox:
     'https://codesandbox.io/embed/react-new?fontsize=14&hidenavigation=1&theme=dark',
   figma:
-    'https://www.figma.com/embed?embed_host=uikit.consta.design&url=https://www.figma.com/file/v9Jkm2GrymD277dIGpRBSH/Consta-UI-Kit?node-id=58%3A2269',
+    'https://www.figma.com/embed?embed_host=uikit.consta.design&url=https://www.figma.com/file/v9Jkm2GrymD277dIGpRBSH/Consta-UI-Kit?node-id=20626%3A3378',
   order: 10,
 });
