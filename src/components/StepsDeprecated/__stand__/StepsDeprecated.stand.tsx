25eaee867aa817137d01e36d59d332f974b6f96a
Maxim Kolesnikov <44449561+N1MBER@users.noreply.github.com>
2022-08-16 16:46:59 +0300

    Docs/steps deprecated (#2513)
    * docs(custom): stepsDeprecated
    
    Add doc for StepsDeprecated
    
    closes #2448
    
    * fix(custom):
    
    StepsDeprecated
    
    BREAKING CHANGE:
    fix import
    
    closes #2448
    
    * fix(custom): stepsDeprecatex
    
    fix id
    
    closes #2448

---
+++ src/components/StepsDeprecated/__stand__/StepsDeprecated.stand.tsx
@@ -0,0 +1,18 @@
+import { createStand } from '##/stand/standConfig';
+
+import image from './Steps.image.svg';
+
+export default createStand({
+  title: 'Steps',
+  id: 'Steps',
+  group: 'components',
+  image,
+  description: 'Шаги.',
+  version: '4.0.0',
+  status: 'deprecated',
+  sandbox:
+    'https://codesandbox.io/embed/react-new?fontsize=14&hidenavigation=1&theme=dark',
+  figma:
+    'https://www.figma.com/embed?embed_host=uikit.consta.design&url=https://www.figma.com/file/v9Jkm2GrymD277dIGpRBSH/Consta-UI-Kit?node-id=20626%3A2941',
+  order: 10,
+});
---
+++ src/components/StepsDeprecated/__stand__/examples/ExampleStepsValue/ExampleStepsValue.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import { cnDocsDecorator } from '../../../../../uiKit/components/DocsDecorator/DocsDecorator';
+import { StoryBookExample } from '../../../../../uiKit/components/StoryBookExample/StoryBookExample';
+import { simpleItems as items } from '../../../__mocks__/mock.data';
+import { Steps } from '../../../StepsDeprecated';
+
+const emptyFunction = () => {};
+
+export const ExampleStepsValue = () => (
+  <StoryBookExample className={cnDocsDecorator('Section')}>
+    <Steps
+      items={items}
+      value={items[1]}
+      getLabel={(item) => item}
+      onChange={emptyFunction}
+    />
+  </StoryBookExample>
+);
