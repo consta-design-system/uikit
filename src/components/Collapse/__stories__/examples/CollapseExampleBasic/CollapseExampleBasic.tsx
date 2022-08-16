5190a67636c8448e2feba37f4c7e93c199d7e766
Anya Ovsyannikova <arhayka@gmail.com>
2022-08-16 08:40:06 +0300

    docs(custom): collapse

--- src/components/Collapse/__stories__/examples/CollapseExampleBasic/CollapseExampleBasic.tsx
+++ src/components/Collapse/__stories__/examples/CollapseExampleBasic/CollapseExampleBasic.tsx
@@ -4,16 +4,15 @@ import { content } from '../../../__mocks__/mock.data';
 import { cnDocsDecorator } from '../../../../../uiKit/components/DocsDecorator/DocsDecorator';
 import { Collapse } from '../../../Collapse';
 
-export const CollapseExampleHover = () => {
+export const CollapseExample = () => {
   const [isOpen, setOpen] = useState<boolean>(false);
 
   return (
     <div className={cnDocsDecorator('Section')}>
       <Collapse
-        label="Здесь есть подсветка"
+        label="Это самый настоящий коллапс"
         isOpen={isOpen}
         onClick={() => setOpen(!isOpen)}
-        hoverEffect
       >
         {content}
       </Collapse>
