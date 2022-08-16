6b7404a7c2f3d5a5718ff99efd9a9972ee1d67b4
Maxim Kolesnikov <44449561+N1MBER@users.noreply.github.com>
2022-08-16 16:46:02 +0300

    docs(custom): switchGroupDeprecated (#2509)
    * docs(custom): switchGroupDeprecated
    
    add doc for SwitchGroupDeprecated
    
    closes #2448
    
    * fix(custom): switchGroupDeprecated
    
    Fix id
    
    closes #2448
    
    * fix(custom): switchGroupDeprecated
    
    fix links
    
    closes #2448

---
+++ src/components/SwitchGroupDeprecated/__stand__/SwitchGroupDeprecated.stand.tsx
@@ -0,0 +1,18 @@
+import { createStand } from '##/stand/standConfig';
+
+import image from './SwitchGroup.image.svg';
+
+export default createStand({
+  title: 'SwitchGroup',
+  id: 'SwitchGroup',
+  group: 'components',
+  image,
+  description: 'Группа переключателей',
+  version: '4.0.0',
+  status: 'deprecated',
+  sandbox:
+    'https://codesandbox.io/embed/react-new?fontsize=14&hidenavigation=1&theme=dark',
+  figma:
+    'https://www.figma.com/file/v9Jkm2GrymD277dIGpRBSH/Consta-UI-Kit?node-id=20626%3A3378',
+  order: 10,
+});
