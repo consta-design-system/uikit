f803a7a536bdc38227be02e57f7ab58ec334f1cf
gizeasy <gizeasy@gmail.com>
2022-08-17 13:46:50 +0300

    Update useGlobalKeys.stand.tsx

--- src/hooks/useGlobalKeys/__stand__/useGlobalKeys.stand.tsx
+++ src/hooks/useGlobalKeys/__stand__/useGlobalKeys.stand.tsx
@@ -3,7 +3,7 @@ import { createStand } from '##/stand/standConfig';
 export default createStand({
   title: 'useGlobalKeys',
   id: 'useGlobalKeys',
-  group: 'components',
+  group: 'hooks',
   description: 'Хук для перехвата нажатия клавиш.',
   version: '4.0.0',
   status: 'stable',
