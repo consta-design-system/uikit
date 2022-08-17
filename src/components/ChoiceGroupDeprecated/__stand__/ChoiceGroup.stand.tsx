c8e87d2d1ec2f5d1a4034d6baef5a7943f3bbd20
Anya Ovsyannikova <arhayka@gmail.com>
2022-08-17 12:35:35 +0300

    docs(custom): choicegroup_deprecated

---
+++ src/components/ChoiceGroupDeprecated/__stand__/ChoiceGroup.stand.tsx
@@ -0,0 +1,18 @@
+import { createStand } from '##/stand/standConfig';
+
+import image from './ChoiceGroup.image.svg';
+
+export default createStand({
+  title: 'ChoiceGroup',
+  id: 'ChoiceGroup',
+  group: 'components',
+  image,
+  description: 'Группа кнопок. Можно выбрать один или несколько вариантов.',
+  version: '3.0.28',
+  status: 'deprecated',
+  sandbox:
+    'https://codesandbox.io/embed/react-new?fontsize=14&hidenavigation=1&theme=dark',
+  figma:
+    'https://www.figma.com/embed?embed_host=uikit.consta.design&url=https://www.figma.com/file/v9Jkm2GrymD277dIGpRBSH/Consta-UI-Kit?node-id=5156%3A79693',
+  order: 10,
+});
