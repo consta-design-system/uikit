97a7d202a74ba491431ed1fa22777706fb7f0e16
gizeasy <gizeasy@gmail.com>
2022-08-17 13:38:53 +0300

    Update useDebounce.stand.tsx

--- src/hooks/useDebounce/__stand__/useDebounce.stand.tsx
+++ src/hooks/useDebounce/__stand__/useDebounce.stand.tsx
@@ -3,7 +3,7 @@ import { createStand } from '##/stand/standConfig';
 export default createStand({
   title: 'useDebounce',
   id: 'useDebounce',
-  group: 'components',
+  group: 'hooks',
   description: 'Хук для таймаута функций.',
   version: '4.0.0',
   status: 'stable',
