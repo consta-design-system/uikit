4b47545f9d143ddd53c912d8d9af75d0f77d04f6
N1MBER <socialfox33@gmail.com>
2022-08-17 09:52:00 +0300

    docs(custom): useHideElementsInLine
    add doc for useHideElementsInLine
    
    closes #2448

---
+++ src/hooks/useHideElementsInLine/__stand__/useHideElementsInLine.stand.tsx
@@ -0,0 +1,11 @@
+import { createStand } from '##/stand/standConfig';
+
+export default createStand({
+  title: 'useHideElementsInLine',
+  id: 'useHideElementsInLine',
+  group: 'components',
+  description: 'Хук для сокрытия элементов.',
+  version: '4.0.0',
+  status: 'stable',
+  order: 10,
+});
