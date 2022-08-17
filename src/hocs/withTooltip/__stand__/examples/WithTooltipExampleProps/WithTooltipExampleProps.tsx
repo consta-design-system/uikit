d1c72b74a95e0b9c309736f9874b283dac5c5b39
N1MBER <socialfox33@gmail.com>
2022-08-17 09:03:57 +0300

    docs(custom): withTooltip
    add doc for withTooltip
    
    closes #2448

---
+++ src/hocs/withTooltip/__stand__/examples/WithTooltipExampleProps/WithTooltipExampleProps.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import { Button } from '../../../../../components/Button/Button';
+import { cnDocsDecorator } from '../../../../../uiKit/components/DocsDecorator/DocsDecorator';
+import * as wp from '../../../../../uiKit/whitepaper/whitepaper';
+import { withTooltip } from '../../../withTooltip';
+
+const ButtonWithTooltip = withTooltip({ content: 'Тултип сверху' })(Button);
+
+export const WithTooltipExampleProps = () => {
+  return (
+    <div
+      className={cnDocsDecorator('Section', [
+        wp.tplGrid({ 'ratio': '1-1', 'col-gap': 'full' }),
+      ])}
+    >
+      <ButtonWithTooltip label="Кнопка без свойств" />
+      <ButtonWithTooltip
+        label="Свойство описано в кнопке"
+        tooltipProps={{ content: 'Тултип снизу', direction: 'downCenter' }}
+      />
+    </div>
+  );
+};
