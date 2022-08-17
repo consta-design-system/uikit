51ea48ca8af1335d856acab1a2590c1bee03d54e
Maxim Kolesnikov <44449561+N1MBER@users.noreply.github.com>
2022-08-17 16:38:08 +0300

    docs(custom): useDebounce (#2549)
    * docs(custom): useDebounce
    
    add doc for useDebounce
    
    closes #2448
    
    * Update useDebounce.stand.tsx
    
    Co-authored-by: gizeasy <gizeasy@gmail.com>

---
+++ src/hooks/useDebounce/__stand__/useDebounce.stand.tsx
@@ -0,0 +1,11 @@
+import { createStand } from '##/stand/standConfig';
+
+export default createStand({
+  title: 'useDebounce',
+  id: 'useDebounce',
+  group: 'hooks',
+  description: 'Хук для таймаута функций.',
+  version: '4.0.0',
+  status: 'stable',
+  order: 10,
+});
