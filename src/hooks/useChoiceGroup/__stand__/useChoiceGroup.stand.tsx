04d48a106be135401874751242e0182fb6f9c0f5
gizeasy <gizeasy@gmail.com>
2022-08-17 13:33:11 +0300

    Update useChoiceGroup.stand.tsx

--- src/hooks/useChoiceGroup/__stand__/useChoiceGroup.stand.tsx
+++ src/hooks/useChoiceGroup/__stand__/useChoiceGroup.stand.tsx
@@ -3,7 +3,7 @@ import { createStand } from '##/stand/standConfig';
 export default createStand({
   title: 'useChoiceGroup',
   id: 'useChoiceGroup',
-  group: 'components',
+  group: 'hooks',
   description: 'Отслеживанние размера экрана.',
   version: '4.0.0',
   status: 'stable',
