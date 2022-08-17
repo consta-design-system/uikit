c47f88b64d4fc26a781320f1602b59771db00015
arhayka <arhayka@gmail.com>
2022-08-17 14:02:36 +0300

    docs(custom): choicegroup_deprecated (#2560)

---
+++ src/components/ChoiceGroupDeprecated/__stand__/examples/ChoiceGroupExampleIcon/ChoiceGroupExampleIcon.tsx
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+
+import { IconComponent } from '../../../../../icons/Icon/Icon';
+import { IconCamera } from '../../../../../icons/IconCamera/IconCamera';
+import { IconPhoto } from '../../../../../icons/IconPhoto/IconPhoto';
+import { IconRing } from '../../../../../icons/IconRing/IconRing';
+import { cnDocsDecorator } from '../../../../../uiKit/components/DocsDecorator/DocsDecorator';
+import { StoryBookExample } from '../../../../../uiKit/components/StoryBookExample/StoryBookExample';
+import { ChoiceGroup } from '../../../ChoiceGroupDeprecated';
+
+type Item = {
+  name: string;
+  icon?: IconComponent;
+};
+
+const items: Item[] = [
+  {
+    name: 'Первый',
+    icon: IconPhoto,
+  },
+  {
+    name: 'Второй',
+    icon: IconRing,
+  },
+  {
+    name: 'Третий вариант',
+    icon: IconCamera,
+  },
+];
+
+export const ChoiceGroupExampleIcon = () => {
+  const [value, setValue] = useState<Item | null>(items[0]);
+  return (
+    <StoryBookExample className={cnDocsDecorator('Section')}>
+      <ChoiceGroup
+        value={value}
+        onChange={({ value }) => setValue(value)}
+        items={items}
+        getLabel={(item) => item.name}
+        getIcon={(item) => item.icon}
+        name="ChoiceGroupExampleIcon"
+      />
+      <ChoiceGroup
+        value={value}
+        onChange={({ value }) => setValue(value)}
+        items={items}
+        getLabel={(item) => item.name}
+        getIcon={(item) => item.icon}
+        onlyIcon
+        name="ChoiceGroupExampleIcon"
+      />
+    </StoryBookExample>
+  );
+};
