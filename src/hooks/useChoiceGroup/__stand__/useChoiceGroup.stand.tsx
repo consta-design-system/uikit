a56c72f2c71f45d8bf4bea4db39bd7bca165e4d7
Maxim Kolesnikov <44449561+N1MBER@users.noreply.github.com>
2022-08-17 16:36:59 +0300

    docs(custom): useChoiceGroup (#2546)
    * docs(custom): useChoiceGroup
    
    add doc for useChoiceGroup
    
    closes #2448
    
    * Update useChoiceGroup.stand.tsx
    
    Co-authored-by: gizeasy <gizeasy@gmail.com>

---
+++ src/hooks/useChoiceGroup/__stand__/useChoiceGroup.stand.tsx
@@ -0,0 +1,11 @@
+import { createStand } from '##/stand/standConfig';
+
+export default createStand({
+  title: 'useChoiceGroup',
+  id: 'useChoiceGroup',
+  group: 'hooks',
+  description: 'Отслеживанние размера экрана.',
+  version: '4.0.0',
+  status: 'stable',
+  order: 10,
+});
