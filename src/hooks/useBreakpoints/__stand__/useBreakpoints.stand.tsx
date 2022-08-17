14fa65cdafcf93d29325daa4abc12d8b1d08816f
N1MBER <socialfox33@gmail.com>
2022-08-17 09:14:56 +0300

    docs(custom): useBreakpoints
    add  doc  for useBreakPoints
    
    closes #2448

---
+++ src/hooks/useBreakpoints/__stand__/useBreakpoints.stand.tsx
@@ -0,0 +1,11 @@
+import { createStand } from '##/stand/standConfig';
+
+export default createStand({
+  title: 'useBreakpoints',
+  id: 'useBreakpoints',
+  group: 'components',
+  description: 'Отслеживанние размера экрана.',
+  version: '4.0.0',
+  status: 'stable',
+  order: 10,
+});
