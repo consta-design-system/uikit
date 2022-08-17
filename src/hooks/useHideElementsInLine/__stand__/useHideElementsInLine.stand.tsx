60ce7790605cedccb5646d6f58e27347a7864a4e
Maxim Kolesnikov <44449561+N1MBER@users.noreply.github.com>
2022-08-17 16:38:48 +0300

    docs(custom): useHideElementsInLine (#2553)
    * docs(custom): useHideElementsInLine
    
    add doc for useHideElementsInLine
    
    closes #2448
    
    * Update useHideElementsInLine.stand.tsx
    
    Co-authored-by: gizeasy <gizeasy@gmail.com>

---
+++ src/hooks/useHideElementsInLine/__stand__/useHideElementsInLine.stand.tsx
@@ -0,0 +1,11 @@
+import { createStand } from '##/stand/standConfig';
+
+export default createStand({
+  title: 'useHideElementsInLine',
+  id: 'useHideElementsInLine',
+  group: 'hooks',
+  description: 'Хук для сокрытия элементов.',
+  version: '4.0.0',
+  status: 'stable',
+  order: 10,
+});
