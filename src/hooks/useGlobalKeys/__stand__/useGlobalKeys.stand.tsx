07826bf35ad342201899e411fc3dc3f6d74d6bb4
Maxim Kolesnikov <44449561+N1MBER@users.noreply.github.com>
2022-08-17 16:53:38 +0300

    docs(custom): useGlobalKeys (#2552)
    * docs(custom): useGlobalKeys
    
    add doc for useGlobalKeys
    
    closes #2448
    
    * Update useGlobalKeys.stand.tsx
    
    Co-authored-by: gizeasy <gizeasy@gmail.com>

---
+++ src/hooks/useGlobalKeys/__stand__/useGlobalKeys.stand.tsx
@@ -0,0 +1,11 @@
+import { createStand } from '##/stand/standConfig';
+
+export default createStand({
+  title: 'useGlobalKeys',
+  id: 'useGlobalKeys',
+  group: 'hooks',
+  description: 'Хук для перехвата нажатия клавиш.',
+  version: '4.0.0',
+  status: 'stable',
+  order: 10,
+});
