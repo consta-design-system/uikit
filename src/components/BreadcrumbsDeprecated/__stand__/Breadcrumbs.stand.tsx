bc411f2174c9155a3a1667a69bfcfbe687d7fe2d
Anya Ovsyannikova <arhayka@gmail.com>
2022-08-17 10:28:35 +0300

    docs(custom): breadcrumbs_deprecated

---
+++ src/components/BreadcrumbsDeprecated/__stand__/Breadcrumbs.stand.tsx
@@ -0,0 +1,18 @@
+import { createStand } from '##/stand/standConfig';
+
+import image from './Breadcrumbs.image.svg';
+
+export default createStand({
+  title: 'Breadcrumbs',
+  id: 'Breadcrumbs',
+  group: 'components',
+  image,
+  description: 'Хлебные крошки. Показывают путь к текущей странице.',
+  version: '3.0.28',
+  status: 'deprecated',
+  sandbox:
+    'https://codesandbox.io/embed/react-new?fontsize=14&hidenavigation=1&theme=dark',
+  figma:
+    'https://www.figma.com/embed?embed_host=uikit.consta.design&url=https://www.figma.com/file/v9Jkm2GrymD277dIGpRBSH/Consta-UI-Kit?node-id=7752%3A136131',
+  order: 10,
+});
