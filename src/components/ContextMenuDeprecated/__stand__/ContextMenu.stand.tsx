743131cf2f7df423d5a11063a45adc07a7995d8f
arhayka <arhayka@gmail.com>
2022-08-17 16:41:24 +0300

    docs: add docs for ContextMenuDeprecated (#2563)

---
+++ src/components/ContextMenuDeprecated/__stand__/ContextMenu.stand.tsx
@@ -0,0 +1,18 @@
+import { createStand } from '##/stand/standConfig';
+
+import image from './ContextMenu.image.svg';
+
+export default createStand({
+  title: 'ContextMenu',
+  id: 'ContextMenu',
+  group: 'components',
+  image,
+  description: 'Контекстное меню. Дополнительные действия на странице.',
+  version: '3.0.28',
+  status: 'deprecated',
+  sandbox:
+    'https://codesandbox.io/embed/react-new?fontsize=14&hidenavigation=1&theme=dark',
+  figma:
+    'https://www.figma.com/embed?embed_host=uikit.consta.design&url=https://www.figma.com/file/v9Jkm2GrymD277dIGpRBSH/Consta-UI-Kit?node-id=4894%3A74617',
+  order: 10,
+});
