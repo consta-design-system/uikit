7e85ec72ae7d857afdd145118a7c0c8650f06d9a
gizeasy <gizeasy@gmail.com>
2022-08-17 13:48:19 +0300

    Update useHideElementsInLine.stand.tsx

--- src/hooks/useHideElementsInLine/__stand__/useHideElementsInLine.stand.tsx
+++ src/hooks/useHideElementsInLine/__stand__/useHideElementsInLine.stand.tsx
@@ -3,7 +3,7 @@ import { createStand } from '##/stand/standConfig';
 export default createStand({
   title: 'useHideElementsInLine',
   id: 'useHideElementsInLine',
-  group: 'components',
+  group: 'hooks',
   description: 'Хук для сокрытия элементов.',
   version: '4.0.0',
   status: 'stable',
