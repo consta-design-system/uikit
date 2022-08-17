c720c5a9349a970cfdaabc07094565062c9883e4
gizeasy <gizeasy@gmail.com>
2022-08-17 13:40:35 +0300

    Update useForkRef.stand.tsx

--- src/hooks/useForkRef/__stand__/useForkRef.stand.tsx
+++ src/hooks/useForkRef/__stand__/useForkRef.stand.tsx
@@ -3,7 +3,7 @@ import { createStand } from '##/stand/standConfig';
 export default createStand({
   title: 'useForkRef',
   id: 'useForkRef',
-  group: 'components',
+  group: 'hooks',
   description: 'Хук для создания нескольких функций.',
   version: '4.0.0',
   status: 'stable',
