5d623ad8f0f788a93a12fb483dee39bd3860aac6
Maxim Kolesnikov <44449561+N1MBER@users.noreply.github.com>
2022-08-17 16:34:53 +0300

    docs(custom): withTooltip (#2545)
    * docs(custom): withTooltip
    
    add doc for withTooltip
    
    closes #2448
    
    * feat(stands): add group
    
    Co-authored-by: gizeasy <gizeasy@gmail.com>

---
+++ src/hocs/withTooltip/__stand__/withTooltip.stand.tsx
@@ -0,0 +1,11 @@
+import { createStand } from '##/stand/standConfig';
+
+export default createStand({
+  title: 'withTooltip',
+  id: 'withTooltip',
+  group: 'hocs',
+  description: 'Хок для добавления тултипа.',
+  version: '4.0.0',
+  status: 'stable',
+  order: 10,
+});
