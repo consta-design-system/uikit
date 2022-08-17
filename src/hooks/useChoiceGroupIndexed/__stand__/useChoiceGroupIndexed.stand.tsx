7f06ed54d73a22a608d5b87b6456af72b77fe71e
N1MBER <socialfox33@gmail.com>
2022-08-17 09:26:47 +0300

    docs(custom): useChoiceGroupIndexed
    add doc for useChoiceGroupIndexed
    
    closes #2448

---
+++ src/hooks/useChoiceGroupIndexed/__stand__/useChoiceGroupIndexed.stand.tsx
@@ -0,0 +1,11 @@
+import { createStand } from '##/stand/standConfig';
+
+export default createStand({
+  title: 'useChoiceGroupIndexed',
+  id: 'useChoiceGroupIndexed',
+  group: 'components',
+  description: 'Хук для выбора нескольких значений из списка.',
+  version: '4.0.0',
+  status: 'stable',
+  order: 10,
+});
