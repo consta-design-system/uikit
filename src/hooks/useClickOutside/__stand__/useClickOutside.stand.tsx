d2b37db39e3bf7235327043f286aa6187d0e0f67
N1MBER <socialfox33@gmail.com>
2022-08-17 09:31:58 +0300

    docs(custom): useClickOutside
    closes #2448

---
+++ src/hooks/useClickOutside/__stand__/useClickOutside.stand.tsx
@@ -0,0 +1,11 @@
+import { createStand } from '##/stand/standConfig';
+
+export default createStand({
+  title: 'useClickOutside',
+  id: 'useClickOutside',
+  group: 'components',
+  description: 'Хук для обработки кликов вне элемента.',
+  version: '4.0.0',
+  status: 'stable',
+  order: 10,
+});
