eb789342a42d6a026f7daa74d677a77b60d9e0ba
gizeasy <gizeasy@gmail.com>
2022-08-18 15:08:24 +0300

    docs(mixs): add mixs docs (#2572)

---
+++ src/mixs/MixPopoverAnimate/__stand__/MixPopoverAnimate.stand.tsx
@@ -0,0 +1,10 @@
+import { createStand } from '##/stand/standConfig';
+
+export default createStand({
+  title: 'MixPopoverAnimate',
+  id: 'MixPopoverAnimate',
+  group: 'mixs',
+  version: '4.0.0',
+  status: 'stable',
+  order: 10,
+});
---
+++ src/mixs/MixPopoverArrow/__stand__/MixPopoverArrow.stand.tsx
@@ -0,0 +1,10 @@
+import { createStand } from '##/stand/standConfig';
+
+export default createStand({
+  title: 'MixPopoverArrow',
+  id: 'MixPopoverArrow',
+  group: 'mixs',
+  version: '4.0.0',
+  status: 'stable',
+  order: 10,
+});
