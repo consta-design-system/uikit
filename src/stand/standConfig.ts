3361d4c8b4426f024f12874a0b61a6a31b29dd2c
gizeasy <gizeasy@gmail.com>
2022-09-27 23:57:09 +0300

    chore(deps): update @consta/stand

--- src/stand/standConfig.ts
+++ src/stand/standConfig.ts
@@ -4,49 +4,48 @@ import image from './ConstaImage.png';
 import description from './description.mdx';
 import { StandPageDecoration as standPageDecoration } from './standPageDecoration';
 
-const groups = [
-  {
-    title: 'О дизайн-системе',
-    id: 'about',
-  },
-  {
-    title: 'Компоненты',
-    id: 'components',
-  },
-  {
-    title: 'Миксины',
-    id: 'mixs',
-  },
-  {
-    title: 'Hocs',
-    id: 'hocs',
-  },
-  {
-    title: 'Hooks',
-    id: 'hooks',
-  },
-  {
-    title: 'Тематизация',
-    id: 'theme',
-  },
-  {
-    title: 'Как работать с Consta',
-    id: 'workflow',
-  },
-  {
-    title: 'Кастомизация',
-    id: 'custom',
-  },
-  {
-    title: 'Правила и справочники',
-    id: 'reference',
-  },
-] as const;
-
 export const { createStand } = createConfig({
   title: 'Consta UI Kit',
   id: 'uikit',
-  groups,
+  groups: [
+    {
+      title: 'О дизайн-системе',
+      id: 'about',
+    },
+    {
+      title: 'Компоненты',
+      id: 'components',
+      view: 'card',
+    },
+    {
+      title: 'Миксины',
+      id: 'mixs',
+    },
+    {
+      title: 'Hocs',
+      id: 'hocs',
+    },
+    {
+      title: 'Hooks',
+      id: 'hooks',
+    },
+    {
+      title: 'Тематизация',
+      id: 'theme',
+    },
+    {
+      title: 'Как работать с Consta',
+      id: 'workflow',
+    },
+    {
+      title: 'Кастомизация',
+      id: 'custom',
+    },
+    {
+      title: 'Правила и справочники',
+      id: 'reference',
+    },
+  ],
   group: 'библиотеки компонентов',
   image,
   description,
