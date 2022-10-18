99e3855088d1afb40e381e985d780695b94aa8bf
N1MBER <socialfox33@gmail.com>
2022-10-18 13:57:53 +0300

    fix(variants): remove filefield variants and fix header varinat

--- src/components/Header/__stand__/Header.variants.tsx
+++ src/components/Header/__stand__/Header.variants.tsx
@@ -1,8 +1,8 @@
 import './HeaderVariants.css';
 
+import { useSelect } from '@consta/stand';
 import React from 'react';
 
-import { Text } from '##/components/Text/Text';
 import { cn } from '##/utils/bem';
 
 import { HeaderFullExample } from './examples/HeaderFullExample/HeaderFullExample';
@@ -13,31 +13,34 @@ import { HeaderWithoutSearchExample } from './examples/HeaderWithoutSearchExampl
 
 const cnHeaderVariants = cn('HeaderVariants');
 
+const types = [
+  'Полный',
+  'Без поиска',
+  'С минилогином',
+  'Без меню',
+  'С уникальным лого',
+] as const;
+
+const getVariant = (type?: typeof types[number]) => {
+  if (type === 'Полный') {
+    return <HeaderFullExample />;
+  }
+  if (type === 'Без поиска') {
+    return <HeaderWithoutSearchExample />;
+  }
+  if (type === 'С минилогином') {
+    return <HeaderMinifyLoginExample />;
+  }
+  if (type === 'Без меню') {
+    return <HeaderWithoutMenuExample />;
+  }
+  return <HeaderWithLogoExample />;
+};
+
 const Variants = () => {
-  return (
-    <div className={cnHeaderVariants()}>
-      <Text size="3xl" lineHeight="l">
-        Полный
-      </Text>
-      <HeaderFullExample />
-      <Text size="3xl" lineHeight="l">
-        Без поиска
-      </Text>
-      <HeaderWithoutSearchExample />
-      <Text size="3xl" lineHeight="l">
-        С минилогином
-      </Text>
-      <HeaderMinifyLoginExample />
-      <Text size="3xl" lineHeight="l">
-        Без меню
-      </Text>
-      <HeaderWithoutMenuExample />
-      <Text size="3xl" lineHeight="l">
-        С уникальным лого
-      </Text>
-      <HeaderWithLogoExample />
-    </div>
-  );
+  const type = useSelect('type', types, types[0]);
+
+  return <div className={cnHeaderVariants()}>{getVariant(type)}</div>;
 };
 
 export default Variants;
