92507e66684bb46cb50ac6f3a6304400dd7ec2c6
arhayka <arhayka@gmail.com>
2022-10-19 17:08:01 +0300

    docs(TableComparison): write how to choose table (#2710)
    Co-authored-by: gizeasy <gizeasy@gmail.com>

---
+++ src/docs/tableComparison/tableComparison.stand.tsx
@@ -0,0 +1,8 @@
+import { createStand } from '##/stand/standConfig';
+
+export default createStand({
+  title: 'Какую таблицу выбрать',
+  id: 'tableComparison',
+  group: 'workflow',
+  order: 70,
+});
