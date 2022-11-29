cd5d47832d4e82bd586fb1908f4e18ae7f6457dc
gizeasy <gizeasy@gmail.com>
2022-11-29 12:24:57 +0300

    Update isEq.ts

--- src/hooks/useBreakpoints/isEq.ts
+++ src/hooks/useBreakpoints/isEq.ts
@@ -4,12 +4,10 @@ export const isEq = <POINTS extends string>(
   state: Returned<POINTS>,
   newState: Returned<POINTS>,
 ) => {
-  let eq = true;
   for (const key in newState) {
     if (state[key] !== newState[key]) {
-      eq = false;
-      return eq;
+      return false;
     }
   }
-  return eq;
+  return true;
 };
