ef61677d305c2fc76cce6125b0b22ceef1173e17
Maxim Kolesnikov <socialfox33@gmail.com>
2023-02-16 17:33:15 +0300

    docs(ChoiceGroup): update link to sandbox

--- src/components/ChoiceGroup/__stand__/ChoiceGroup.stand.tsx
+++ src/components/ChoiceGroup/__stand__/ChoiceGroup.stand.tsx
@@ -7,7 +7,7 @@ export default createStand({
   description: 'Группа кнопок. Можно выбрать один или несколько вариантов.',
   version: '1.5.0',
   status: 'stable',
-  sandbox: 'collapse-e2duqr',
+  sandbox: 'choicegroup-i10glc',
   figma:
     'https://www.figma.com/embed?embed_host=uikit.consta.design&url=https://www.figma.com/file/v9Jkm2GrymD277dIGpRBSH/Consta-UI-Kit?node-id=5156%3A79693',
   order: 10,
