5054901961da6cd47486bb07309655ab03a74ed9
Maxim Kolesnikov <socialfox33@gmail.com>
2023-06-21 10:25:00 +0300

    docs(Spoiler): update variants example

--- src/components/SpoilerCanary/__stand__/Spoiler.variants.tsx
+++ src/components/SpoilerCanary/__stand__/Spoiler.variants.tsx
@@ -1,12 +1,18 @@
+import './SpoilerVariants.css';
+
 import { IconArrowDown } from '@consta/icons/IconArrowDown';
 import { IconArrowUp } from '@consta/icons/IconArrowUp';
 import { useBoolean, useSelect, useText } from '@consta/stand';
 import React from 'react';
 
+import { Text } from '##/components/Text';
 import { useFlag } from '##/hooks/useFlag';
+import { cn } from '##/utils/bem';
 
 import { Spoiler } from '../Spoiler';
 import { defaultSpoilerPropSize, spolierPropSize } from '../types';
+
+const cnSpoilerVariants = cn('SpoilerVariants');
 
 const Variants = () => {
   const size = useSelect('size', spolierPropSize, defaultSpoilerPropSize);
@@ -17,15 +23,22 @@ const Variants = () => {
   const [isOpen, setIsOpen] = useFlag();
 
   return (
-    <Spoiler
-      size={size}
-      open={isOpen}
-      onClick={setIsOpen.toggle}
-      lessLabel={lessLabel}
-      moreLabel={moreLabel}
-      moreIcon={withIcons ? IconArrowDown : undefined}
-      lessIcon={withIcons ? IconArrowUp : undefined}
-    />
+    <div className={cnSpoilerVariants()}>
+      <Text weight="semibold">Какой-то текст</Text>
+      <Text className={cnSpoilerVariants('Text', { open: isOpen })}>
+        Здесь может быть что угодно. Например, этот текст. Но не обязательно: вы
+        можете добавить иконки, кнопки, картинки или даже мини-игру (ну вдруг).
+      </Text>
+      <Spoiler
+        size={size}
+        open={isOpen}
+        onClick={setIsOpen.toggle}
+        lessLabel={lessLabel}
+        moreLabel={moreLabel}
+        moreIcon={withIcons ? IconArrowDown : undefined}
+        lessIcon={withIcons ? IconArrowUp : undefined}
+      />
+    </div>
   );
 };
 
