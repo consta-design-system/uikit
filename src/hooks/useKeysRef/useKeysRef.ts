c217510908ac8e43a3bf87ac56d5afdbdee567dc
Maxim Kolesnikov <socialfox33@gmail.com>
2023-09-06 19:45:11 +0300

    fix(DatePciker): fix keyboard events

--- src/hooks/useKeysRef/useKeysRef.ts
+++ src/hooks/useKeysRef/useKeysRef.ts
@@ -12,6 +12,7 @@ type UseKeysProps = {
   ref?: React.RefObject<HTMLElement>;
   keys?: KeyHandlers;
   isActive?: boolean;
+  eventHandler?: (event: KeyboardEvent) => void;
   eventType?: 'keypress' | 'keydown' | 'keyup';
 };
 
@@ -22,13 +23,20 @@ const getKeyHandler = (keys: KeyHandlers) => {
 };
 
 export const useKeysRef = (params: UseKeysProps) => {
-  const { ref, keys, isActive = true, eventType = 'keydown' } = params;
+  const {
+    ref,
+    keys,
+    isActive = true,
+    eventHandler,
+    eventType = 'keydown',
+  } = params;
 
   const onKeyDown = useCallback(
     (e: Event) => {
       if (keys && isActive) {
         const event = e as unknown as KeyboardEvent;
         getKeyHandler(keys)(event);
+        eventHandler?.(event);
       }
     },
     [keys, isActive],
