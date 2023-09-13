c2b3291e4a2747964208a40d9444376410d5903c
Maxim Kolesnikov <socialfox33@gmail.com>
2023-09-13 11:44:31 +0300

    feat(Button): added new prop

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -91,6 +91,7 @@ export type Props = {
   iconSize?: IconPropSize;
   title?: string;
   children?: never;
+  type?: 'button' | 'reset' | 'submit';
 };
 
 export const COMPONENT_NAME = 'Button' as const;
@@ -125,6 +126,7 @@ export const Button = forwardRefWithAs<Props, 'button'>((props, ref) => {
     onClick,
     disabled,
     loading,
+    type = 'button',
     tabIndex,
     as = 'button',
     onlyIcon,
@@ -152,6 +154,7 @@ export const Button = forwardRefWithAs<Props, 'button'>((props, ref) => {
     <Tag
       {...otherProps}
       onClick={onClick ? handleClick : undefined}
+      type={type}
       className={cnButton(
         {
           size,
