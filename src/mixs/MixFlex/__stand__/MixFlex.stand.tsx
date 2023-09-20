a18bde18b22bf3bfe1a2ca03ad7f9a4d767a20e9
Maxim Kolesnikov <socialfox33@gmail.com>
2023-09-20 18:20:31 +0300

    feat(MixFlex): add new mixin
    closes #3291

---
+++ src/mixs/MixFlex/__stand__/MixFlex.stand.tsx
@@ -0,0 +1,11 @@
+import { createStand } from '##/stand/standConfig';
+
+export default createStand({
+  title: 'MixFlex',
+  id: 'MixFlex',
+  group: 'mixs',
+  version: '4.31.0',
+  status: 'stable',
+  alias: ['миксин', 'контейнер', 'флекс', 'лаяут', 'положение', 'flex'],
+  order: 10,
+});
