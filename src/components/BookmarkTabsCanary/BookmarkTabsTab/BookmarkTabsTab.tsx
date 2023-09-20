3913dc4397e1d773999ac2e0ace5a825681b7aa0
Maxim Kolesnikov <44449561+N1MBER@users.noreply.github.com>
2023-09-20 14:56:42 +0300

    feat(BookmarkTabs): add tab navigation to close button (#3256)

--- src/components/BookmarkTabsCanary/BookmarkTabsTab/BookmarkTabsTab.tsx
+++ src/components/BookmarkTabsCanary/BookmarkTabsTab/BookmarkTabsTab.tsx
@@ -1,7 +1,7 @@
 import './BookmarkTabsTab.css';
 
 import { IconClose } from '@consta/icons/IconClose';
-import React, { forwardRef } from 'react';
+import React, { forwardRef, useRef } from 'react';
 
 import { Button } from '##/components/Button';
 import { Text } from '##/components/Text';
@@ -15,13 +15,13 @@ export const cnBookmarkTabsTab = cn('BookmarkTabsTab');
 
 const BookmarkTabsTabRender = (
   props: BookmarkTabsTabProps,
-  ref: React.Ref<HTMLButtonElement>,
+  ref: React.Ref<HTMLDivElement>,
 ) => {
   const {
     label,
     leftIcon: LeftIcon,
     rightIcon: RightIcon,
-    as: Tag = 'button',
+    as: Tag = 'div',
     onClick,
     size,
     form,
@@ -30,6 +30,7 @@ const BookmarkTabsTabRender = (
     tabRef,
     controlRef,
     onClose,
+    onKeyDown: onKeyDownProp,
     view,
     className,
     bordered,
@@ -37,6 +38,31 @@ const BookmarkTabsTabRender = (
     style,
     ...otherProps
   } = props;
+
+  const closeButtonRef = useRef<HTMLButtonElement>(null);
+  const tagRef = useRef<HTMLDivElement>(null);
+
+  const onCloseKeydown: React.KeyboardEventHandler = (e) => {
+    const { code } = e;
+    e.stopPropagation();
+    e.preventDefault();
+    if (code === 'Tab' || code === 'Escape') {
+      tagRef?.current?.focus();
+    }
+    if (code === 'Enter' || code === 'Space') {
+      onClose?.(e);
+    }
+  };
+
+  const onKeyDown: React.KeyboardEventHandler<HTMLDivElement> = (e) => {
+    const { code } = e;
+    onKeyDownProp?.(e);
+    if (onClose && code === 'Enter') {
+      e.stopPropagation();
+      e.preventDefault();
+      closeButtonRef.current?.focus();
+    }
+  };
 
   return (
     <Tag
@@ -54,10 +80,10 @@ const BookmarkTabsTabRender = (
         },
         [className],
       )}
-      role="tab"
-      type="button"
+      role="button"
       onClick={onClick}
-      ref={useForkRef([ref, tabRef, controlRef])}
+      onKeyDown={onKeyDown}
+      ref={useForkRef([ref, tabRef, controlRef, tagRef])}
       style={{
         ['--bookmarks-tab-width' as string]: tabWidth,
         ...style,
@@ -90,8 +116,10 @@ const BookmarkTabsTabRender = (
             <Button
               size="xs"
               onlyIcon
-              as="div"
+              ref={closeButtonRef}
               view="clear"
+              onKeyDown={onCloseKeydown}
+              tabIndex={-1}
               className={cnBookmarkTabsTab('Button', [
                 cnMixSpace({ mL: 'xs' }),
               ])}
