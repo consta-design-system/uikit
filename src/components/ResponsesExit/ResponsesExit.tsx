04c832e480d19a08e6c81ad72e3a71eff7bf9a33
gizeasy <gizeasy@gmail.com>
2023-10-03 11:52:43 +0300

    feat(Responses): added images reexports (#3308)
    closed #3307

--- src/components/ResponsesExit/ResponsesExit.tsx
+++ src/components/ResponsesExit/ResponsesExit.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
-import { ResponsesImageExit } from '../../responsesImages/ResponsesImageExit/ResponsesImageExit';
-import { Button } from '../Button/Button';
-import { createResponses } from '../Responses/createResponses';
+import { Button } from '##/components/Button';
+import { createResponses } from '##/components/Responses/createResponses';
+import { ResponsesImageExit } from '##/responsesImages/ResponsesImageExit/ResponsesImageExit';
 
 export const ResponsesExit = createResponses({
   name: 'ResponsesExit',
