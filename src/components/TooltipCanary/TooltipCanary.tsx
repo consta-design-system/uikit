197b3f18b8027e1377c4b246b0aa5f2cc4360753
Maxim Kolesnikov <44449561+N1MBER@users.noreply.github.com>
2023-10-23 14:34:01 +0300

    feat(TooltipCanary): added canary tooltip with animations (#3334)
    closes #3303

---
+++ src/components/TooltipCanary/TooltipCanary.tsx
@@ -0,0 +1,103 @@
+import './Tooltip.css';
+
+import React, { forwardRef, useMemo, useState } from 'react';
+import { Transition } from 'react-transition-group';
+
+import { animateTimeout, cnMixPopoverAnimate } from '##/mixs/MixPopoverAnimate';
+import { cnMixPopoverArrow } from '##/mixs/MixPopoverArrow/MixPopoverArrow';
+import { cnCanary } from '##/utils/bem';
+
+import { Direction, Popover } from '../Popover';
+import { Text } from '../Text';
+import {
+  generateDeps,
+  generateThemeClassNames,
+  ThemeContext,
+  useTheme,
+} from '../Theme';
+import { TooltipProps, tooltipPropSizesDefault } from './types';
+
+const cnTooltip = cnCanary('Tooltip');
+
+const ARROW_SIZE = 6;
+const ARROW_OFFSET = 8;
+
+export const Tooltip = forwardRef<HTMLDivElement, TooltipProps>(
+  (props, ref) => {
+    const {
+      isOpen,
+      size = tooltipPropSizesDefault,
+      status,
+      onSetDirection: onSetDirectionProp,
+      offset = 0,
+      style,
+      className,
+      children,
+      ...otherProps
+    } = props;
+
+    const { theme } = useTheme();
+
+    const [direction, setDirection] = useState<Direction | undefined>();
+
+    const onSetDirection = (direction: Direction) => {
+      onSetDirectionProp?.(direction);
+      setDirection(direction);
+    };
+
+    const value = useMemo(() => {
+      const tooltipTheme = {
+        ...theme,
+        color: {
+          primary: status ? theme.color.accent : theme.color.invert,
+          accent: theme.color.accent,
+          invert: theme.color.primary,
+        },
+      };
+      return {
+        theme: tooltipTheme,
+        themeClassNames: generateThemeClassNames(tooltipTheme),
+      };
+    }, [generateDeps(theme), status]);
+
+    return (
+      <Transition in={isOpen} unmountOnExit timeout={animateTimeout}>
+        {(animate) => (
+          <ThemeContext.Provider value={value}>
+            <Popover
+              arrowOffset={ARROW_OFFSET + ARROW_SIZE}
+              offset={ARROW_SIZE + ARROW_OFFSET / 2 + offset}
+              onSetDirection={onSetDirection}
+              ref={ref}
+              className={cnTooltip({ status }, [
+                className,
+                cnMixPopoverAnimate({ animate }),
+              ])}
+              style={{
+                ['--popover-arrow-size' as string]: `${ARROW_SIZE}px`,
+                ['--popover-arrow-offset' as string]: `${ARROW_OFFSET}px`,
+                ...style,
+              }}
+              {...otherProps}
+            >
+              <div className={cnTooltip('Background')} />
+              <div
+                className={cnTooltip('Arrow', [
+                  cnMixPopoverArrow({ direction }),
+                ])}
+              />
+              <div className={cnTooltip('Content', { size })}>
+                {typeof children === 'string' ||
+                typeof children === 'number' ? (
+                  <Text size="xs">{children}</Text>
+                ) : (
+                  children
+                )}
+              </div>
+            </Popover>
+          </ThemeContext.Provider>
+        )}
+      </Transition>
+    );
+  },
+);
---
+++ src/components/TooltipCanary/__stand__/Tooltip.variants.tsx
@@ -0,0 +1,57 @@
+import { useNumber, useSelect, useText } from '@consta/stand';
+import React, { useRef, useState } from 'react';
+
+import { Button } from '##/components/Button/Button';
+
+import { directions } from '../../Popover/Popover';
+import { Tooltip } from '../TooltipCanary';
+import {
+  tooltipPropSizes,
+  tooltipPropSizesDefault,
+  tooltipPropStatus,
+} from '../types';
+
+const Variants = () => {
+  const anchorRef = useRef<HTMLButtonElement>(null);
+  const [isTooltipVisible, setIsTooltipVisible] = useState(false);
+  const size = useSelect('size', tooltipPropSizes, tooltipPropSizesDefault);
+  const direction = useSelect('direction', directions, 'upCenter');
+  const spareDirection = useSelect(
+    'spareDirection',
+    directions,
+    'downStartLeft',
+  );
+
+  const status = useSelect('status', tooltipPropStatus);
+
+  const children = useText('children', 'Текст тултипа');
+  const offset = useNumber('offset', 0);
+
+  const handleClickOnAnchor = () => {
+    setIsTooltipVisible(!isTooltipVisible);
+  };
+
+  return (
+    <>
+      <Button
+        label="Нажми меня"
+        type="button"
+        onClick={handleClickOnAnchor}
+        ref={anchorRef}
+      />
+      <Tooltip
+        isOpen={isTooltipVisible}
+        size={size}
+        anchorRef={anchorRef}
+        direction={direction}
+        spareDirection={spareDirection}
+        status={status}
+        offset={offset}
+      >
+        {children}
+      </Tooltip>
+    </>
+  );
+};
+
+export default Variants;
