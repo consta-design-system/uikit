5e31a20e1279c4c4c7c6bece177a320f456a83ba
alyonurchick1 <Alyonurchick1@yandex.ru>
2024-04-25 22:27:08 +0500

    Исправление по комментариям в PR

--- src/components/DragNDropFieldCanary/__stand__/DragNDropField.stand.tsx
+++ src/components/DragNDropFieldCanary/__stand__/DragNDropField.stand.tsx
@@ -6,9 +6,8 @@ export default createStand({
   group: 'components',
   description:
     'Компонент позволяет загружать файлы путем перетаскивания или через диалоговое окно',
-  version: '2.1.0',
+  version: '5.3.0',
   status: 'canary',
-  sandbox: 'dragndropfield-gst04t',
   alias: ['поле', 'ввод', 'файл', 'файлы', 'загрузка', 'выбор'],
   figma:
     'https://www.figma.com/file/v9Jkm2GrymD277dIGpRBSH/Consta-UI-Kit?type=design&node-id=32774-0&mode=design',
