a75f36ba64053c01024e6f954069e7e191b1dbd2
gizeasy <gizeasy@gmail.com>
2024-10-03 17:00:04 +0300

    Update index.ts

--- src/components/TextFieldCanary/index.ts
+++ src/components/TextFieldCanary/index.ts
@@ -4,5 +4,6 @@ export * from './TextFieldTypeText';
 export * from './TextFieldTypeTextArea';
 export * from './TextFieldTypePassword';
 export * from './TextFieldTypeNumber';
+export * from './TextFieldTypeTextArray';
 
 export * from './types';
