0617120ac4f82b86bd549e25b26c17d4fba94d66
gizeasy <gizeasy@gmail.com>
2024-10-24 14:51:15 +0300

    feat(Informer): add _view_outline
    closes #3511

--- src/components/Informer/__stand__/examples/InformerExampleView/InformerExampleView.tsx
+++ src/components/Informer/__stand__/examples/InformerExampleView/InformerExampleView.tsx
@@ -3,7 +3,6 @@ import React from 'react';
 
 import { cnMixSpace } from '##/mixs/MixSpace';
 
-import { cnDocsExample } from '../../../../../uiKit/components/DocsExample/DocsExample';
 import { Informer } from '../../../Informer';
 
 export function InformerExampleView() {
@@ -16,7 +15,19 @@ export function InformerExampleView() {
           status="success"
           className={cnMixSpace({ mB: 'm' })}
         />
-        <p className={cnDocsExample('Caption')}>
+        <p>
+          Такой лучше использовать, когда сообщение — часть контента и находится
+          на одном уровне с ним.
+        </p>
+      </>
+      <>
+        <Informer
+          label="Всё заполнено"
+          view="outline"
+          status="success"
+          className={cnMixSpace({ mB: 'm' })}
+        />
+        <p>
           Такой лучше использовать, когда сообщение — часть контента и находится
           на одном уровне с ним.
         </p>
@@ -28,9 +39,7 @@ export function InformerExampleView() {
           status="success"
           className={cnMixSpace({ mB: 'm' })}
         />
-        <p className={cnDocsExample('Caption')}>
-          А такой — когда сообщение должно быть на уровень выше контента.
-        </p>
+        <p>А такой — когда сообщение должно быть на уровень выше контента.</p>
       </>
     </Example>
   );
