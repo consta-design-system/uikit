b46890555deac4530b4f82fb959482ccddd47780
Dmitry Belkin <62993219+belk1ng@users.noreply.github.com>
2024-11-19 14:33:06 +0300

    feat(StepsStep): add wrapper to step index (#3821)
    Добавил обертку для порядкового номера шага.

--- src/components/Steps/StepsStep/StepsStep.tsx
+++ src/components/Steps/StepsStep/StepsStep.tsx
@@ -60,9 +60,9 @@ export const StepsStep = React.forwardRef<HTMLButtonElement, Props>(
         {completed ? (
           <IconCheck className={cnStepsStep('Icon')} size={iconSize} />
         ) : (
-          step
+          <span className={cnStepsStep('Number')}>{step}</span>
         )}
-        <span>{label}</span>
+        <span className={cnStepsStep('Label')}>{label}</span>
       </button>
     );
   },
