bbd831e18dd1cd43056a0f0cb28c54ccd5289203
alyonurchick1 <Alyonurchick1@yandex.ru>
2024-11-25 15:46:40 +0300

    правка слов

--- src/components/AutoCompleteCanary/__stand__/AutoComplete.stand.tsx
+++ src/components/AutoCompleteCanary/__stand__/AutoComplete.stand.tsx
@@ -7,7 +7,7 @@ export default createStand({
   description: 'Поле для ввода текста с поиском по возможным значениям.',
   version: '5.11.0',
   status: 'canary',
-  alias: ['поле', 'ввод', 'инпут', 'коррекция', 'выбор', 'лист', 'автокоплит'],
+  alias: ['поле', 'ввод', 'инпут', 'коррекция', 'выбор', 'лист', 'автокомплит'],
   sandbox: 'autocomplete-7qm1vl',
   figma:
     'https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2Fv9Jkm2GrymD277dIGpRBSH%2FConsta-UI-Kit%3Fnode-id%3D96749%253A188498',
