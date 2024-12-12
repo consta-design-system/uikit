3728cab89756bd790eea304636186bf88201ebea
gizeasy <gizeasy@gmail.com>
2024-12-12 13:20:28 +0300

    feat(Attachment): add actions (#3846)
    closes #3619
    
    ---------
    
    Co-authored-by: alyonurchick1 <Alyonurchick1@yandex.ru>

--- src/components/Attachment/types.ts
+++ src/components/Attachment/types.ts
@@ -1,9 +1,17 @@
 import { IconComponent } from '@consta/icons/Icon';
+import React from 'react';
 
 export const attachmentPropSize = ['s', 'xs', 'm'] as const;
 export type AttachmentPropSize = typeof attachmentPropSize[number];
 export const attachmentPropSizeDefault: AttachmentPropSize =
   attachmentPropSize[0];
+
+export type AttachmentActions = {
+  onClick: React.MouseEventHandler<HTMLButtonElement>;
+  icon: IconComponent;
+  title?: string;
+  ref?: React.Ref<HTMLButtonElement>;
+};
 
 export type AttachmentProps = {
   fileExtension?: string;
@@ -15,10 +23,23 @@ export type AttachmentProps = {
   errorText?: string;
   size?: AttachmentPropSize;
   loadingText?: string;
-  onButtonClick?: React.EventHandler<React.MouseEvent>;
-  buttonIcon?: IconComponent;
-  buttonTitle?: string;
-  withAction?: boolean;
   className?: string;
   children?: never;
+  actions?: AttachmentActions[];
+  /**
+   * @deprecated use actions or onClick
+   */
+  withAction?: boolean;
+  /**
+   * @deprecated use actions
+   */
+  onButtonClick?: React.EventHandler<React.MouseEvent>;
+  /**
+   * @deprecated use actions
+   */
+  buttonIcon?: IconComponent;
+  /**
+   * @deprecated use actions
+   */
+  buttonTitle?: string;
 };
