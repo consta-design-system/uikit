fe47f652dab5e9e99aac872efdd1f03fd15f1fbd
alyonurchick1 <121174918+alyonurchick1@users.noreply.github.com>
2025-07-10 14:55:43 +0300

    docs (Checkbox): implementation of editorial policy (#3990)
    Co-authored-by: gizeasy <gizeasy@gmail.com>

--- src/components/Checkbox/__stand__/examples/CheckboxExampleAlign/CheckboxExampleAlign.tsx
+++ src/components/Checkbox/__stand__/examples/CheckboxExampleAlign/CheckboxExampleAlign.tsx
@@ -4,14 +4,16 @@ import React from 'react';
 import { Checkbox } from '../../../Checkbox';
 
 export const CheckboxExampleAlign = () => (
-  <Example style={{ width: 300 }}>
+  <Example>
     <Checkbox
+      style={{ maxWidth: 300 }}
       align="center"
       checked
       label="Длинный текст для чекбокса с выравниванием по центру, здесь несколько строк"
       onChange={() => console.log('onChange')}
     />
     <Checkbox
+      style={{ maxWidth: 300 }}
       align="top"
       checked
       label="Длинный текст для чекбокса с выравниванием по верху, здесь несколько строк"
