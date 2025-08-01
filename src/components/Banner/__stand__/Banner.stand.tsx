49b7e5a47a32e775a7fe5302531601528f116172
gizeasy <gizeasy@gmail.com>
2025-08-01 14:11:43 +0300

    update description

--- src/components/Banner/__stand__/Banner.stand.tsx
+++ src/components/Banner/__stand__/Banner.stand.tsx
@@ -4,7 +4,7 @@ export default createStand({
   title: 'Banner',
   id: 'Banner',
   group: 'components',
-  description: 'Баннеры отображают срочные и важные сообщения',
+  description: 'Компонент который отображает срочные и важные сообщения',
   version: '5.25.0',
   status: 'stable',
   alias: ['баннер'],
