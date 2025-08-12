f2a975f86488dcc10963d7cb6d35db5789a72684
alyonurchick1 <Alyonurchick1@yandex.ru>
2025-08-12 16:43:53 +0300

    doc

--- src/components/Banner/__stand__/Banner.stand.tsx
+++ src/components/Banner/__stand__/Banner.stand.tsx
@@ -4,7 +4,8 @@ export default createStand({
   title: 'Banner',
   id: 'Banner',
   group: 'components',
-  description: 'Компонент который отображает срочные и важные сообщения',
+  description:
+    'Компонент, который отображает срочные и важные оповещения в верхней или нижней части страницы',
   version: '5.25.0',
   status: 'stable',
   alias: ['баннер'],
