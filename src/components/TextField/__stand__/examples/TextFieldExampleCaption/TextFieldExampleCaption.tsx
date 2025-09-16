5292347196a4c213171c56c02784e590c8e75305
alyonurchick1 <Alyonurchick1@yandex.ru>
2025-09-16 14:04:13 +0300

    docs(TextField): implementation of editorial policy

--- src/components/TextField/__stand__/examples/TextFieldExampleCaption/TextFieldExampleCaption.tsx
+++ src/components/TextField/__stand__/examples/TextFieldExampleCaption/TextFieldExampleCaption.tsx
@@ -11,27 +11,6 @@ export const TextFieldExampleCaption = () => {
       <TextField
         onChange={setValue}
         value={value}
-        status="success"
-        placeholder="Статус success"
-        caption="Это подпись"
-      />
-      <TextField
-        onChange={setValue}
-        value={value}
-        status="alert"
-        placeholder="Статус alert"
-        caption="Это подпись"
-      />
-      <TextField
-        onChange={setValue}
-        value={value}
-        status="warning"
-        placeholder="Статус warning"
-        caption="Это подпись"
-      />
-      <TextField
-        onChange={setValue}
-        value={value}
         placeholder="Обычное поле"
         caption="Это подпись"
       />
