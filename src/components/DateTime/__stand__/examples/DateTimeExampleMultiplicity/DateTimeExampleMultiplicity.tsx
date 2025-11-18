aabf7be9aa40a943f3a0f015f2618e41b447807c
ShavrinAleksei <apshavrinwork@gmail.com>
2025-11-18 14:49:56 +0000

    docs(DateTime): add state management to time options examples

--- src/components/DateTime/__stand__/examples/DateTimeExampleMultiplicity/DateTimeExampleMultiplicity.tsx
+++ src/components/DateTime/__stand__/examples/DateTimeExampleMultiplicity/DateTimeExampleMultiplicity.tsx
@@ -1,13 +1,16 @@
 import { Example } from '@consta/stand';
-import React from 'react';
+import React, { useState } from 'react';
 
 import { DateTime } from '../../../DateTime';
 
 export const DateTimeExampleMultiplicity = () => {
+  const [value, setValue] = useState<Date | undefined>();
   return (
     <Example>
       <DateTime
         type="time"
+        value={value}
+        onChange={setValue}
         multiplicityHours={2}
         multiplicityMinutes={5}
         multiplicitySeconds={0}
--- src/components/DateTime/__stand__/examples/DateTimeExampleTimeOptions/DateTimeExampleTimeOptions.tsx
+++ src/components/DateTime/__stand__/examples/DateTimeExampleTimeOptions/DateTimeExampleTimeOptions.tsx
@@ -1,11 +1,12 @@
 import { Example } from '@consta/stand';
-import React from 'react';
+import React, { useState } from 'react';
 
 import { DateTime } from '../../../DateTime';
 
 const timeOptionsExamples = [
   {
     label: 'Пример 1',
+    index: 1,
     options: {
       hours: { start: 9, step: 2, stop: 18 }, // 9, 11, 13, 15, 17
       minutes: [0, 15, 45], // конкретные значения минут
@@ -14,6 +15,7 @@ const timeOptionsExamples = [
   },
   {
     label: 'Пример 2',
+    index: 2,
     options: {
       hours: { step: 1 }, // все часы
       minutes: [], // пустой массив — минуты не показываются
@@ -22,6 +24,7 @@ const timeOptionsExamples = [
   },
   {
     label: 'Пример 3',
+    index: 3,
     options: {
       hours: { step: 1 }, // все часы
       minutes: { step: 15 }, // каждые 15 минут
@@ -30,6 +33,7 @@ const timeOptionsExamples = [
   },
   {
     label: 'Пример 4',
+    index: 4,
     options: {
       hours: [8, 12, 16],
       minutes: [10, 40],
@@ -39,13 +43,27 @@ const timeOptionsExamples = [
 ];
 
 export const DateTimeExampleTimeOptions = () => {
+  const [values, setValues] = useState<{ [key: string]: Date | undefined }>({});
+
+  const handleChange = (index: number) => (value: Date) => {
+    setValues((prev) => ({
+      ...prev,
+      [index]: value,
+    }));
+  };
+
   return (
     <Example
       col={4}
       separately
       items={timeOptionsExamples}
       getItemNode={(item) => (
-        <DateTime type="time" timeOptions={item.options} />
+        <DateTime
+          type="time"
+          timeOptions={item.options}
+          value={values[item.index]}
+          onChange={handleChange(item.index)}
+        />
       )}
       getItemLabel={(item) => item.label}
     />
