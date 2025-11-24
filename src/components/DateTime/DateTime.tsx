8dd16e50885931b078e449c5fd644ca2275f592d
ShavrinAleksei <apshavrinwork@gmail.com>
2025-11-24 11:19:23 +0000

    feat(DateTime): add time configuration support and stop multiplicity propagation

--- src/components/DateTime/DateTime.tsx
+++ src/components/DateTime/DateTime.tsx
@@ -1,4 +1,4 @@
-import React, { forwardRef, useRef } from 'react';
+import React, { forwardRef, useMemo, useRef } from 'react';
 
 import { usePropsHandler } from '##/components/EventInterceptor/usePropsHandler';
 import { useForkRef } from '##/hooks/useForkRef/useForkRef';
@@ -9,11 +9,13 @@ import { DateTimeTypeDateTime } from './DateTimeTypeDateTime/DateTimeTypeDateTim
 import { DateTimeTypeMonth } from './DateTimeTypeMonth/DateTimeTypeMonth';
 import { DateTimeTypeTime } from './DateTimeTypeTime/DateTimeTypeTime';
 import { DateTimeTypeYear } from './DateTimeTypeYear/DateTimeTypeYear';
+import { isTypeWithTime } from './helpers';
 import {
   DateTimeComponent,
   DateTimePropType,
   dateTimePropTypeDefault,
   DateTimeTypeComponent,
+  TimeOptions,
 } from './helpers/types';
 
 const typeMap: Record<
@@ -31,15 +33,56 @@ export const COMPONENT_NAME = 'DateTime' as const;
 
 export const DateTime: DateTimeComponent = forwardRef((props, ref) => {
   const dateTimeRef = useRef<HTMLDivElement>(null);
-  const { type = dateTimePropTypeDefault, ...otherProps } = usePropsHandler(
-    COMPONENT_NAME,
-    props,
-    dateTimeRef,
+  const {
+    type = dateTimePropTypeDefault,
+    timeOptions,
+    multiplicityHours,
+    multiplicityMinutes,
+    multiplicitySeconds,
+    ...otherProps
+  } = usePropsHandler(COMPONENT_NAME, props, dateTimeRef);
+
+  // TODO: major - удалить multiplicity* props конвертацию, используйте timeOptions напрямую
+  const effectiveTimeOptions = useMemo((): TimeOptions => {
+    return {
+      hours:
+        timeOptions?.hours ??
+        (multiplicityHours !== undefined
+          ? { step: multiplicityHours }
+          : undefined),
+      minutes:
+        timeOptions?.minutes ??
+        (multiplicityMinutes !== undefined
+          ? { step: multiplicityMinutes }
+          : undefined),
+      seconds:
+        timeOptions?.seconds ??
+        (multiplicitySeconds !== undefined
+          ? { step: multiplicitySeconds }
+          : undefined),
+    };
+  }, [
+    timeOptions,
+    multiplicityHours,
+    multiplicityMinutes,
+    multiplicitySeconds,
+  ]);
+
+  const timeProps = useMemo(
+    () =>
+      isTypeWithTime(type) ? { timeOptions: effectiveTimeOptions } : undefined,
+    [type, effectiveTimeOptions],
   );
 
   const Component = getByMap(typeMap, type);
 
-  return <Component {...otherProps} ref={useForkRef([ref, dateTimeRef])} />;
+  return (
+    <Component
+      {...otherProps}
+      {...timeProps}
+      ref={useForkRef([ref, dateTimeRef])}
+    />
+  );
 });
 
 export * from './helpers/types';
