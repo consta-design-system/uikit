1ab852289bd20c663c6503aef0a7a645d27a4cd4
ShavrinAleksei <apshavrinwork@gmail.com>
2026-01-28 13:07:56 +0000

    docs(useBodyScrollFreeze): add base documentation

---
+++ src/hooks/useBodyScrollFreeze/__stand__/useBodyScrollFreeze.stand.ts
@@ -0,0 +1,10 @@
+import { createStand } from '##/stand/standConfig';
+
+export default createStand({
+  title: 'useBodyScrollFreeze',
+  id: 'useBodyScrollFreeze',
+  group: 'hooks',
+  description: 'Хук для блокировки прокрутки страницы.',
+  alias: ['хук', 'скролл', 'body', 'modal'],
+  order: 10,
+});
