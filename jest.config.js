8153eff3b2b19e5214fa88c7c20df9884a380ce7
Mikhail Baev <baev2m@gmail.com>
2026-02-01 20:27:41 +0500

    chore(tests): remove `ts-jest`, keep `babel-jest` for tests

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   verbose: true,
-  preset: 'ts-jest',
   moduleNameMapper: {
     '\\.css$': '<rootDir>/__mocks__/styleMock.js',
     '\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
