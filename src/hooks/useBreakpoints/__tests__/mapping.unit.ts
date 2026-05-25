7d17e289eceb145d0e439a47f76c5d5469010fb0
gizeasy <gizeasy@gmail.com>
2026-05-25 09:03:00 +0300

    Create mapping.unit.ts

---
+++ src/hooks/useBreakpoints/__tests__/mapping.unit.ts
@@ -0,0 +1,39 @@
+import { describe, expect, test } from 'vitest';
+
+import { mapping, sortObj } from '../mapping';
+
+describe('useBreakpoints', () => {
+  test('sortObj', () => {
+    const result = sortObj({
+      desktop: 1000,
+      mobile: 0,
+      tablet: 800,
+    });
+
+    const expected: typeof result = {
+      mobile: 0,
+      tablet: 800,
+      desktop: 1000,
+    };
+
+    expect(Object.keys(result).join('-')).toEqual(
+      Object.keys(expected).join('-'),
+    );
+  });
+
+  test('mapping', () => {
+    const result = mapping(802, {
+      desktop: 1000,
+      mobile: 0,
+      tablet: 800,
+    });
+
+    const expected: typeof result = {
+      mobile: true,
+      tablet: true,
+      desktop: false,
+    };
+
+    expect(result).toEqual(expected);
+  });
+});
